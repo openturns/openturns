e97fd245932816763f0ed8a58307a9e3ba3f03cc
Regis lebrun <regis_anne.lebrun_dutfoy@yahoo.fr>
2015-11-05 17:48:37 +0100

    Added KarhunenLoeveP1Factory class.
    This class allows to compute the Karhunen-Loeve decomposition of a covariance model usinga P1 Lagrange interpolation. Currently, it is based on dense linear algebra that limits the size of the problem that can be addressed. A more memory efficient implementation will be proposed later.

---
+++ lib/test/t_KarhunenLoeveP1Factory_std.cxx
@@ -0,0 +1,51 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief The test file of class GaussKronrod
+ *
+ *  Copyright 2005-2015 Airbus-EDF-IMACS-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include "OT.hxx"
+#include "OTtestcode.hxx"
+#include <cmath>
+
+using namespace OT;
+using namespace OT::Test;
+
+int main(int argc, char *argv[])
+{
+  TESTPREAMBLE;
+  OStream fullprint(std::cout);
+  setRandomGenerator();
+
+  try
+  {
+    Mesh mesh(IntervalMesher(Indices(1, 9)).build(Interval(-1.0, 1.0)));
+    KarhunenLoeveP1Factory factory(mesh, 0.0);
+    NumericalPoint lambda;
+    ProcessSample KLModes(factory.buildAsProcessSample(AbsoluteExponential(1, 1.0), lambda));
+    fullprint << "KL modes=" << KLModes;
+    fullprint << "KL eigenvalues=" << lambda;
+  }
+  catch (TestFailed & ex)
+  {
+    std::cerr << ex << std::endl;
+    return ExitCode::Error;
+  }
+
+
+  return ExitCode::Success;
+}
---
+++ python/src/KarhunenLoeveP1Factory.i
@@ -0,0 +1,10 @@
+// SWIG file KarhunenLoeveP1Factory.i
+
+%{
+#include "KarhunenLoeveP1Factory.hxx"
+%}
+
+%include KarhunenLoeveP1Factory_doc.i
+
+%include KarhunenLoeveP1Factory.hxx
+namespace OT { %extend KarhunenLoeveP1Factory { KarhunenLoeveP1Factory(const KarhunenLoeveP1Factory & other) { return new OT::KarhunenLoeveP1Factory(other); } } }
