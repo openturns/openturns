13318cca16d7f9d6f211930d1f8eb914db6fadc6
Sofiane HADDAD <haddad@imacs.polytechnique.fr>
2015-12-02 14:18:21 +0100

    Add cxx/py test for BoxCoxFactory::build that relies on generalized linear models

---
+++ lib/test/t_BoxCoxFactory_glm.cxx
@@ -0,0 +1,83 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief The test file of class BoxCoxFactory for standard methods using GLM
+ *
+ *  Copyright 2005-2015 Airbus-EDF-IMACS-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include "OT.hxx"
+#include "OTtestcode.hxx"
+
+using namespace OT;
+using namespace OT::Test;
+
+int main(int argc, char *argv[])
+{
+  TESTPREAMBLE;
+  OStream fullprint(std::cout);
+  setRandomGenerator();
+
+  try
+  {
+    const UnsignedInteger size = 200;
+
+    // input sample
+    const NumericalSample inputSample = Uniform(-1.0, 1.0).getSample(size);
+    NumericalSample outputSample(inputSample);
+
+    // Evaluation of y = ax + b (a: scale, b: translate)
+
+    // scale
+    const NumericalPoint scale(1, 3.0);
+    outputSample *= scale;
+
+    // translate sample
+    const NumericalPoint translate(1, 3.1);
+    outputSample += translate;
+
+    // Finally inverse transform using an arbitrary lambda
+    const NumericalPoint lambda(1, 1.8);
+    const InverseBoxCoxEvaluationImplementation  boxCoxFunction(lambda);
+
+    // transform y using BoxCox function
+    outputSample = boxCoxFunction(outputSample);
+
+    // Add small noise
+    const NumericalSample epsilon = Normal(0, 1e-2).getSample(size);
+    outputSample +=  epsilon;
+    // Now we build the factory
+    BoxCoxFactory factory;
+
+    // Creation of the BoxCoxTransform
+    GeneralizedLinearModelResult result;
+    const Basis basis = LinearBasisFactory(1).build();
+    const DiracCovarianceModel covarianceModel;
+    const NumericalPoint shift(1, 1.0e-1);
+    BoxCoxTransform myBoxCox = factory.build(inputSample, outputSample, covarianceModel, basis, shift, result);
+
+    fullprint << "myBoxCox (GLM)=" << myBoxCox << std::endl;
+    fullprint << "GLM result=" << result << std::endl;
+
+  }
+  catch (TestFailed & ex)
+  {
+    std::cerr << ex << std::endl;
+    return ExitCode::Error;
+  }
+
+
+  return ExitCode::Success;
+}
---
+++ python/test/t_BoxCoxFactory_glm.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+import openturns as ot
+
+ot.RandomGenerator.SetSeed(0)
+
+size = 200
+
+# input sample
+inputSample = ot.Uniform(-1.0, 1.0).getSample(size)
+outputSample = ot.NumericalSample(inputSample)
+
+# Evaluation of y = ax + b (a: scale, b: translate)
+
+# scale
+scale = [3.0]
+outputSample *= scale
+
+# translate sample
+translate = [3.1]
+outputSample += translate
+
+# Finally inverse transform using an arbitrary lambda
+lamb = [1.8]
+boxCoxFunction = ot.InverseBoxCoxEvaluationImplementation(lamb)
+
+# transform y using BoxCox function
+outputSample = boxCoxFunction(outputSample)
+
+# Add small noise
+epsilon = ot.Normal(0, 1.0e-2).getSample(size)
+outputSample +=  epsilon
+
+# Now we build the factory
+factory = ot.BoxCoxFactory()
+
+# Creation of the BoxCoxTransform
+result = ot.GeneralizedLinearModelResult()
+basis = ot.LinearBasisFactory(1).build()
+covarianceModel = ot.DiracCovarianceModel()
+shift = [1.0e-1]
+myBoxCox = factory.build(inputSample, outputSample, covarianceModel, basis, shift, result)
+
+print("myBoxCox (GLM) =", myBoxCox)
+print("GLM result     =", result)
+
