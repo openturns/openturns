58f1aff92d6dbc00119d50e75021565312d5bd8b
Julien Schueller <schueller@phimeca.com>
2016-03-18 23:05:34 +0100

    Add Distribution::getProbabilities
    Returns the probability density values over the support

--- python/test/t_Poisson_std.py
+++ python/test/t_Poisson_std.py
@@ -87,6 +87,7 @@ try:
         print("standard moment n=", i, " value=",
               distribution.getStandardMoment(i))
     print("Standard representative=", distribution.getStandardRepresentative())
+    print("probabilities=", distribution.getProbabilities())
 
 except:
     import sys
