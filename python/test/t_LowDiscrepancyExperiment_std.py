c9be7ca0476fb27e35e0786dfa059f59b3562039
regislebrun <regis.lebrun@m4x.org>
2016-06-29 12:33:45 +0200

    Fixed a bug in LowDiscrepancyExperiment.
    The marginals were not correctly initialized when using constructors with no distribution.

--- python/test/t_LowDiscrepancyExperiment_std.py
+++ python/test/t_LowDiscrepancyExperiment_std.py
@@ -11,19 +11,34 @@ try:
     size = 10
     myPlane = LowDiscrepancyExperiment(HaltonSequence(), distribution, size)
     print("myPlane = ", myPlane)
+    # Test sampling with weights
     sample, weights = myPlane.generateWithWeights()
     print("sample  = ", repr(sample))
     print("weights = ", repr(weights))
+    # Test sampling with reinitialization each time the distribution is
+    # set (default behaviour)
+    # sample 2 != sample
     print("sample2 = ", repr(myPlane.generate()))
     myPlane.setDistribution(distribution)
+    # sample 3 == sample
     print("sample3 = ", repr(myPlane.generate()))
+
+    # Test sampling without reinitialization excepted when distribution
+    # dimension changes
     myPlane = LowDiscrepancyExperiment(HaltonSequence(), distribution, size, False)
     print("sample  = ", repr(myPlane.generate()))
+    # sample 2 != sample
     print("sample2 = ", repr(myPlane.generate()))
     myPlane.setDistribution(distribution)
+    # sample 3 != sample && sample 3 != sample 2
     print("sample3 = ", repr(myPlane.generate()))
+    # Test dimension change
+    myPlane.setDistribution(Normal())
+    print("sample = ", repr(myPlane.generate()))
+
+    # Test constructor with no distribution and dimension>1
     myPlane = LowDiscrepancyExperiment(HaltonSequence(2), size)
-    print("sample4 = ", repr(myPlane.generate()))
+    print("sample = ", repr(myPlane.generate()))
 except:
     import sys
     print("t_LowDiscrepancyExperiment_std.py",
