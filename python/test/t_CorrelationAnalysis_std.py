5ba7a8d3ac4018f9907790dbb6796a0a96b1fc90
Sofiane HADDAD <haddad@imacs.polytechnique.fr>
2016-09-07 23:28:12 +0200

    Bugfix in CorrelationAnalysis::SRC
    Normalization factor should be the variance estimated thanks to the linear
    regression and not the initial data.

--- python/test/t_CorrelationAnalysis_std.py
+++ python/test/t_CorrelationAnalysis_std.py
@@ -1,38 +1,36 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
-from openturns import *
+import openturns as ot
 
-TESTPREAMBLE()
-RandomGenerator.SetSeed(0)
+ot.TESTPREAMBLE()
+ot.RandomGenerator.SetSeed(0)
 
 try:
     dimension = 2
     sampleSize = 1000
 
     # we create an analytical function
-    analytical = NumericalMathFunction(['x0', 'x1'], ['f'], ['10+3*x0+x1'])
+    analytical = ot.NumericalMathFunction(['x0', 'x1'], ['f'], ['10+3*x0+x1'])
 
-    # we create a collection of uniform distributions over [-Pi Pi[
-    aCollection = DistributionCollection()
-    for i in range(dimension):
-        aCollection.add(Distribution(Normal()))
+    # we create a collection of centered and reduced gaussian distributions
+    aCollection = [ot.Normal()] * dimension
 
     # we create one distribution object
-    aDistribution = ComposedDistribution(
-        aCollection, Copula(IndependentCopula(dimension)))
+    aDistribution = ot.ComposedDistribution(aCollection)
 
-    randomVector = RandomVector(aDistribution)
-    composite = RandomVector(analytical, randomVector)
+    # Random vectors
+    randomVector = ot.RandomVector(aDistribution)
+    composite = ot.RandomVector(analytical, randomVector)
     print("composite=", composite)
 
     # we create two input samples for the function
     inputSample = randomVector.getSample(sampleSize)
     outputSample = analytical(inputSample)
 
-    src = CorrelationAnalysis.SRC(inputSample, outputSample)
+    src = ot.CorrelationAnalysis.SRC(inputSample, outputSample)
     print("src=", src)
 except:
     import sys
-    print("t_CorrelationAnalysis_sobol.py",
+    print("t_CorrelationAnalysis_std.py",
           sys.exc_info()[0], sys.exc_info()[1])
