a28b32b35a511c07d8ed45d505b2f79fe1234640
Julien Schueller <schueller@phimeca.com>
2016-10-19 17:22:00 +0200

    Test parameter management

--- python/test/t_CovarianceModel_std.py
+++ python/test/t_CovarianceModel_std.py
@@ -6,10 +6,16 @@ import openturns as ot
 
 def test_model(myModel):
 
-    print("myModel = ",  myModel)
+    print('myModel = ',  myModel)
 
     spatialDimension = myModel.getSpatialDimension()
     dimension = myModel.getDimension()
+    active = myModel.getActiveParameter()
+    print('active=', active)
+    print('parameter=', myModel.getParameter())
+    print('parameterDescription=', myModel.getParameterDescription())
+
+
 
     x1 = ot.NumericalPoint(spatialDimension)
     x2 = ot.NumericalPoint(spatialDimension)
@@ -19,21 +25,21 @@ def test_model(myModel):
 
     eps = 1e-5
     if (dimension == 1):
-        print("myModel(", x1, ", ", x2, ")=",  myModel(x1, x2))
+        print('myModel(', x1, ', ', x2, ')=',  myModel(x1, x2))
 
         grad = myModel.partialGradient(x1, x2)
-        print("dCov =", grad)
+        print('dCov =', grad)
         gradfd = ot.NumericalPoint(spatialDimension)
         for j in range(spatialDimension):
             x1_d = ot.NumericalPoint(x1)
             x1_d[j] = x1_d[j] + eps
             gradfd[j] = (myModel(x1_d, x2)[0, 0] - myModel(x1, x2)[0, 0]) / eps
-        print("dCov (FD)=", gradfd)
+        print('dCov (FD)=', gradfd)
     else:
-        print("myModel(", x1, ", ", x2, ")=",  repr(myModel(x1, x2)))
+        print('myModel(', x1, ', ', x2, ')=',  repr(myModel(x1, x2)))
 
         grad = myModel.partialGradient(x1, x2)
-        print("dCov =", repr(grad))
+        print('dCov =', repr(grad))
 
         gradfd = ot.Matrix(spatialDimension, dimension * dimension)
         covarianceX1X2 = myModel(x1, x2)
@@ -50,13 +56,13 @@ def test_model(myModel):
                 localCovariance.getImplementation())
             for j in range(currentValue.getSize()):
                 gradfd[i, j] = (currentValue[j] - centralValue[j]) / eps
-        print("dCov (FD)=", repr(gradfd))
+        print('dCov (FD)=', repr(gradfd))
 
 spatialDimension = 2
 
 
 myDefautModel = ot.SquaredExponential()
-print("myDefautModel = ",  myDefautModel)
+print('myDefautModel = ',  myDefautModel)
 
 
 myModel = ot.SquaredExponential(spatialDimension)
@@ -64,28 +70,49 @@ test_model(myModel)
 
 
 myDefautModel = ot.GeneralizedExponential()
-print("myDefautModel = ",  myDefautModel)
+print('myDefautModel = ',  myDefautModel)
 
 myModel = ot.GeneralizedExponential([10.0] * spatialDimension, 1.5)
 test_model(myModel)
 
 
 myDefautModel = ot.AbsoluteExponential()
-print("myDefautModel = ",  myDefautModel)
+print('myDefautModel = ',  myDefautModel)
 
 myModel = ot.AbsoluteExponential(spatialDimension)
 test_model(myModel)
 
 
 myDefautModel = ot.MaternModel()
-print("myDefautModel = ",  myDefautModel)
+print('myDefautModel = ',  myDefautModel)
 
 myModel = ot.MaternModel([8.0] * spatialDimension, 2.0)
 test_model(myModel)
 
 
+myDefautModel = ot.ExponentiallyDampedCosineModel()
+print('myDefautModel = ',  myDefautModel)
+
+myModel = ot.ExponentiallyDampedCosineModel([1.0] * spatialDimension, [1.0], 0.1)
+test_model(myModel)
+
+myDefautModel = ot.SphericalModel()
+print('myDefautModel = ',  myDefautModel)
+
+myModel = ot.SphericalModel([1.0] * spatialDimension, [1.0], 0.5)
+test_model(myModel)
+
+
+myDefautModel = ot.DiracCovarianceModel()
+print('myDefautModel = ',  myDefautModel)
+
+amplitude = list(map(lambda k: 1.5 + 2.0 * k, range(2)))
+myModel = ot.DiracCovarianceModel(spatialDimension, amplitude)
+test_model(myModel)
+
+
 myDefautModel = ot.ProductCovarianceModel()
-print("myDefautModel = ",  myDefautModel)
+print('myDefautModel = ',  myDefautModel)
 
 cov1 = ot.AbsoluteExponential([3.0])
 cov2 = ot.SquaredExponential([2.0])
