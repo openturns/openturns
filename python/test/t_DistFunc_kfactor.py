32ef315f3db98751b5856f968fbbe8ca5fc982d8
Julien Schueller <schueller@phimeca.com>
2017-01-03 18:02:43 +0100

    Actually test DistFunc.kFactorPooled
    kFactorPooled was not used, fixed it to match cxx results
    Also fix a formatting issue with Python 3.5

--- python/test/t_DistFunc_kfactor.py
+++ python/test/t_DistFunc_kfactor.py
@@ -1,35 +1,30 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
-from openturns import *
+import openturns as ot
 
-TESTPREAMBLE()
-RandomGenerator.SetSeed(0)
-
-try:
-    nMin = 2
-    nMax = 4
-    pMin = 0.1
-    pMax = 0.9
-    nP = 3
-    alphaMin = 0.1
-    alphaMax = 0.9
-    nAlpha = 3
-    for n in range(nMin, nMax):
-        for iP in range(nP):
-            p = pMin + (pMax - pMin) * iP / (nP + 1.0)
-            for iAlpha in range(nAlpha):
-                alpha = alphaMin + (alphaMax - alphaMin) * iAlpha / (nAlpha + 1.0)
-                print("kFactor(", 2 * n, ", ", p, ", ", alpha, ")=%.5f" % DistFunc.kFactor(2 * n, p, alpha))
-    mMin = 1
-    mMax = 3
-    for n in range(nMin, nMax):
-        for m in range(mMin, mMax):
-            for iP in range(nP):
-                p = pMin + (pMax - pMin) * iP / (nP + 1.0)
-                for iAlpha in range(nAlpha):
-                    alpha = alphaMin + (alphaMax - alphaMin) * iAlpha / (nAlpha + 1.0)
-                    print("kFactorPooled(", 2 * n, ", ", m, ", ", p, ", ", alpha, ")=%.5f" % DistFunc.kFactor(2 * n, p, alpha))
-except:
-    import sys
-    print("t_DistFunc_kfactor.py", sys.exc_info()[0], sys.exc_info()[1])
+nMin = 2
+nMax = 4
+pMin = 0.1
+pMax = 0.9
+nP = 2
+alphaMin = 0.1
+alphaMax = 0.9
+nAlpha = 2
+for n in range(nMin, nMax):
+    for iP in range(nP + 1):
+        p = pMin + (pMax - pMin) * iP / nP
+        for iAlpha in range(nAlpha + 1):
+            alpha = alphaMin + (alphaMax - alphaMin) * iAlpha / nAlpha
+            kF = ot.DistFunc.kFactor(2 * n, p, alpha)
+            print('kFactor(%d, %.12g, %.12g)=%.6g' % (2 * n, p, alpha, kF))
+mMin = 1
+mMax = 3
+for n in range(nMin, nMax):
+    for m in range(mMin, mMax):
+        for iP in range(nP + 1):
+            p = pMin + (pMax - pMin) * iP / nP
+            for iAlpha in range(nAlpha + 1):
+                alpha = alphaMin + (alphaMax - alphaMin) * iAlpha / nAlpha
+                kF = ot.DistFunc.kFactorPooled(2 * n, m, p, alpha)
+                print('kFactorPooled(%d, %.12g, %.12g, %.12g)=%.6g' % (2 * n, m, p, alpha, kF))
