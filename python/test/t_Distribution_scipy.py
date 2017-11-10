Fix SciPyDistribution range computation

python/test/t_Distribution_scipy.py
@@ -1,41 +1,32 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
+from __future__ import print_function
 import openturns as ot
 import scipy as sp
 import scipy.stats as st
 
-try:
-    # create a scipy distribution
+for scipy_dist in [st.uniform(), st.johnsonsu(2.55, 2.25)]:
     sp.random.seed(42)
-    scipy_dist = st.uniform()
 
     # create an openturns distribution
     py_dist = ot.SciPyDistribution(scipy_dist)
     distribution = ot.Distribution(py_dist)
 
-    print(('distribution=%s' % str(distribution)))
-    print(('realization=%s' % str(distribution.getRealization())))
-    print(('sample=%s' % str(distribution.getSample(5))))
-    point = ot.Point(1, 0.6)
-    print(('pdf=%s' % str(distribution.computePDF(point))))
-    print(('cdf=%s' % str(distribution.computeCDF(point))))
-    print(('mean=%s' % str(distribution.getMean())))
-    print(('std=%s' % str(distribution.getStandardDeviation())))
-    print(('skewness=%s' % str(distribution.getSkewness())))
-    print(('kurtosis=%s' % str(distribution.getKurtosis())))
-    print(('range=%s' % str(distribution.getRange())))
-
-    # create an openturns random vector
-    py_rv = ot.SciPyRandomVector(scipy_dist)
-    randomVector = ot.RandomVector(py_rv)
-    print(('randomVector=%s' % str(randomVector)))
-    print(('realization=%s' % str(randomVector.getRealization())))
-    print(('sample=%s' % str(randomVector.getSample(5))))
-    print(('mean=%s' % str(randomVector.getMean())))
-    print(('covariance=%s' % str(randomVector.getCovariance())))
-
-
-except:
-    import sys
-    import traceback
-    traceback.print_exc(file=sys.stdout)
+    print('distribution=', distribution)
+    print('realization=', distribution.getRealization())
+    sample = distribution.getSample(10000)
+    print('sample=', sample[0:5])
+    point = [0.6]
+    print('pdf= %.6g' % distribution.computePDF(point))
+    cdf = distribution.computeCDF(point)
+    print('cdf= %.6g' % cdf)
+    print('quantile=', distribution.computeQuantile(cdf))
+    print('mean=', distribution.getMean())
+    print('mean(sampling)=', sample.computeMean())
+    print('std=', distribution.getStandardDeviation())
+    print('std(sampling)=', sample.computeStandardDeviationPerComponent())
+    print('skewness=', distribution.getSkewness())
+    print('skewness(sampling)=', sample.computeSkewness())
+    print('kurtosis=', distribution.getKurtosis())
+    print('kurtosis(sampling)=', sample.computeKurtosis())
+    print('range=', distribution.getRange())