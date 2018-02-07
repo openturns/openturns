0460f0de468ce1657ec5628700f6bea0a338a29f
Denis Barbier <barbier@imacs.polytechnique.fr>
2018-02-07 17:36:17 +0100

    Fix output for old Python
    Also remove ot.TESTPREAMBLE() and try/catch.

--- python/test/t_DomainComplement_std.py
+++ python/test/t_DomainComplement_std.py
@@ -3,29 +3,25 @@
 from __future__ import print_function
 import openturns as ot
 
-ot.TESTPREAMBLE()
+ot.PlatformInfo.SetNumericalPrecision(6)
 
-try:
-    dim = 2
-    p1 = ot.Point(dim, -1.0)
-    p2 = ot.Point(dim, 2.0)
-    domain = ot.Interval(p1, p2)
+dim = 2
+p1 = ot.Point(dim, -1.0)
+p2 = ot.Point(dim, 2.0)
+domain = ot.Interval(p1, p2)
 
-    complement = ot.DomainComplement(domain)
+complement = ot.DomainComplement(domain)
 
-    # Inside
-    p3 = (p1 + p2) * 0.5
-    # Outside
-    p4 = 2.0 * p2
-    print("Domain=", domain)
-    print("Complement=", complement)
-    # Accessors
-    print("Dimension=", complement.getDimension())
-    # Contains
-    print("is point ", p3, " inside complement? ",  complement.contains(p3))
-    print("is point ", p4, " inside complement? ",  complement.contains(p4))
-    sample = [p3, p4]
-    print("is sample ", sample, " inside? ",  complement.contains(sample))
-except:
-    import sys
-    print("t_DomainComplement_std.py", sys.exc_info()[0], sys.exc_info()[1])
+# Inside
+p3 = (p1 + p2) * 0.5
+# Outside
+p4 = 2.0 * p2
+print("Domain=", domain)
+print("Complement=", complement)
+# Accessors
+print("Dimension=", complement.getDimension())
+# Contains
+print("is point ", p3, " inside complement? ",  complement.contains(p3))
+print("is point ", p4, " inside complement? ",  complement.contains(p4))
+sample = [p3, p4]
+print("is sample ", sample, " inside? ",  complement.contains(sample))
