59f8d601c9677650a6302f01c4b6b9ad61201288
Denis Barbier <barbier@imacs.polytechnique.fr>
2018-02-17 23:55:10 +0100

    Rewrite t_KDTree_std.py
    Remove output and instead check return values.

--- python/test/t_KDTree_std.py
+++ python/test/t_KDTree_std.py
@@ -2,16 +2,40 @@
 
 from __future__ import print_function
 import openturns as ot
+import numpy as np
+import os
 
 ot.TESTPREAMBLE()
 
 
-sample = ot.Normal(3).getSample(10)
+sample = ot.Normal(3).getSample(1000)
 tree = ot.KDTree(sample)
 print('tree=', tree)
 
-test = ot.Normal(3).getSample(20)
-for i in range(len(test)):
-    index = tree.getNearestNeighbourIndex(test[i])
-    neighbour = tree.getNearestNeighbour(test[i])
-    print('Nearest neighbour of', test[i], '=', neighbour, 'index=', index)
+test = ot.Normal(3).getSample(100)
+
+sample_np = np.array(sample)
+test_np = np.array(test)
+
+def nearest_debug(x):
+    global sample_np
+    a = np.sum(np.square(sample_np - x), axis=1)
+    return np.argmin(a)
+
+def nearest_debug_indices(x):
+    global sample_np
+    a = np.sum(np.square(sample_np - x), axis=1)
+    return a.argsort()
+
+neighbourIndices = tree.getNearestNeighbourIndex(test)
+
+neighbourIndices_np = map(nearest_debug, test)
+
+if neighbourIndices != neighbourIndices_np:
+    print("Errors found in getNearestNeighbourIndex")
+    os.exit(1)
+
+for x in test:
+    if np.any(nearest_debug_indices(x)[:10] != tree.getNearestNeighboursIndices(x, 10, True)):
+        print("Errors found in getNearestNeighbours")
+        os.exit(1)
