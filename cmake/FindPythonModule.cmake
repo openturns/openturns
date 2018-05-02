82cf9558e28836c0fdaf51d5b14f614664fe3647
Denis Barbier <barbier@imacs.polytechnique.fr>
2018-05-02 13:52:04 +0200

    Drop support for CMake < 2.4
    Update architecture.rst

--- cmake/FindPythonModule.cmake
+++ cmake/FindPythonModule.cmake
@@ -42,6 +42,8 @@ macro (find_python_module module)
       set (${module}_FIND_VERSION ${ARGV1})
     endif ()
 
+    set (_${module_upper}_VERSION_MATCH TRUE)
+
     # A module's location is usually a directory, but for binary modules it's a .so file.
     execute_process (COMMAND "${PYTHON_EXECUTABLE}" "-c" 
                       "import re, ${module}; print(re.compile('/__init__.py.*').sub('',${module}.__file__))"
@@ -57,7 +59,6 @@ macro (find_python_module module)
                         OUTPUT_VARIABLE _${module}_version
                         ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-      set (_${module_upper}_VERSION_MATCH TRUE)
       if (NOT _${module}_status)
         set (${module_upper}_VERSION_STRING ${_${module}_version})
         if (${module}_FIND_VERSION)
@@ -69,18 +70,10 @@ macro (find_python_module module)
       endif ()
     endif ()
 
-    if (CMAKE_VERSION VERSION_LESS 2.8.3)
-      find_package_handle_standard_args (${module} DEFAULT_MSG ${module_upper}_LOCATION 
-                                          ${module}_FIND_OPTIONAL
-                                          _${module_upper}_VERSION_MATCH
-                                       )
-    else ()
-      find_package_handle_standard_args (${module} REQUIRED_VARS ${module_upper}_LOCATION 
-                                          ${module}_FIND_OPTIONAL
-                                          _${module_upper}_VERSION_MATCH
-                                          VERSION_VAR ${module_upper}_VERSION_STRING
-                                       )
-    endif ()
+    find_package_handle_standard_args (${module} DEFAULT_MSG ${module_upper}_LOCATION
+                                        ${module}_FIND_OPTIONAL
+                                        _${module_upper}_VERSION_MATCH
+                                     )
     mark_as_advanced (${module_upper}_LOCATION)
   endif (NOT ${module_upper}_FOUND)
 endmacro (find_python_module)
