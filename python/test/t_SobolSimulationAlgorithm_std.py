c318a18ca82cefb9b2d9ab1aff7a9cd5c0c0a4d5
Julien Schueller <schueller@phimeca.com>
2018-09-10 09:40:56 +0200

    Add validation references

--- python/test/t_SobolSimulationAlgorithm_std.py
+++ python/test/t_SobolSimulationAlgorithm_std.py
@@ -20,19 +20,19 @@ def stop():
 dimension = 3
 
 # gsobol
-meanTh = 1.0
-a = [0.0] * dimension
-inputVariables = [''] * dimension
-formula = '1.0'
-covTh = 1.0
-for i in range(dimension):
-    a[i] = 0.5 * i
-    covTh = covTh * (1.0 + 1.0 / (3.0 * (1.0 + a[i]) ** 2))
-    inputVariables[i] = "xi" + str(i)
-    formula += " * ((abs(4.0 * xi" + str(i) + " - 2.0) + " + str(a[i]) + ") / (1.0 + " + str(a[i]) + "))"
-covTh = covTh - 1.0
-model = ot.SymbolicFunction(inputVariables, [formula])
-distribution = ot.ComposedDistribution([ot.Uniform(0.0, 1.0)] * dimension)
+#meanTh = 1.0
+#a = [0.0] * dimension
+#inputVariables = [''] * dimension
+#formula = '1.0'
+#covTh = 1.0
+#for i in range(dimension):
+    #a[i] = 0.5 * i
+    #covTh = covTh * (1.0 + 1.0 / (3.0 * (1.0 + a[i]) ** 2))
+    #inputVariables[i] = "xi" + str(i)
+    #formula += " * ((abs(4.0 * xi" + str(i) + " - 2.0) + " + str(a[i]) + ") / (1.0 + " + str(a[i]) + "))"
+#covTh = covTh - 1.0
+#model = ot.SymbolicFunction(inputVariables, [formula])
+#distribution = ot.ComposedDistribution([ot.Uniform(0.0, 1.0)] * dimension)
 
 # ishigami
 formula = 'sin(pi_*X1)+7*sin(pi_*X2)*sin(pi_*X2)+0.1*((pi_*X3)*(pi_*X3)*(pi_*X3)*(pi_*X3))*sin(pi_*X1)'
