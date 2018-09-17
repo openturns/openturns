8a9e4d73fb6b9af98c677a1989b8a959307a44fd
Julien Schueller <schueller@phimeca.com>
2018-09-17 12:14:56 +0200

    Do not allow to add twice the pair to the cache
    Else the keys_ attribute contains duplicates.
    Does not hurt the insertion performance, see bench from
    https://github.com/openturns/openturns/pull/606.

--- python/test/t_MemoizeFunction_std.py
+++ python/test/t_MemoizeFunction_std.py
@@ -1,39 +1,38 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
-from openturns import *
+import openturns as ot
 
-TESTPREAMBLE()
 
-try:
-    f = SymbolicFunction("x", "x^2")
-    f = MemoizeFunction(f)
-    f.disableHistory()
-    print(f)
-    size = 4
-    inputSample = Sample(size, 1)
-    for i in range(size):
-        inputSample[i, 0] = i
-    outputSample = f(inputSample)
-    print("Is history enabled for f? ", f.isHistoryEnabled())
-    print("input history=", f.getInputHistory())
-    print("output history=", f.getOutputHistory())
-    f.enableHistory()
-    outputSample = f(inputSample)
-    print("Is history enabled for f? ", f.isHistoryEnabled())
-    print("input history=", f.getInputHistory())
-    print("output history=", f.getOutputHistory())
-    f.clearHistory()
-    print("Is history enabled for f? ", f.isHistoryEnabled())
-    print("input history=", f.getInputHistory())
-    print("output history=", f.getOutputHistory())
-    # Perform the computation twice
-    outputSample = f(inputSample)
-    outputSample = f(inputSample)
-    print("input history=", f.getInputHistory())
-    print("output history=", f.getOutputHistory())
+f = ot.SymbolicFunction("x", "x^2")
+f = ot.MemoizeFunction(f)
+f.disableHistory()
+print(f)
+size = 4
+inputSample = ot.Sample(size, 1)
+for i in range(size):
+    inputSample[i, 0] = i
+outputSample = f(inputSample)
+print("Is history enabled for f? ", f.isHistoryEnabled())
+print("input history=", f.getInputHistory())
+print("output history=", f.getOutputHistory())
+f.enableHistory()
+outputSample = f(inputSample)
+print("Is history enabled for f? ", f.isHistoryEnabled())
+print("input history=", f.getInputHistory())
+print("output history=", f.getOutputHistory())
+f.clearHistory()
+print("Is history enabled for f? ", f.isHistoryEnabled())
+print("input history=", f.getInputHistory())
+print("output history=", f.getOutputHistory())
+# Perform the computation twice
+outputSample = f(inputSample)
+outputSample = f(inputSample)
+print("input history=", f.getInputHistory())
+print("output history=", f.getOutputHistory())
 
-except:
-    import sys
-    print("t_Function_history.py",
-          sys.exc_info()[0], sys.exc_info()[1])
+mem = ot.MemoizeFunction(ot.SymbolicFunction(['x1', 'x2'], ['x1+x2', '3*(x1+x2)']))
+mem2 = ot.MemoizeFunction(mem.getMarginal(1))
+y = mem2([1, 2])
+print('mem2.in', mem2.getCacheInput())
+print('mem2.out', mem2.getCacheOutput())
