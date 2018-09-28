1775b2720621dee794eba763cc3bbf6349882ddb
Julien Schueller <schueller@phimeca.com>
2018-09-28 17:26:22 +0200

    CI: Use CircleCI docker builds
    Move Linux builds from Travis to CircleCI only.
    Using docker allows for a much faster startup time (no install on
    outdated host without pip binaries).
    It also allows to run a MinGW job.
    Travis is kept for OSX builds.
    This should improve reliability of builds.
    Added SWIG_COMPILE_FLAGS variable to pass additional compiler flags to
    swig modules.

---
+++ .circleci/run_docker_mingw.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -xe
+
+mkdir build && cd build
+
+ARCH=i686
+MINGW_PREFIX=/usr/${ARCH}-w64-mingw32
+PYMAJMIN=37
+PREFIX=${PWD}/install
+CXXFLAGS="-D_hypot=hypot -D_GLIBCXX_ASSERTIONS" ${ARCH}-w64-mingw32-cmake \
+  -DSWIG_COMPILE_FLAGS="-O0" \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+  -DPYTHON_INCLUDE_DIR=${MINGW_PREFIX}/include/python${PYMAJMIN} \
+  -DPYTHON_LIBRARY=${MINGW_PREFIX}/lib/libpython${PYMAJMIN}.dll.a \
+  -DPYTHON_EXECUTABLE=/usr/bin/${ARCH}-w64-mingw32-python${PYMAJMIN}-bin \
+  -DPYTHON_SITE_PACKAGES=Lib/site-packages \
+  -DUSE_COTIRE=ON -DCOTIRE_MAXIMUM_NUMBER_OF_UNITY_INCLUDES="-j16" ..
+make install
+${ARCH}-w64-mingw32-strip --strip-unneeded ${PREFIX}/bin/*.dll ${PREFIX}/Lib/site-packages/openturns/*.pyd
+echo lib/test python/test | xargs -n 1 cp ${PREFIX}/bin/*.dll
+ctest -R pyinstallcheck --output-on-failure --timeout 1000 ${MAKEFLAGS}
+make tests
+ctest -R cppcheck --output-on-failure --timeout 1000 ${MAKEFLAGS}
--- .circleci/upload_github_io.sh
+++ .circleci/upload_github_io.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-test -z "${CI_PULL_REQUEST}" || exit 0
+test -z "${CIRCLE_PULL_REQUEST}" || exit 0
 
 git clone https://${GH_TOKEN}@github.com/openturns/openturns.github.io.git
 if test -n "${CIRCLE_TAG}"
