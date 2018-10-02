4b11fab8e397e796cb443caccbd2da8d601afb10
Julien Schueller <schueller@phimeca.com>
2018-10-02 14:43:29 +0200

    Update indent.sh

--- utils/indent.sh
+++ utils/indent.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # indent some code, requires python, astyle and autopep8:
-# cd /tmp && git clone https://github.com/hhatto/autopep8.git && cd autopep8 && ./setup.py install --user
+# sudo apt install python3-examples astyle python-autopep8
 
-astyle --suffix=none --style=ansi --recursive --indent=spaces=2 --indent-switches --pad-oper --break-closing-brackets --max-instatement-indent=40 lib/*.?xx
-astyle --suffix=none --style=ansi             --indent=spaces=2 --indent-switches --pad-oper --break-closing-brackets --max-instatement-indent=40 python/src/*.?xx
+astyle --suffix=none --style=ansi --recursive --indent=spaces=2 --indent-switches --pad-oper --break-closing-brackets --max-instatement-indent=40 lib/*.?xx python/*.?xx
 
 # python
-/usr/share/doc/python2.7/examples/Tools/scripts/reindent.py --verbose --recurse python/
+/usr/share/doc/python3.6/examples/scripts/reindent.py --verbose --recurse python/
 autopep8 --verbose --in-place --recursive python/
