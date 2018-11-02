ee9b45e3452bf511fb768abcf389bbef6a39228f
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2018-11-02 11:08:14 +0100

    Perform BernsteinCopula test
     * Rewrote the test using "ot" instead of import *
     * Define a compute_error function that is called when estimating the
     copula using all criteria, such as if we want to refine error
     estimate, we only should update the function
     * Add copula property checking

--- python/test/t_BernsteinCopulaFactory_std.py
+++ python/test/t_BernsteinCopulaFactory_std.py
@@ -1,53 +1,68 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
-from openturns import *
+import openturns as ot
 
-TESTPREAMBLE()
-RandomGenerator.SetSeed(0)
+ot.TESTPREAMBLE()
+ot.RandomGenerator.SetSeed(0)
+
+def compute_max_error(ref_copula, est_copula):
+    """
+    Compute max error between ref_copula & estimated one
+    Error is evaluated in the set (u, v) where both belong to
+    {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}
+    """
+    max_error = 0.0
+    for n in range(11):
+        for p in range(11):
+            point = [0.1 * n, 0.1 * p]
+            max_error = max(max_error, abs(ref_copula.computeCDF(
+                point) - est_copula.computeCDF(point)))
+    return max_error
+
+def check_bernstein_copula(est_copula):
+    """
+    Check if an estimated distribution of kind EmpiricalBernstein
+    is of type copula and all marginals are also.
+    """
+    print("Is estimation a copula ? --> ", est_copula.isCopula())
+    print("Maginal checking")
+    dimension = est_copula.getDimension()
+    for d in range(dimension):
+        print("Is marginal %d a copula ? --> %s"%(d,est_copula.isCopula()))
+
 
 try:
-    coll = [GumbelCopula(3.0), ClaytonCopula(3.0), FrankCopula(3.0)]
+    coll = [ot.GumbelCopula(3.0), ot.ClaytonCopula(3.0), ot.FrankCopula(3.0)]
     size = 100
-    for i in range(len(coll)):
+    for i, ref_copula in enumerate(coll):
         ref_copula = coll[i]
         print("Reference copula", str(ref_copula))
         sample = ref_copula.getSample(size)
         # Default method: log-likelihood
-        m = BernsteinCopulaFactory.ComputeLogLikelihoodBinNumber(sample)
+        m = ot.BernsteinCopulaFactory.ComputeLogLikelihoodBinNumber(sample)
         print("Log-likelihood m=", m)
-        est_copula = BernsteinCopulaFactory().build(sample, m)
-        max_error = 0.0
-        for n in range(11):
-            for p in range(11):
-                point = [0.1 * n, 0.1 * p]
-                max_error = max(max_error, abs(ref_copula.computeCDF(
-                    point) - est_copula.computeCDF(point)))
+        est_copula = ot.BernsteinCopulaFactory().build(sample, m)
+        max_error = compute_max_error(ref_copula, est_copula)
         print("Max. error=%.5f" % max_error)
+        check_bernstein_copula(est_copula)
         # AMISE method
-        m = BernsteinCopulaFactory.ComputeAMISEBinNumber(sample)
+        m = ot.BernsteinCopulaFactory.ComputeAMISEBinNumber(sample)
         print("AMISE m=", m)
-        est_copula = BernsteinCopulaFactory().build(sample, m)
-        max_error = 0.0
-        for n in range(11):
-            for p in range(11):
-                point = [0.1 * n, 0.1 * p]
-                max_error = max(max_error, abs(ref_copula.computeCDF(
-                    point) - est_copula.computeCDF(point)))
+        est_copula = ot.BernsteinCopulaFactory().build(sample, m)
+        max_error = compute_max_error(ref_copula, est_copula)
         print("Max. error=%.5f" % max_error)
+        check_bernstein_copula(est_copula)
         # Penalized Csiszar divergence method
-        f = SymbolicFunction("t", "-log(t)")
-        m = BernsteinCopulaFactory.ComputePenalizedCsiszarDivergenceBinNumber(
+        f = ot.SymbolicFunction("t", "-log(t)")
+        m = ot.BernsteinCopulaFactory.ComputePenalizedCsiszarDivergenceBinNumber(
             sample, f)
         print("Penalized Csiszar divergence m=", m)
-        est_copula = BernsteinCopulaFactory().build(sample, m)
-        max_error = 0.0
-        for n in range(11):
-            for p in range(11):
-                point = [0.1 * n, 0.1 * p]
-                max_error = max(max_error, abs(ref_copula.computeCDF(
-                    point) - est_copula.computeCDF(point)))
+        est_copula = ot.BernsteinCopulaFactory().build(sample, m)
+        max_error = compute_max_error(ref_copula, est_copula)
         print("Max. error=%.5f" % max_error)
+        check_bernstein_copula(est_copula)
+        print("")
 
 except:
     import sys
