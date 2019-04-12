e21dde323dc8af3078d77740a466f36946134ef8
regislebrun <regis.lebrun@m4x.org>
2019-04-12 17:18:19 +0200

    Fixed a bug in LeastSquaresProblem
    The 1/2 factor was already taken into account by QuadraticFunction. BTW, I implemented a specific Function for half the squared L2 norm, much more compact and much more efficient than the generic QuadraticFunction. Many thanks to Sofiane for catching it!

--- lib/src/Base/Optim/LeastSquaresProblem.cxx
+++ lib/src/Base/Optim/LeastSquaresProblem.cxx
@@ -55,19 +55,220 @@ Function LeastSquaresProblem::getResidualFunction() const
   return residualFunction_;
 }
 
+namespace LeastSquaresProblemFunctions
+{
+class HalfSquaredNormEvaluation: public EvaluationImplementation
+{
+public:
+  HalfSquaredNormEvaluation(const UnsignedInteger dimension)
+    : EvaluationImplementation()
+    , dimension_(dimension)
+  {
+    // Nothing to do
+  }
+
+  HalfSquaredNormEvaluation * clone() const
+  {
+    return new HalfSquaredNormEvaluation(*this);
+  }
+
+  Point operator() (const Point & point) const
+  {
+    if (point.getDimension() != dimension_) throw InvalidArgumentException(HERE) << "Error: expected a point of dimension=" << dimension_ << ", got dimension=" << point.getDimension();
+    return Point(1, 0.5 * point.normSquare());
+  }
+
+  UnsignedInteger getInputDimension() const
+  {
+    return dimension_;
+  }
+
+  UnsignedInteger getOutputDimension() const
+  {
+    return 1;
+  }
+
+  Description getInputDescription() const
+  {
+    return Description::BuildDefault(dimension_, "x");
+  }
+
+  Description getOutputDescription() const
+  {
+    return Description(1, "y");
+  }
+
+  Description getDescription() const
+  {
+    Description description(getInputDescription());
+    description.add(getOutputDescription());
+    return description;
+  }
+
+  String __repr__() const
+  {
+    OSS oss;
+    oss << "HalfSquaredNormEvaluation(" << dimension_ << ")";
+    return oss;
+  }
+
+  String __str__(const String & ) const
+  {
+    OSS oss;
+    oss << "HalfSquaredNormEvaluation(" << dimension_ << ")";
+    return oss;
+  }
+
+private:
+  const UnsignedInteger dimension_;
+}; // class HalfSquaredNormEvaluation
+
+class HalfSquaredNormGradient: public GradientImplementation
+{
+public:
+  HalfSquaredNormGradient(const UnsignedInteger dimension)
+    : GradientImplementation()
+    , dimension_(dimension)
+  {
+    // Nothing to do
+  }
+
+  HalfSquaredNormGradient * clone() const
+  {
+    return new HalfSquaredNormGradient(*this);
+  }
+
+  Matrix gradient(const Point & point) const
+  {
+    if (point.getDimension() != dimension_) throw InvalidArgumentException(HERE) << "Error: expected a point of dimension=" << dimension_ << ", got dimension=" << point.getDimension();
+    return Matrix(dimension_, 1, point);
+  }
+
+  UnsignedInteger getInputDimension() const
+  {
+    return dimension_;
+  }
+
+  UnsignedInteger getOutputDimension() const
+  {
+    return 1;
+  }
+
+  Description getInputDescription() const
+  {
+    return Description::BuildDefault(dimension_, "x");
+  }
+
+  Description getOutputDescription() const
+  {
+    return Description(1, "y");
+  }
+
+  Description getDescription() const
+  {
+    Description description(getInputDescription());
+    description.add(getOutputDescription());
+    return description;
+  }
+
+  String __repr__() const
+  {
+    OSS oss;
+    oss << "HalfSquaredNormGradient(" << dimension_ << ")";
+    return oss;
+  }
+
+  String __str__(const String & ) const
+  {
+    OSS oss;
+    oss << "HalfSquaredNormGradient(" << dimension_ << ")";
+    return oss;
+  }
+
+private:
+  const UnsignedInteger dimension_;
+
+}; // class HalfSquaredNormGradient
+
+class HalfSquaredNormHessian: public HessianImplementation
+{
+public:
+  HalfSquaredNormHessian(const UnsignedInteger dimension)
+    : HessianImplementation()
+    , dimension_(dimension)
+  {
+    // Nothing to do
+  }
+
+  HalfSquaredNormHessian * clone() const
+  {
+    return new HalfSquaredNormHessian(*this);
+  }
+
+  SymmetricTensor hessian(const Point & point) const
+  {
+    if (point.getDimension() != dimension_) throw InvalidArgumentException(HERE) << "Error: expected a point of dimension=" << dimension_ << ", got dimension=" << point.getDimension();
+    SymmetricTensor hess(dimension_, 1);
+    for (UnsignedInteger i = 0; i < dimension_; ++i)
+      hess(i, i, 0) = 1.0;
+    return hess;
+  }
+
+  UnsignedInteger getInputDimension() const
+  {
+    return dimension_;
+  }
+
+  UnsignedInteger getOutputDimension() const
+  {
+    return 1;
+  }
+
+  Description getInputDescription() const
+  {
+    return Description::BuildDefault(dimension_, "x");
+  }
+
+  Description getOutputDescription() const
+  {
+    return Description(1, "y");
+  }
+
+  Description getDescription() const
+  {
+    Description description(getInputDescription());
+    description.add(getOutputDescription());
+    return description;
+  }
+
+  String __repr__() const
+  {
+    OSS oss;
+    oss << "HalfSquaredNormHessian(" << dimension_ << ")";
+    return oss;
+  }
+
+  String __str__(const String & ) const
+  {
+    OSS oss;
+    oss << "HalfSquaredNormHessian(" << dimension_ << ")";
+    return oss;
+  }
+
+private:
+  const UnsignedInteger dimension_;
+
+}; // class HalfSquaredNormHessian
+} // namespace LeastSquaresProblemFunctions
+
+
 void LeastSquaresProblem::setResidualFunction(const Function & residualFunction)
 {
   // r = ||(f)||^2/2
   const UnsignedInteger residualSize = residualFunction.getOutputDimension();
   residualFunction_ = residualFunction;
-  const Point center(residualSize);
-  const Point constant(1);
-  const Matrix linear(residualSize, 1);
-  SymmetricTensor quadratic(residualSize, 1);
-  for (UnsignedInteger i = 0; i < residualSize; ++ i)
-    quadratic(i, i, 0) = 0.5;
-  const QuadraticFunction sumQuad(center, constant, linear, quadratic);
-  objective_ = ComposedFunction(sumQuad, residualFunction);
+  const Function halfSquaredNorm(LeastSquaresProblemFunctions::HalfSquaredNormEvaluation(residualSize).clone(), LeastSquaresProblemFunctions::HalfSquaredNormGradient(residualSize).clone(), LeastSquaresProblemFunctions::HalfSquaredNormHessian(residualSize).clone());
+  objective_ = ComposedFunction(halfSquaredNorm, residualFunction);
   dimension_ = residualFunction.getInputDimension();
 }
 
