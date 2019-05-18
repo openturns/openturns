a6d0f1454834bf5173c0ca310f3e57d10db3fd4a
Joseph MURE <mure@math.univ-paris-diderot.fr>
2019-05-18 22:45:16 +0200

    fix python test by removing numpy import and rounding outputs to 3 decimals

--- python/test/t_DiscreteCompoundDistribution_std.py
+++ python/test/t_DiscreteCompoundDistribution_std.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 from openturns import *
-import numpy as np
+#import numpy as np
 
 TESTPREAMBLE()
 RandomGenerator.SetSeed(0)
@@ -15,16 +15,20 @@ try:
     upper_bound = distribution.computeIntegerUpperBound()
     print("Upper bound : {!r}".format(upper_bound))
 
-    for i in range(upper_bound):
-        print("Probability of {!r} = {!r}".format(i, distribution.computePDF(Point([i]))))
-
-    points = np.array(range(upper_bound)).reshape(upper_bound, 1)
-
-    pdf = distribution.computePDF(points)
-
+    # Compare with mathematically equal distribution
     poisson_distribution = Poisson(10.0)
-    poisson_pdf = poisson_distribution.computePDF(points)
+
+    for i in range(upper_bound):
+        print("")
+        print("Probability of {!r} = {:0.3f}".format(i, distribution.computePDF(Point([i]))))
+        print("Probability of {!r} with equivalent Poisson distribution = {:0.3f}".format(i, poisson_distribution.computePDF(Point([i]))))
+                                                                                          
+
+    #numpy necessary for the last lines
+    #points = np.array(range(upper_bound)).reshape(upper_bound, 1)
+    #pdf = distribution.computePDF(points)
+    #poisson_pdf = poisson_distribution.computePDF(points)
 
 except:
     import sys
-    print("t_Geometric.py", sys.exc_info()[0], sys.exc_info()[1])
+    print("t_DiscreteCompoundDistribution.py", sys.exc_info()[0], sys.exc_info()[1])
