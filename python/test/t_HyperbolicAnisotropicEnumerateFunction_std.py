6c2f3f3a56281dee3bf9f1f2038ee5197dc8caac
regislebrun <regis.lebrun@m4x.org>
2019-05-20 11:43:24 +0200

    Fixed a bug in HyperbolicAnisotropicEnumerateFunction
    The inverse() method segfault if the cache is empty.

--- python/test/t_HyperbolicAnisotropicEnumerateFunction_std.py
+++ python/test/t_HyperbolicAnisotropicEnumerateFunction_std.py
@@ -11,6 +11,14 @@ print('Default q :', ResourceMap.GetAsScalar(
 # check weight constructor
 f = HyperbolicAnisotropicEnumerateFunction((1., 2., 3., 4., 5.), 0.75)
 
+# check inverse when the cache is empty
+print("inverse([0,0,0,0,0])=", f.inverse([0,0,0,0,0]))
+# fill the cache
+for i in range(4):
+    res = f(i)
+# check inverse when the cache is filled
+print("inverse([2,0,0,0,0])=", f.inverse([2,0,0,0,0]))
+
 # verify consistency with LinearEnumerateFunction
 size = 10
 stratas = 5
