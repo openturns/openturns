992ff735706f69d97d330c892a8de190399459c3
Jonathan Feinberg <jonathf@gmail.com>
2019-07-31 11:32:31 +0200

    adding tests

--- python/test/t_Distribution_chaospy.py
+++ python/test/t_Distribution_chaospy.py
@@ -8,7 +8,8 @@ import numpy as np
 # A chaospy Triangle distribution
 d0 = cp.Triangle(2.0, 3.5, 4.0)
 d1 = cp.Kumaraswamy(2.0, 3.0, -1.0, 4.0)
-for chaospy_dist in [d0, d1]:
+d2 = cp.J(d0, d1)
+for chaospy_dist in [d0, d1, d2]:
     np.random.seed(42)
 
     # create an openturns distribution
@@ -25,9 +26,6 @@ for chaospy_dist in [d0, d1]:
     print('cdf= %.6g' % cdf)
     print('quantile=', distribution.computeQuantile(cdf))
     print('quantile (tail)=', distribution.computeQuantile(cdf, True))
-    print('scalar quantile=%.6g' % distribution.computeScalarQuantile(cdf))
-    print('scalar quantile (tail)=%.6g' %
-          distribution.computeScalarQuantile(cdf, True))
     print('mean=', distribution.getMean())
     print('mean(sampling)=', sample.computeMean())
     print('std=', distribution.getStandardDeviation())
@@ -37,3 +35,6 @@ for chaospy_dist in [d0, d1]:
     print('kurtosis=', distribution.getKurtosis())
     print('kurtosis(sampling)=', sample.computeKurtosis())
     print('range=', distribution.getRange())
+    print('scalar quantile=%.6g' % distribution.computeScalarQuantile(cdf))
+    print('scalar quantile (tail)=%.6g' %
+        distribution.computeScalarQuantile(cdf, True))
