ab657adfee4f30474152d292f073958e15c42cec
Julien Schueller <schueller@phimeca.com>
2019-12-05 09:49:27 +0100

    Script to locally run docker CI

--- .circleci/run_docker_linux.sh
+++ .circleci/run_docker_linux.sh
@@ -8,12 +8,20 @@ then
   export SOURCE_DATE_EPOCH=1514764800
 fi
 
+if test -d "/io"
+then
+  source_dir=/io  # local
+  cd /tmp
+else
+  source_dir=`pwd`  # circleci
+fi
+
 mkdir build && cd build
 
 cmake -DCMAKE_INSTALL_PREFIX=~/.local \
       -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
       -DCMAKE_CXX_FLAGS="-Wall -Wextra -Werror -D_GLIBCXX_ASSERTIONS" -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter -Wno-missing-field-initializers" \
-      ..
+      ${source_dir}
 make install
 ctest -R pyinstallcheck --output-on-failure --timeout 100 ${MAKEFLAGS}
 make tests
