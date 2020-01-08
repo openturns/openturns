4b778d511a4e12fcc4bff470b6af108009e44f3f
Julien Schueller <schueller@phimeca.com>
2020-01-08 16:54:35 +0100

    CircleCI: Run sphinx in parallel

--- .circleci/run_docker_linux.sh
+++ .circleci/run_docker_linux.sh
@@ -21,6 +21,7 @@ mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=~/.local \
       -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
       -DCMAKE_CXX_FLAGS="-Wall -Wextra -Werror -D_GLIBCXX_ASSERTIONS" -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter -Wno-missing-field-initializers" \
+      -DSPHINX_FLAGS="-W -T -j2" \
       ${source_dir}
 make install
 ctest -R pyinstallcheck --output-on-failure --timeout 100 ${MAKEFLAGS}
