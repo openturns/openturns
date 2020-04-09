eb0dfc681d41fc11dffda74641abc91f7c017f31
Julien Schueller <schueller@phimeca.com>
2020-04-09 18:29:13 +0200

    CMake: Work around eigen config bug

--- cmake/FindSpectra.cmake
+++ cmake/FindSpectra.cmake
@@ -24,7 +24,16 @@ find_path (SPECTRA_INCLUDE_DIR
   HINTS "${SPECTRA_ROOT_DIR}/include"
 )
 
-find_package(Eigen3 NO_MODULE)
+find_package (Eigen3 NO_MODULE)
+
+# workaround broken eigen3 cmake config
+if (EIGEN3_INCLUDE_DIRS AND NOT EXISTS "${EIGEN3_INCLUDE_DIRS}")
+  unset (EIGEN3_INCLUDE_DIRS)
+  find_path (EIGEN3_INCLUDE_DIRS NAMES signature_of_eigen3_matrix_library
+              PATH_SUFFIXES eigen3
+              HINTS ${EIGEN3_ROOT_DIR}/include)
+endif ()
+
 set (SPECTRA_INCLUDE_DIRS ${EIGEN3_INCLUDE_DIRS} ${SPECTRA_INCLUDE_DIR})
 
 include (FindPackageHandleStandardArgs)
