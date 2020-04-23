9eddbdd86654d8d348f726a8118f103fc8d48f45
Julien Schueller <schueller@phimeca.com>
2020-04-23 10:25:23 +0200

    PythonEvaluation: Fix leaks
    - wrong increfs
    - buffer args leak
    
    Closes #1448

--- python/src/PythonEvaluation.cxx
+++ python/src/PythonEvaluation.cxx
@@ -108,8 +108,6 @@ PythonEvaluation::PythonEvaluation(PyObject * pyCallable)
 /* Virtual constructor */
 PythonEvaluation * PythonEvaluation::clone() const
 {
-  Py_XINCREF(pyObj_);
-  Py_XINCREF(pyBufferClass_);
   return new PythonEvaluation(*this);
 }
 
@@ -226,7 +224,8 @@ Point PythonEvaluation::operator() (const Point & inP) const
     PyTuple_SetItem(shapeTuple.get(), 0, convert< UnsignedInteger, _PyInt_ > (inP.getSize()));
 
     // Call openturns.memoryview.Buffer() to create a read-only buffer
-    ScopedPyObjectPointer readOnlyBufferObj(PyObject_CallObject(pyBufferClass_, Py_BuildValue("OO", ptrTuple.get(), shapeTuple.get())));
+    ScopedPyObjectPointer bufferArgs(Py_BuildValue("OO", ptrTuple.get(), shapeTuple.get()));
+    ScopedPyObjectPointer readOnlyBufferObj(PyObject_CallObject(pyBufferClass_, bufferArgs.get()));
 
     // Pass this buffer to _exec function if it has been defined by user, otherwise call _exec_sample(Buffer.augment())[0]
     // If both pyObj_has_exec_ and pyObj_has_exec_sample_ are false, this is not a PythonFunction but a Function(OpenTURNSPythonFunction).
@@ -328,7 +327,8 @@ Sample PythonEvaluation::operator() (const Sample & inS) const
       PyTuple_SetItem(shapeTuple.get(), 1, convert< UnsignedInteger, _PyInt_ > (inDim));
 
       // Call openturns.memoryview.Buffer() to create a read-only buffer
-      ScopedPyObjectPointer readOnlyBufferObj(PyObject_CallObject(pyBufferClass_, Py_BuildValue("OO", ptrTuple.get(), shapeTuple.get())));
+      ScopedPyObjectPointer bufferArgs(Py_BuildValue("OO", ptrTuple.get(), shapeTuple.get()));
+      ScopedPyObjectPointer readOnlyBufferObj(PyObject_CallObject(pyBufferClass_, bufferArgs.get()));
 
       // Pass this buffer to _exec_sample function if it has been defined by user, otherwise loop on Buffer on call _exec
       // If both pyObj_has_exec_ and pyObj_has_exec_sample_ are false, this is not a PythonFunction but a Function(OpenTURNSPythonFunction).
