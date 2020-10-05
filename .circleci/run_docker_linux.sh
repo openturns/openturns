51b2f34b91166161f1502dbe69502a2f29b3aa24
Julien Schueller <schueller@phimeca.com>
2020-10-05 09:30:04 +0200

    Debug sphinx parallel in CircleCI

--- .circleci/run_docker_linux.sh
+++ .circleci/run_docker_linux.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -xe
+sudo pacman -S strace --noconfirm
 
 # build with frozen date unless on release for reproducible builds
 if test "${CIRCLE_BRANCH}" = "master"
@@ -21,8 +22,9 @@ mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=~/.local \
       -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
       -DCMAKE_CXX_FLAGS="-Wall -Wextra -Werror -D_GLIBCXX_ASSERTIONS" -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter -Wno-missing-field-initializers" \
+      -DSPHINX_FLAGS="-W -T -j8" \
       ${source_dir}
-make install
+strace make install
 ctest -R pyinstallcheck --output-on-failure --timeout 100 ${MAKEFLAGS}
 make tests
 ctest -R cppcheck --output-on-failure --timeout 100 ${MAKEFLAGS}
