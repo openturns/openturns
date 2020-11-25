7e839ff97f10adbc6dc7a4ab2b4620b2fd5bdd2d
Julien Schueller <schueller@phimeca.com>
2020-11-25 17:07:54 +0100

    CMake: Workaround cmake 3.19.1 FPHSA bug

--- cmake/FindPythonModule.cmake
+++ cmake/FindPythonModule.cmake
@@ -69,6 +69,10 @@ macro (find_python_module module)
       endif ()
     endif ()
 
+    # https://gitlab.kitware.com/cmake/cmake/-/issues/21505
+    if (CMAKE_VERSION VERSION_EQUAL 3.19.1)
+      set (CMAKE_FIND_PACKAGE_NAME "${module}")
+    endif ()
     find_package_handle_standard_args (${module} REQUIRED_VARS ${module_upper}_LOCATION
                                         ${module}_FIND_OPTIONAL
                                         _${module_upper}_VERSION_MATCH
