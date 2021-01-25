dfe746f5d9838b67c2d21827781e4fbb03a30c27
chuvanmai <chu.mai@edf.fr>
2021-01-25 12:11:19 +0100

    correction LHSExperiment.py

--- python/doc/pyplots/LHSExperiment.py
+++ python/doc/pyplots/LHSExperiment.py
@@ -1,25 +1,14 @@
 import openturns as ot
 from matplotlib import pyplot as plt
-from openturns.viewer import View
+import openturns.viewer as otv
 ot.RandomGenerator.SetSeed(0)
 
-# Generate sample with the given plane
-distribution = ot.ComposedDistribution([ot.Uniform(0, 1)] * 2)
-size = 10
-experiment = ot.LHSExperiment(distribution, size)
-
+distribution = ot.ComposedDistribution([ot.Uniform()]*3)
+samplesize = 5
+experiment = ot.LHSExperiment(distribution, samplesize, False, False)
 sample = experiment.generate()
 
-# Create an empty graph
-graph = ot.Graph("LHS experiment", "x1", "x2", True, "")
+bounds = distribution.getRange()
 
-# Create the cloud
-cloud = ot.Cloud(sample, "blue", "fsquare", "")
+otv.PlotDesign(sample, bounds)
 
-# Then, draw it
-graph.add(cloud)
-
-fig = plt.figure(figsize=(4, 4))
-axis = fig.add_subplot(111)
-axis.set_xlim(auto=True)
-View(graph, figure=fig, axes=[axis], add_legend=False)
