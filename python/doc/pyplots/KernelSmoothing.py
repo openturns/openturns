23d987dcfac85074d1a82946fa1ce12b987535e8
A74579 <vanessa.verges@edf.fr>
2021-03-29 16:09:39 +0200

    KernelSmoothing API doc: add examples and image
    Closes #1287

---
+++ python/doc/pyplots/KernelSmoothing.py
@@ -0,0 +1,14 @@
+import openturns as ot
+from openturns.viewer import View
+
+sample = ot.Gamma(6.0, 1.0).getSample(100)
+ks = ot.KernelSmoothing()
+bandwidth = [0.9]
+fittedDist = ks.build(sample, bandwidth)
+
+graph = fittedDist.drawPDF()
+graph.add( ot.Gamma(6.0, 1.0).drawPDF())
+graph.setColors(ot.Drawable.BuildDefaultPalette(2))
+graph.setLegends(['KS dist', 'Gamma'])
+View(graph)
+#View(graph, figure_kw={'figsize': (8, 4)})
