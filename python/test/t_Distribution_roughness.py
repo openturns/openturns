bf97e837d5cb8098aa9d6bc8bcc530f57a30c7f1
Julien Schueller <schueller@phimeca.com>
2021-09-27 14:26:50 +0200

    Distribution: Fix roughness test division

--- python/test/t_Distribution_roughness.py
+++ python/test/t_Distribution_roughness.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-from __future__ import print_function
+from __future__ import print_function, division
 import openturns as ot
 import math as m
 import openturns.testing as ott
