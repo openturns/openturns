082ae2ce3c8edf12cf2205a96e9b64e160a3db6c
Julien Schueller <schueller@phimeca.com>
2021-12-07 08:40:37 +0100

    Point: Add range ctor

--- lib/src/Base/Type/openturns/Collection.hxx
+++ lib/src/Base/Type/openturns/Collection.hxx
@@ -146,8 +146,8 @@ public:
 
   /** Constructor from a range of elements */
   template <typename InputIterator>
-  Collection(const InputIterator first,
-             const InputIterator last)
+  Collection(InputIterator first,
+             InputIterator last)
     : coll__(first, last)
   {
     // Nothing to do
