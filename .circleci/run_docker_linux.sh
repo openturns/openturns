27848d79cbdcca6f49dbd97d4962f83d6c39c400
Julien Schueller <schueller@phimeca.com>
2022-02-03 10:27:39 +0100

    CI: Update sphinx timestamp

--- .circleci/run_docker_linux.sh
+++ .circleci/run_docker_linux.sh
@@ -10,7 +10,7 @@ env
 # build with frozen date unless on release for reproducible builds
 if test "${CIRCLE_BRANCH}" = "master"
 then
-  export SOURCE_DATE_EPOCH=1514764800
+  export SOURCE_DATE_EPOCH=1640991600
 fi
 
 if test -d "/io"
