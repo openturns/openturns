6517dd627464b1012b88af326817476296af4f1b
vchabri <60410196+vchabri@users.noreply.github.com>
2022-02-18 08:53:22 +0100

    Doc: Fix math formatting

--- python/doc/examples/meta_modeling/general_purpose_metamodels/plot_taylor_approximation.py
+++ python/doc/examples/meta_modeling/general_purpose_metamodels/plot_taylor_approximation.py
@@ -3,17 +3,17 @@ Taylor approximations
 =====================
 """
 # %%
-# In this example we are going to build a local approximation of a model using the taylor decomposition:
+# In this example we are going to build a local approximation of a model using the Taylor decomposition:
 #
 # Here is the decomposition at the first order:
 #
 # .. math::
-#    \underline{y} \, \approx \, \widehat{h}(\underline{x}) \,
-#          = \, h(\underline{x}_0) \, +
-#         \, \sum_{i=1}^{n_{X}} \; \frac{\partial h}{\partial x_i}(\underline{x}_0).\left(x_i - x_{0,i} \right)
+#    \vect{y} \, \approx \, \widehat{h}(\vect{x}) \,
+#          = \, h(\vect{x}_0) \, +
+#         \, \sum_{i=1}^{n_{X}} \; \frac{\partial h}{\partial x_i}(\vect{x}_0).\left(x_i - x_{0,i} \right)
 #
 #
-# Here :math:`h(x) = [cos(x_1 + x_2), (x2 + 1)* e^{x_1 - 2* x_2}]`.
+# Here :math:`h(x) = [cos(x_1 + x_2), (x_2 + 1) e^{x_1 - 2 x_2}]`.
 #
 
 # %%
@@ -35,13 +35,13 @@ a = -0.4
 b = 0.0
 
 # %%
-# create a linear (first order) Taylor approximation
+# Create a linear (first order) Taylor approximation.
 algo = ot.LinearTaylor(x0, model)
 algo.run()
 responseSurface = algo.getMetaModel()
 
 # %%
-# plot 2nd output of our model with x1=x0_1
+# Plot 2nd output of our model with x1=x0_1.
 graph = ot.ParametricFunction(
     responseSurface, [0], [x0[1]]).getMarginal(1).draw(a, b)
 graph.setLegends(['taylor'])
@@ -56,20 +56,20 @@ view = viewer.View(graph)
 # %%
 # Here is the decomposition at the second order:
 #
-# .. math::\underline{y} \, \approx \, \widehat{h}(\underline{x}) \, = \,
-#         h(\underline{x}_0) \, + \, \sum_{i=1}^{n_{X}} \;
-#       \frac{\partial h}{\partial x_i}(\underline{x}_0).\left(x_i - x_{0,i} \right) \, +
+# .. math::\vect{y} \, \approx \, \widehat{h}(\vect{x}) \, = \,
+#         h(\vect{x}_0) \, + \, \sum_{i=1}^{n_{X}} \;
+#       \frac{\partial h}{\partial x_i}(\vect{x}_0).\left(x_i - x_{0,i} \right) \, +
 #      \, \frac{1}{2} \; \sum_{i,j=1}^{n_X} \;
-#       \frac{\partial^2 h}{\partial x_i \partial x_j}(\underline{x}_0).\left(x_i - x_{0,i} \right).\left(x_j - x_{0,j} \right):math:``
+#       \frac{\partial^2 h}{\partial x_i \partial x_j}(\vect{x}_0).\left(x_i - x_{0,i} \right).\left(x_j - x_{0,j} \right):math:``
 
 # %%
-# create a quadratic (2nd order) Taylor approximation
+# Create a quadratic (second order) Taylor approximation.
 algo = ot.QuadraticTaylor(x0, model)
 algo.run()
 responseSurface = algo.getMetaModel()
 
 # %%
-# plot 2nd output of our model with x1=x0_1
+# Plot second output of our model with x1=x0_1.
 graph = ot.ParametricFunction(
     responseSurface, [0], [x0[1]]).getMarginal(1).draw(a, b)
 graph.setLegends(['taylor'])
