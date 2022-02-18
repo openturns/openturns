9adf87d2687d5dc956214bb66326dac0688d69b8
vchabri <60410196+vchabri@users.noreply.github.com>
2022-02-18 12:35:21 +0100

    Doc: Fixes typos in plot_taylor_approximation.py

--- python/doc/examples/meta_modeling/general_purpose_metamodels/plot_taylor_approximation.py
+++ python/doc/examples/meta_modeling/general_purpose_metamodels/plot_taylor_approximation.py
@@ -23,25 +23,25 @@ import openturns.viewer as viewer
 from matplotlib import pylab as plt
 ot.Log.Show(ot.Log.NONE)
 
-# prepare some data
+# Prepare some data.
 formulas = ['cos(x1 + x2)', '(x2 + 1) * exp(x1 - 2 * x2)']
 model = ot.SymbolicFunction(['x1', 'x2'], formulas)
 
-# center of the approximation
+# Center of the approximation.
 x0 = [-0.4, -0.4]
 
-# drawing bounds
+# Drawing bounds.
 a = -0.4
 b = 0.0
 
 # %%
-# Create a linear (first order) Taylor approximation.
+# Create a linear (first-order) Taylor approximation.
 algo = ot.LinearTaylor(x0, model)
 algo.run()
 responseSurface = algo.getMetaModel()
 
 # %%
-# Plot 2nd output of our model with x1=x0_1.
+# Plot the second output of our model with :math:`x_1=x_{0,1}`.
 graph = ot.ParametricFunction(
     responseSurface, [0], [x0[1]]).getMarginal(1).draw(a, b)
 graph.setLegends(['taylor'])
@@ -63,13 +63,13 @@ view = viewer.View(graph)
 #       \frac{\partial^2 h}{\partial x_i \partial x_j}(\vect{x}_0).\left(x_i - x_{0,i} \right).\left(x_j - x_{0,j} \right):math:``
 
 # %%
-# Create a quadratic (second order) Taylor approximation.
+# Create a quadratic (second-order) Taylor approximation.
 algo = ot.QuadraticTaylor(x0, model)
 algo.run()
 responseSurface = algo.getMetaModel()
 
 # %%
-# Plot second output of our model with x1=x0_1.
+# Plot second output of our model with :math:`x_1=x_{0,1}`.
 graph = ot.ParametricFunction(
     responseSurface, [0], [x0[1]]).getMarginal(1).draw(a, b)
 graph.setLegends(['taylor'])
