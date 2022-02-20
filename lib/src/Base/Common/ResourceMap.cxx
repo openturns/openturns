e64f778de545eb5280b80ba4cd1e744f5c8bb33b
Michael BAUDIN <michael.baudin@gmail.com>
2022-02-20 08:45:43 +0100

    Update the ResourceMap key.

--- lib/src/Base/Common/ResourceMap.cxx
+++ lib/src/Base/Common/ResourceMap.cxx
@@ -1291,7 +1291,7 @@ void ResourceMap::loadDefaultConfiguration()
   addAsUnsignedInteger("WeightedExperiment-DefaultSize", 100);
 
   // GaussProductExperiment parameters //
-  addAsUnsignedInteger("GaussProductExperiment-DefaultMarginalDegree", 5);
+  addAsUnsignedInteger("GaussProductExperiment-DefaultMarginalSize", 5);
 
   // HyperbolicAnisotropicEnumerateFunction parameters //
   addAsScalar("HyperbolicAnisotropicEnumerateFunction-DefaultQ", 0.4);
