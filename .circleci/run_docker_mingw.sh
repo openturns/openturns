cd48a315b4a79433dffc227e302229c0ce68e15f
Julien Schueller <schueller@phimeca.com>
2022-03-02 18:05:41 +0100

    CI: Drop -Wno-missing-field-initializers

--- .circleci/run_docker_mingw.sh
+++ .circleci/run_docker_mingw.sh
@@ -17,7 +17,7 @@ MINGW_PREFIX=/usr/${ARCH}-w64-mingw32
 PYMAJMIN=310
 PREFIX=${PWD}/install
 CXXFLAGS="-Wall -Wextra -Werror -D_GLIBCXX_ASSERTIONS" ${ARCH}-w64-mingw32-cmake \
-  -DSWIG_COMPILE_FLAGS="-O0 -Wno-unused-parameter -Wno-missing-field-initializers" \
+  -DSWIG_COMPILE_FLAGS="-O0 -Wno-unused-parameter" \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
   -DPYTHON_INCLUDE_DIR=${MINGW_PREFIX}/include/python${PYMAJMIN} \
   -DPYTHON_LIBRARY=${MINGW_PREFIX}/lib/libpython${PYMAJMIN}.dll.a \
