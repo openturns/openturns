ef8b49022a2253d6f9ef23699aa63c4f6f794dbf
Julien Floquet <julien.floquet@protonmail.com>
2022-03-04 09:18:15 +0100

    update test C++ for MACOS build

--- lib/test/t_NAIS_std.cxx
+++ lib/test/t_NAIS_std.cxx
@@ -21,7 +21,7 @@ int main()
   naisResult.setProbabilityEstimate(0.67);
   std::cout << naisResult.getProbabilityEstimate() << std::endl;
 
-  String formula = "min(3+(0.1*pow(a-b,2))-((a+b)/(sqrt(2))),3+(0.1*pow(a-b,2))+((a+b)/(sqrt(2))),(a-b)+(c/ (sqrt(2))),(b-a)+(c/(sqrt(2))))";
+  const String formula = "min(3+(0.1*pow(a-b,2))-((a+b)/(sqrt(2))),3+(0.1*pow(a-b,2))+((a+b)/(sqrt(2))),(a-b)+(c/ (sqrt(2))),(b-a)+(c/(sqrt(2))))";
   
   // Definition de la pythonfunction generale
   Description input(3);
@@ -29,18 +29,18 @@ int main()
   input[1] = "b";
   input[2] = "c";
 
-  Function myfourBranch = SymbolicFunction(input, Description(1, formula));
+  const Function myfourBranch = SymbolicFunction(input, Description(1, formula));
 
   // Transformation de la pythonfunction vers parametricfunction en figeant le parametre k
-  Indices indexFrozen = Indices(1,2);
-  Point identifPoint(1,7);
+  const Indices indexFrozen = Indices(1,2);
+  const Point identifPoint(1,7);
   ParametricFunction myfourBranchUpdate(myfourBranch, indexFrozen, identifPoint);
 
   // Definition of input variable PDF
-  Point mean(2,(0.0, 0.0)) ;
-  Point sigma(2,(1.0, 1.0)) ;
-  Normal distX(mean, sigma, CorrelationMatrix(2));
-  RandomVector inputVector = RandomVector(distX);
+  const Point mean(2,0.0) ;
+  const Point sigma(2,1.0) ;
+  const Normal distX(mean, sigma, CorrelationMatrix(2));
+  const RandomVector inputVector = RandomVector(distX);
 
   // Determination of reference probability
   
@@ -50,28 +50,28 @@ int main()
   // Creation of event
   RandomGenerator::SetSeed(1);
 
-  RandomVector vect = RandomVector(distX);
-  CompositeRandomVector compositeVector = CompositeRandomVector(myfourBranchUpdate, vect);
-  ThresholdEvent event = ThresholdEvent(compositeVector, Less(), 0.0);
+  const RandomVector vect = RandomVector(distX);
+  const CompositeRandomVector compositeVector = CompositeRandomVector(myfourBranchUpdate, vect);
+  const ThresholdEvent event = ThresholdEvent(compositeVector, Less(), 0.0);
 
   // Create a Monte Carlo algorithm
-  MonteCarloExperiment experiment = MonteCarloExperiment();
+  const MonteCarloExperiment experiment = MonteCarloExperiment();
   ProbabilitySimulationAlgorithm algo = ProbabilitySimulationAlgorithm(event, experiment);
   algo.setMaximumOuterSampling(numberOfMonteCarlo);
   algo.run();
 
   // Retrieve results
-  ProbabilitySimulationResult result = algo.getResult();
-  Scalar probability = result.getProbabilityEstimate();
+  const ProbabilitySimulationResult result = algo.getResult();
+  const Scalar probability = result.getProbabilityEstimate();
   std::cout << "Pf= " << probability << std::endl;
 	
   // Hyperparameters of the algorithm
   
   // Number of samples at each iteration
-  int numberSamples= 10 ;
+  const int numberSamples= 10 ;
   
   // Quantile determining the percentage of failure samples in the current population
-  float rhoQuantile = 0.25 ;
+  const float rhoQuantile = 0.25 ;
 
   // Definition of the algoritm
   NAIS NAIS_algo = NAIS(event,numberSamples,rhoQuantile);
@@ -79,7 +79,7 @@ int main()
   // Run of the algorithm
   NAIS_algo.run();
 
-  NAISResult NAIS_result = NAIS_algo.getResult();
+  const NAISResult NAIS_result = NAIS_algo.getResult();
   std::cout << "Probability of failure: " << NAIS_result.getProbabilityEstimate()<< std::endl;
   std::cout << "Sample: " << NAIS_result.getSample()<< std::endl;
 
