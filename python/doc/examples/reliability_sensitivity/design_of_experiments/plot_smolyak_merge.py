d367fd99a14bdda8cd2d99699357504dd4911559
Michael BAUDIN <michael.baudin@gmail.com>
2022-04-26 23:03:19 +0200

    Fixed doc.

--- python/doc/examples/reliability_sensitivity/design_of_experiments/plot_smolyak_merge.py
+++ python/doc/examples/reliability_sensitivity/design_of_experiments/plot_smolyak_merge.py
@@ -94,7 +94,6 @@ for epsilon in epsilon_array:
     level_list = list(range(1, 10))
     for level in level_list:
         size = computeNumberOfSmolyakNodes(level, epsilon, epsilon)
-        print("epsilon = %.2e, level = %d, size = %d" % (epsilon, level, size))
         size_list.append(size)
     cloud = ot.Cloud(level_list, size_list)
     cloud.setLegend("$\epsilon = %.2e$" % (epsilon))
@@ -116,14 +115,13 @@ otv.View(graph, figure_kw={"figsize": (4.0, 3.0)},
 # nodes are close to each other, so that these nodes are merged. 
 # In this case, the quadrature is not accurate enough. 
 
-#
+# %%
 # In the next `for` loop, for each value of the `level` parameter, 
 # we decrease the tolerance from :math:`10^{-1}` 
 # down to :math:`10^{-5}` and see how this changes the number of nodes. 
  
 graph = ot.Graph("Sensitivity to tolerance", "Epsilon", "Size", True)
 point_styles = ["circle", "fdiamond", "fsquare", "ftriangleup", "triangledown"]
-number_of_levels = 5
 number_of_epsilons = 12
 level_list = list(range(1, 10, 2))
 index = 0
@@ -132,7 +130,6 @@ for level in level_list:
     epsilon_array = np.logspace(0, -5, number_of_epsilons)
     for epsilon in epsilon_array:
         size = computeNumberOfSmolyakNodes(level, epsilon, epsilon)
-        print("level = %d, epsilon = %.2e, size = %d" % (level, epsilon, size))
         size_list.append(size)
     cloud = ot.Cloud(epsilon_array, size_list)
     cloud.setLegend("Level = %d" % (level))
@@ -150,3 +147,35 @@ otv.View(graph, figure_kw={"figsize": (4.0, 3.0)},
 # We see that changing the tolerance from :math:`1` down to :math:`10^{-3}` 
 # changes the number of nodes. 
 # Below this threshold, the number of nodes does not change much. 
+
+# %%
+# The `SmolyakExperiment-MergeQuadrature` key of the `ResourceMap` allows 
+# to disable or enable the merge algorithm. 
+# In order to see its effect, the following script performs a loop over 
+# the levels and compute the size without and with the merge algorithm. 
+# Then we plot the number of nodes which have been detected as duplicated 
+# and plot it versus the level.
+
+# Restore to default values
+ot.ResourceMap.SetAsScalar("SmolyakExperiment-DefaultPointRelativeEpsilon", 1.e-8)
+ot.ResourceMap.SetAsScalar("SmolyakExperiment-DefaultPointAbsoluteEpsilon", 1.e-8)
+level_list = list(range(1, 10))
+size_decrease_list = []
+for level in level_list:
+    ot.ResourceMap.SetAsBool("SmolyakExperiment-MergeQuadrature", False)
+    sizeNoMerge = computeNumberOfSmolyakNodes(level, epsilon, epsilon)
+    ot.ResourceMap.SetAsBool("SmolyakExperiment-MergeQuadrature", True)
+    sizeWithMerge = computeNumberOfSmolyakNodes(level, epsilon, epsilon)
+    sizeDecrease = sizeNoMerge - sizeWithMerge
+    size_decrease_list.append(sizeDecrease)
+
+graph = ot.Graph("Sensitivity to merge", "Level", "Size decrease", True)
+cloud = ot.Cloud(level_list, size_decrease_list)
+graph.add(cloud)
+otv.View(graph, figure_kw={"figsize": (4.0, 3.0)}, 
+         legend_kw={"bbox_to_anchor":(1.0, 1.0), "loc":"upper left"})
+
+# We see that the number of nodes is reduced when the merge algorithm is 
+# enabled. 
+# Moreover, the number of nodes which are duplicated increases when the 
+# level increases.
