7503a43d6adf6b2a791e7e371a04e53b47dc6dcc
pab-pereira <111493202+pab-pereira@users.noreply.github.com>
2022-08-18 11:18:30 +0200

    Spelling corrections on import/export samples

--- python/doc/examples/data_analysis/manage_data_and_samples/plot_import_export_sample_csv.py
+++ python/doc/examples/data_analysis/manage_data_and_samples/plot_import_export_sample_csv.py
@@ -10,22 +10,23 @@ import openturns as ot
 ot.Log.Show(ot.Log.NONE)
 
 # %%
-# create a sample from a 2-d gaussian distribution
+# Create a sample from a 2-d gaussian distribution.
 sample = ot.Normal(2).getSample(5)
 sample.setDescription(['u1', 'u2'])
 
 # %%
-# write a CSV file
-# warning the default separator is ';'
+# Write a CSV file.
+#
+# Warning: the default separator is ';'.
 sample.exportToCSVFile('sample.csv', ',')
 
 # %%
-# print the content of the written file
+# Print the content of the written file.
 with open('sample.csv', 'r') as f:
     for line in f.readlines():
         print(line, end='')
 
 # %%
-# read the previous CSV file
+# Read the previous CSV file.
 sample = ot.Sample.ImportFromCSVFile('sample.csv', ',')
 print(sample)
