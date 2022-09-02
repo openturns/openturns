9c637a84706a719a21d3b9499dee4e632be9332e
Mathieu Balesdent <mathieubalesdent@gmail.com>
2022-09-02 12:25:15 +0200

    Removal of ot.TESTPREABLE() from test

--- python/test/t_wingweight_function.py
+++ python/test/t_wingweight_function.py
@@ -4,9 +4,6 @@ import openturns as ot
 from openturns.testing import assert_almost_equal
 from openturns.usecases import wingweight_function
 
-ot.TESTPREAMBLE()
-ot.PlatformInfo.SetNumericalPrecision(5)
-
 """
 Test the WingWeightModel data class on a reference point.
 """
