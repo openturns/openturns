1dbdc4daca866c983f0234325f158f6c57fef02b
Julien Schueller <schueller@phimeca.com>
2022-12-07 12:02:12 +0100

    Pagmo: Fix fronts computation

--- python/doc/examples/numerical_methods/optimization/plot_optimization_pagmo.py
+++ python/doc/examples/numerical_methods/optimization/plot_optimization_pagmo.py
@@ -3,7 +3,7 @@ Multi-objective optimization using Pagmo
 ========================================
 """
 # %%
-# In this example we are going to explore optimization using the `Pagmo <https://esa.github.io/pagmo2/>`_ solver.
+# In this example we are going to explore optimization using the :class:`~openturns.Pagmo` solver.
 
 # %%
 import openturns as ot
@@ -18,9 +18,6 @@ for algo in ot.Pagmo.GetAlgorithmNames():
     print(algo)
 
 # %%
-# More details on Pagmo algorithms are available `here <https://esa.github.io/pagmo2/overview.html#list-of-algorithms>`_ .
-
-# %%
 # Create the problem, from ZDT test suite
 f = ot.SymbolicFunction(
     ["x1", "x2"], ["x1", "var g := 1.0 + 9.0 * (x1 + x2); g * (1.0 - sqrt(x1 / g))"]
@@ -33,9 +30,9 @@ zdt1.setBounds(ot.Interval([0.0] * 2, [1.0] * 2))
 pop0 = ot.ComposedDistribution([ot.Uniform(0.0, 1.0)] * 2).getSample(100)
 
 # %%
-# We create the algorithm that should evolve over 180 generations
+# We create the algorithm that should evolve over 10 generations
 algo = ot.Pagmo(zdt1, "nsga2", pop0)
-algo.setGenerationNumber(180)
+algo.setGenerationNumber(10)
 
 # %%
 # Benefit from parallel evaluations if the function allows it
