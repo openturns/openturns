0b66e1a7f926104bac37eb0e1e1dd92ca9d7dac6
Julien Schueller <schueller@phimeca.com>
2023-01-24 15:51:42 +0100

    GLM: Tweak invalid bound

--- lib/src/Uncertainty/Algorithm/MetaModel/Kriging/GeneralLinearModelAlgorithm.cxx
+++ lib/src/Uncertainty/Algorithm/MetaModel/Kriging/GeneralLinearModelAlgorithm.cxx
@@ -209,7 +209,14 @@ void GeneralLinearModelAlgorithm::setCovarianceModel(const CovarianceModel & cov
     if (isScaleActive)
     {
       const Point inputSampleRange(inputSample_.getMax() - inputSample_.getMin());
-      for (UnsignedInteger k = 0; k < reducedCovarianceModel_.getScale().getSize(); ++k) upperBound[k] = inputSampleRange[k] * scaleFactor;
+      for (UnsignedInteger k = 0; k < reducedCovarianceModel_.getScale().getSize(); ++k)
+      {
+	upperBound[k] = inputSampleRange[k] * scaleFactor;
+	if (upperBound[k] < lowerBound[k])
+	{
+	  upperBound[k] += lowerBound[k];
+	}
+      }
     }
     LOGWARN(OSS() <<  "Warning! For coherency we set scale upper bounds = " << upperBound.__str__());
 
