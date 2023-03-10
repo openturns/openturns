2aecd105ff164615341fd23888f5a7d1b643e4b0
Julien Schueller <schueller@phimeca.com>
2023-03-10 08:59:00 +0100

    CI: Reduce testing concurrency on aarch64

--- .ci_support/run_docker_aarch64.sh
+++ .ci_support/run_docker_aarch64.sh
@@ -8,4 +8,4 @@ aarch64-cmake -DCMAKE_INSTALL_PREFIX=~/.local \
   -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
   -DPython_EXECUTABLE=/usr/bin/aarch64-python -DSWIG_COMPILE_FLAGS="-O1" /io
 make install
-OMP_NUM_THREADS=1 ctest -R pyinstall --output-on-failure --timeout 1000 -j2 --schedule-random
+OMP_NUM_THREADS=1 ctest -R pyinstall --output-on-failure --timeout 1000 ${MAKEFLAGS} --schedule-random
