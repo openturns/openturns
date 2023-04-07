c3c9cb4ba7e2ec08bb2925d2fbb0b0ad05e00dbf
Mathieu Balesdent <mathieubalesdent@gmail.com>
2023-04-07 13:50:15 +0200

    modification of example #2

--- python/doc/examples/reliability_sensitivity/reliability/plot_crossentropy.py
+++ python/doc/examples/reliability_sensitivity/reliability/plot_crossentropy.py
@@ -40,7 +40,7 @@ g = ot.SymbolicFunction(['R', 'F'], ['R-F/(pi_*1e-4)'])
 
 # %%
 graph = ot.Graph("Simple stress beam", "R", "F", True, "topright")
-drawfunction = g.draw([1.8e6,600], [4e6,950.], [100] * 2)
+drawfunction = g.draw([1.8e6, 600], [4e6, 950.], [100] * 2)
 graph.add(drawfunction)
 view = viewer.View(graph)
 
@@ -64,7 +64,7 @@ event = ot.ThresholdEvent(Y, ot.Less(), 0.0)
 # --------------------------------------------------------------
 
 # %%
-myStandardSpaceIS = ot.StandardSpaceCrossEntropyImportanceSampling(event,0.35)
+myStandardSpaceIS = ot.StandardSpaceCrossEntropyImportanceSampling(event, 0.35)
 
 # %%
 # Now we can run the algorithm and get the results.
@@ -99,7 +99,7 @@ print(
 # We can analyze the simulation budget.
 
 # %%
-print('Numerical budget:',myStandardSpaceISResult.getBlockSize()*myStandardSpaceISResult.getOuterSampling())
+print('Numerical budget:', myStandardSpaceISResult.getBlockSize()*myStandardSpaceISResult.getOuterSampling())
 
 # %%
 # We can also retrieve the optimal auxiliary distribution in Standard Space.
@@ -136,7 +136,9 @@ view = viewer.View(myGraph)
 # ----------------------------------------------------------------
 
 # %%
-# For more advanced use, it is possible to work in the physical space to specify the auxiliary distribution. In this second example, we take the auxiliary distribution in the same family as the initial distribution and we want to optimize all the parameters. The parameters to be optimized are the parameters of the native distribution.
+# For more advanced use, it is possible to work in the physical space to specify the auxiliary distribution. 
+# In this second example, we take the auxiliary distribution in the same family as the initial distribution and we want to optimize all the parameters. 
+# The parameters to be optimized are the parameters of the native distribution.
 
 # %%
 ot.RandomGenerator.SetSeed(0)
@@ -145,16 +147,16 @@ distribution_margin2 = ot.Normal()
 aux_marginals = [distribution_margin1, distribution_margin2]
 aux_distribution = ot.ComposedDistribution(aux_marginals)
 # Definition of parameters to be optimized
-active_parameters = [0,1,2,3,4] # active parameters from the auxiliary distribution which will be optimized
+active_parameters = [0, 1, 2, 3, 4] # active parameters from the auxiliary distribution which will be optimized
 # WARNING : native parameters of distribution have to be considered
-bounds = ot.Interval([14,0.01,0.,500,20], # bounds on the active parameters
-                     [16,0.2,0.1,1000,70])
+bounds = ot.Interval([14, 0.01, 0., 500, 20], # bounds on the active parameters
+                     [16, 0.2, 0.1, 1000, 70])
 initial_theta= distribution.getParameter() # initial value of the active parameters
 myPhysicalSpaceIS_1 = ot.PhysicalSpaceCrossEntropyImportanceSampling(event,
-                                                                    active_parameters,
-                                                                    initial_theta,
-                                                                    bounds,
-                                                                    aux_distribution)
+                                                                     active_parameters,
+                                                                     initial_theta,
+                                                                     bounds,
+                                                                     aux_distribution)
 
 # %%
 # Custom solver can be also specified, here for example we choose :class:`~openturns.TNC`.
@@ -189,11 +191,11 @@ aux_distribution = ot.ComposedDistribution(aux_marginals)
 print('Parameters of initial distribution',aux_distribution.getParameter())
 
 # Definition of parameters to be optimized
-active_parameters = [0,3] # active parameters of the auxiliary distribution which will be optimized
+active_parameters = [0, 3] # active parameters of the auxiliary distribution which will be optimized
 # WARNING : native parameters of distribution have to be considered
-bounds = ot.Interval([14,500], # bounds on the active parameters
-                     [16,1000])
-initial_theta= [15,750] # initial value of the active parameters
+bounds = ot.Interval([14, 500], # bounds on the active parameters
+                     [16,1 000])
+initial_theta= [15, 750] # initial value of the active parameters
 myPhysicalSpaceIS_2 = ot.PhysicalSpaceCrossEntropyImportanceSampling(event,
                                                                      active_parameters,
                                                                      initial_theta,
@@ -229,9 +231,9 @@ copula = ot.NormalCopula()
 aux_distribution = ot.ComposedDistribution(aux_marginals,copula)
 print('Initial parameters of auxiliary distribution:', aux_distribution.getParameter())
 # Definition of parameters to be optimized
-active_parameters = [0,1,2,3,4,5] # active parameters of the auxiliary distribution which will be optimized
-bounds = ot.Interval([14,0.01,0.,500,20,-1], # bounds on the active parameters
-                     [16,0.2,0.1,1000,70,1])
+active_parameters = [0, 1, 2, 3, 4, 5] # active parameters of the auxiliary distribution which will be optimized
+bounds = ot.Interval([14, 0.01, 0., 500, 20,-1], # bounds on the active parameters
+                     [16, 0.2, 0.1, 1000, 70, 1])
 initial_theta= distribution.getParameter()# initial value of the active parameters
 myPhysicalSpaceIS_3 = ot.PhysicalSpaceCrossEntropyImportanceSampling(event,
                                                                      active_parameters,
