af4e4b882cde5774d6169865954cbdb7893e1cc1
Michael BAUDIN <michael.baudin@gmail.com>
2023-04-10 10:41:21 +0200

    Fixed G-Sobol' test for groups.

--- lib/test/t_FunctionalChaos_gsobol.cxx
+++ lib/test/t_FunctionalChaos_gsobol.cxx
@@ -90,6 +90,7 @@ int main(int, char *[])
     listAdaptiveStrategy.add(CleaningStrategy(productBasis, indexMax, basisDimension, threshold, false));
     // Second, the most used (and most basic!) strategy
     listAdaptiveStrategy.add(FixedStrategy(productBasis, enumerateFunction.getStrataCumulatedCardinal(degree)));
+    //
 
     for(UnsignedInteger adaptiveStrategyIndex = 0; adaptiveStrategyIndex < listAdaptiveStrategy.getSize(); ++adaptiveStrategyIndex)
     {
@@ -163,20 +164,25 @@ int main(int, char *[])
           indices.fill();
           Scalar value = sensitivity.getSobolIndex(indices);
           fullprint << "Sobol index " << indices << " =" << std::fixed << std::setprecision(5) << value << " absolute error=" << std::scientific << std::setprecision(1) << std::abs(value - sobol(indices, a) / covTh) << std::endl;
-          // First order grouped indice
-          indices = Indices(2);
-          indices.fill();
-          Scalar exactS = sobol(indices, a) / covTh;
+          // First order grouped index
+          indices = Indices({0, 1});
+          Scalar exactS0 = sobol(Indices({0}), a) / covTh;
+          Scalar exactS1 = sobol(Indices({1}), a) / covTh;
+          Scalar exactS01 = sobol(Indices({0, 1}), a) / covTh;
+          Scalar exactS = exactS0 + exactS1 + exactS01;
           value = sensitivity.getSobolGroupedIndex(indices);
-          fullprint << "Grouped First Sobol index " << indices << " =" << std::fixed << std::setprecision(5) << value << " absolute error=" << std::scientific << std::setprecision(1) << std::abs(value - exactS) << std::endl;
-          // Total order grouped indice
+          fullprint << "Grouped First Sobol index " << indices << " =" << std::fixed << std::setprecision(5) << value << ", exact = " << exactS << " absolute error=" << std::scientific << std::setprecision(1) << std::abs(value - exactS) << std::endl;
+          assert_almost_equal(value, exactS, 0.0, 1e-2);
+          // Total order grouped index
           indices = Indices(2);
           indices.fill();
           value = sensitivity.getSobolGroupedTotalIndex(indices);
           Indices complementaryindices(3);
           complementaryindices.fill(2);
           exactS = 1 - sensitivity.getSobolGroupedIndex(complementaryindices);
-          fullprint << "Grouped Total Sobol index " << indices << " =" << std::fixed << std::setprecision(5) << value << " absolute error=" << std::scientific << std::setprecision(1) << std::abs(value - exactS) << std::endl;
+          fullprint << "Grouped Total Sobol index " << indices << " =" << std::fixed << std::setprecision(5) << value << std::endl;
+          assert_almost_equal(value, exactS, 0.0, 1e-2);
+
         }
       }
     }
