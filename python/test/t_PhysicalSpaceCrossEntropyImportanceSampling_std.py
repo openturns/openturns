07ae9970665cbb85721fe57ff28b51e1d0f6d6cf
m-balesdent <mathieubalesdent@gmail.com>
2023-04-22 16:15:51 +0200

    Update python/test/t_PhysicalSpaceCrossEntropyImportanceSampling_std.py
    Co-authored-by: Michael Baudin <31351465+mbaudin47@users.noreply.github.com>

--- python/test/t_PhysicalSpaceCrossEntropyImportanceSampling_std.py
+++ python/test/t_PhysicalSpaceCrossEntropyImportanceSampling_std.py
@@ -23,7 +23,7 @@ auxDistribution = ot.ComposedDistribution(auxMarginals)
 activeParameters = [0, 1, 2, 3, 4]
 bounds = ot.Interval([3, 0.09, 0., 50e3, 2e3],
                      [7, 0.5, 0.5, 100e3, 10e3])
-initial_theta = [5.70, 0.1, 0., 75e3, 5e3]
+initialTheta = [5.70, 0.1, 0.0, 75.0e3, 5.0e3]
 
 myISphysical = otexp.PhysicalSpaceCrossEntropyImportanceSampling(event, auxDistribution, activeParameters, initialTheta, bounds, 0.3)
 my_IS_physical.setOptimizationAlgorithm(ot.TNC())
