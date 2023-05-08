8e2a2f1d2d1e0cc57d48a82c3424b87ed1a1d626
Michael BAUDIN <michael.baudin@gmail.com>
2023-05-08 23:54:35 +0200

    Update plot_create_linear_least_squares_model.py.
    Improve vector linear least squares model description.
    
    Update plot_taylor_approximation.py.
    
    Fixed description of the model.

--- python/doc/examples/meta_modeling/general_purpose_metamodels/plot_create_linear_least_squares_model.py
+++ python/doc/examples/meta_modeling/general_purpose_metamodels/plot_create_linear_least_squares_model.py
@@ -3,19 +3,52 @@ Create a linear least squares model
 ===================================
 """
 # %%
-# In this example we are going to create a global approximation of a model response using  a linear
-# function:
+# In this example we are going to create a global approximation of a model
+# response based on a linear function using the :class:`~openturns.LinearLeastSquares` class.
 #
-# .. math::
-#     \vect{y} \, \approx \, \widehat{h}(\vect{x}) \,
-#    = \, \sum_{j=0}^{n_X} \; a_j \; \psi_j(\vect{x})
-#
-#
-# where :math:`h : \Rset^2 \rightarrow \Rset^2` is defined by:
+# We consider the function :math:`h : \Rset^2 \rightarrow \Rset^2` is defined by:
 #
 # .. math::
 #    h(x_1, x_2) = \left(\cos(x_1 + x_2), (x_2 + 1) e^{x_1 - 2 x_2} \right)
 #
+# for any :math:`\vect{x} \in \Rset^2`.
+# Since the output is a dimension 2 vector, the model has vector
+# coefficients.
+# We use the linear model:
+#
+# .. math::
+#     \vect{y} \, \approx \, \widehat{\vect{h}}(\vect{x}) \,
+#     = \, \sum_{k=0}^2 \; \vect{a}_k \; \psi_k(\vect{x})
+#
+#
+# for any :math:`\vect{x} \in \Rset^2` where :math:`\left\{\vect{a}_k \in \Rset^2\right\}_{k = 0,..., 2}`
+# are the vector coefficients and :math:`\left\{\psi_k : \Rset^2 \rightarrow \Rset\right\}_{k = 0, ..., 2}`
+# are the basis functions.
+# This implies that each marginal output :math:`\widehat{h}_i(\vect{x})`
+# is approximated by the linear model:
+#
+# .. math::
+#     \widehat{h}_i(\vect{x}) \,
+#     = \, \sum_{k=0}^2 \; a_{ki} \; \psi_k(\vect{x})
+#
+# for any :math:`\vect{x} \in \Rset^2` and any :math:`i = 1, 2`
+# where :math:`a_{ki}` is the :math:`k`-th coefficient of the :math:`i`-th output
+# marginal:
+#
+# .. math::
+#      \vect{a}_k = \begin{pmatrix}a_{k1} \\ a_{k2} \end{pmatrix}
+#
+# for :math:`k = 0, 1, 2`.
+# We consider the basis functions:
+#
+# .. math::
+#
+#      \psi_0(\vect{x}) & = 1 \\
+#      \psi_1(\vect{x}) & = x_1 \\
+#      \psi_2(\vect{x}) & = x_2 \\
+#
+# for any :math:`\vect{x} \in \Rset^2`.
+
 
 # %%
 import openturns as ot
@@ -24,27 +57,52 @@ from matplotlib import pylab as plt
 
 ot.Log.Show(ot.Log.NONE)
 
+# %%
+# Define the model
+# ~~~~~~~~~~~~~~~~
+
+# %%
 # Prepare an input sample.
-x = [[0.5, 0.5], [-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5]]
-x += [[0.25, 0.25], [-0.25, -0.25], [-0.25, 0.25], [0.25, -0.25]]
+# Each point is a pair of coordinates :math:`(x_1, x_2)`.
+
+# %%
+inputTrain = [[0.5, 0.5], [-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5]]
+inputTrain += [[0.25, 0.25], [-0.25, -0.25], [-0.25, 0.25], [0.25, -0.25]]
+inputTrain = ot.Sample(inputTrain)
+inputTrain.setDescription(["x1", "x2"])
+inputTrain
 
 # %%
 # Compute the output sample from the input sample and a function.
+
+# %%
 formulas = ["cos(x1 + x2)", "(x2 + 1) * exp(x1 - 2 * x2)"]
 model = ot.SymbolicFunction(["x1", "x2"], formulas)
-y = model(x)
+model.setOutputDescription(["y1", "y2"])
+outputTrain = model(inputTrain)
+outputTrain
+
+# %%
+# Linear least squares
+# ~~~~~~~~~~~~~~~~~~~~
 
 # %%
 # Create a linear least squares model.
-algo = ot.LinearLeastSquares(x, y)
+
+# %%
+algo = ot.LinearLeastSquares(inputTrain, outputTrain)
 algo.run()
 
 # %%
 # Get the linear term.
+
+# %%
 algo.getLinear()
 
 # %%
 # Get the constant term.
+
+# %%
 algo.getConstant()
 
 # %%
@@ -53,19 +111,20 @@ responseSurface = algo.getMetaModel()
 
 # %%
 # Plot the second output of our model with :math:`x_1=0.5`.
-graph = (
-    ot.ParametricFunction(responseSurface, [0], [0.5]).getMarginal(1).draw(-0.5, 0.5)
-)
-graph.setLegends(["linear LS"])
+
+# %%
+graph = ot.ParametricFunction(model, [0], [0.5]).getMarginal(1).draw(-0.5, 0.5)
+graph.setLegends(["Model"])
 curve = (
-    ot.ParametricFunction(model, [0], [0.5])
+    ot.ParametricFunction(responseSurface, [0], [0.5])
     .getMarginal(1)
     .draw(-0.5, 0.5)
     .getDrawable(0)
 )
-curve.setColor("red")
-curve.setLegend("model")
+curve.setLineStyle("dashed")
+curve.setLegend("Linear L.S.")
 graph.add(curve)
 graph.setLegendPosition("topright")
+graph.setColors(ot.Drawable.BuildDefaultPalette(2))
 view = viewer.View(graph)
 plt.show()
--- python/doc/examples/meta_modeling/general_purpose_metamodels/plot_taylor_approximation.py
+++ python/doc/examples/meta_modeling/general_purpose_metamodels/plot_taylor_approximation.py
@@ -3,20 +3,25 @@ Taylor approximations
 =====================
 """
 # %%
-# In this example we are going to build a local approximation of a model using the Taylor decomposition:
+# In this example we build a local approximation of a model using the
+# Taylor decomposition using the :class:`~openturns.LinearTaylor` class.
 #
-# Here is the decomposition at the first order:
-#
-# .. math::
-#    \vect{y} \, \approx \, \widehat{h}(\vect{x}) \,
-#          = \, h(\vect{x}_0) \, +
-#         \, \sum_{i=1}^{n_{X}} \; \frac{\partial h}{\partial x_i}(\vect{x}_0).\left(x_i - x_{0,i} \right)
-#
-# where :math:`h : \Rset^2 \rightarrow \Rset^2` is defined by:
+# We consider the function :math:`h : \Rset^2 \rightarrow \Rset^2` defined by:
 #
 # .. math::
 #    h(\vect{x}) = \left( \cos(x_1 + x_2), (x_2 + 1) e^{x_1 - 2 x_2} \right).
 #
+# for any :math:`\vect{x} \in \Rset^2`.
+# The metamodel :math:`\widehat{h}` is is an approximation of the model :math:`h`:
+#
+# .. math::
+#    \vect{y} \, \approx \, \widehat{h}(\vect{x})
+#
+# for any :math:`\vect{x} \in \Rset^2`.
+# In this example, we consider two different approximations:
+#
+# - the first order Taylor expansion,
+# - the second order Taylor expansion.
 
 # %%
 import openturns as ot
@@ -25,16 +30,40 @@ from matplotlib import pylab as plt
 
 ot.Log.Show(ot.Log.NONE)
 
+# %%
+# Define the model
+# ~~~~~~~~~~~~~~~~
+
+# %%
 # Prepare some data.
 formulas = ["cos(x1 + x2)", "(x2 + 1) * exp(x1 - 2 * x2)"]
 model = ot.SymbolicFunction(["x1", "x2"], formulas)
 
+# %%
 # Center of the approximation.
 x0 = [-0.4, -0.4]
 
+# %%
 # Drawing bounds.
 a = -0.4
 b = 0.0
+
+# %%
+# First order Taylor expansion
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# %%
+# Let :math:`\vect{x}_0 \in \Rset^2` be a reference point where
+# the linear approximation is evaluated.
+# The first order Taylor expansion is:
+#
+# .. math::
+#    \widehat{h}(\vect{x}) \,
+#    = \, h(\vect{x}_0) \, +
+#      \, \sum_{i=1}^{n_{X}} \; \frac{\partial h}{\partial x_i}(\vect{x}_0).\left(x_i - x_{0,i} \right)
+#
+# for any :math:`\vect{x} \in \Rset^2`.
+
 
 # %%
 # Create a linear (first-order) Taylor approximation.
@@ -44,42 +73,60 @@ responseSurface = algo.getMetaModel()
 
 # %%
 # Plot the second output of our model with :math:`x_1=x_{0,1}`.
-graph = ot.ParametricFunction(responseSurface, [0], [x0[1]]).getMarginal(1).draw(a, b)
-graph.setLegends(["taylor"])
+
+# %%
+graph = ot.ParametricFunction(model, [0], [x0[1]]).getMarginal(1).draw(a, b)
+graph.setLegends(["Model"])
 curve = (
-    ot.ParametricFunction(model, [0], [x0[1]]).getMarginal(1).draw(a, b).getDrawable(0)
+    ot.ParametricFunction(responseSurface, [0], [x0[1]]).getMarginal(1).draw(a, b).getDrawable(0)
 )
-curve.setColor("red")
-curve.setLegend("model")
+curve.setLegend("Taylor")
+curve.setLineStyle("dashed")
 graph.add(curve)
 graph.setLegendPosition("topright")
+graph.setColors(ot.Drawable.BuildDefaultPalette(2))
 view = viewer.View(graph)
 
 # %%
-# Here is the decomposition at the second order:
+# Second order Taylor expansion
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# %%
+# Let :math:`\vect{x}_0 \in \Rset^2` be a reference point where
+# the quadratic approximation is evaluated.
+# The second order Taylor expansion is:
 #
-# .. math::\vect{y} \, \approx \, \widehat{h}(\vect{x}) \, = \,
-#         h(\vect{x}_0) \, + \, \sum_{i=1}^{n_{X}} \;
-#       \frac{\partial h}{\partial x_i}(\vect{x}_0).\left(x_i - x_{0,i} \right) \, +
-#      \, \frac{1}{2} \; \sum_{i,j=1}^{n_X} \;
-#       \frac{\partial^2 h}{\partial x_i \partial x_j}(\vect{x}_0).\left(x_i - x_{0,i} \right).\left(x_j - x_{0,j} \right):math:``
+# .. math::
+#
+#     \widehat{h}(\vect{x}) \, = \,
+#     h(\vect{x}_0) \, + \, \sum_{i=1}^{n_{X}} \;
+#     \frac{\partial h}{\partial x_i}(\vect{x}_0).\left(x_i - x_{0,i} \right) \, +
+#     \, \frac{1}{2} \; \sum_{i,j=1}^{n_X} \;
+#     \frac{\partial^2 h}{\partial x_i \partial x_j}(\vect{x}_0).\left(x_i - x_{0,i} \right).\left(x_j - x_{0,j} \right)
+#
+# for any :math:`\vect{x} \in \Rset^2`.
 
 # %%
 # Create a quadratic (second-order) Taylor approximation.
+
+# %%
 algo = ot.QuadraticTaylor(x0, model)
 algo.run()
 responseSurface = algo.getMetaModel()
 
 # %%
 # Plot second output of our model with :math:`x_1=x_{0,1}`.
-graph = ot.ParametricFunction(responseSurface, [0], [x0[1]]).getMarginal(1).draw(a, b)
-graph.setLegends(["taylor"])
+
+# %%
+graph = ot.ParametricFunction(model, [0], [x0[1]]).getMarginal(1).draw(a, b)
+graph.setLegends(["Model"])
 curve = (
-    ot.ParametricFunction(model, [0], [x0[1]]).getMarginal(1).draw(a, b).getDrawable(0)
+    ot.ParametricFunction(responseSurface, [0], [x0[1]]).getMarginal(1).draw(a, b).getDrawable(0)
 )
-curve.setColor("red")
-curve.setLegend("model")
+curve.setLegend("Taylor")
+curve.setLineStyle("dashed")
 graph.add(curve)
 graph.setLegendPosition("topright")
+graph.setColors(ot.Drawable.BuildDefaultPalette(2))
 view = viewer.View(graph)
 plt.show()
