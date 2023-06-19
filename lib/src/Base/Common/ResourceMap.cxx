657f4d3ac0f48fa6b70d99a6124f8552c281d93c
Julien Schueller <schueller@phimeca.com>
2023-06-19 14:39:47 +0200

    HSIC: Fix covariance matrix allocation
    Closes #2338

--- lib/src/Base/Common/ResourceMap.cxx
+++ lib/src/Base/Common/ResourceMap.cxx
@@ -976,6 +976,7 @@ void ResourceMap::loadDefaultConfiguration()
   // HSIC parameters //
   addAsBool("HSICEstimator-ParallelPValues", true);
   addAsUnsignedInteger("HSICEstimator-PermutationSize", 100);
+  addAsUnsignedInteger("HSICEstimator-InputCovarianceMatrixCacheSizeMb", 1000); // 1 Gb
 
   // RandomGenerator parameters //
   addAsUnsignedInteger("RandomGenerator-InitialSeed", 0);
