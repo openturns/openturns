0a9d241d6e72fae6a66dcb82df93478e719aa508
Julien Schueller <schueller@phimeca.com>
2023-10-11 11:25:46 +0200

    GPDFactory: Set solver from ResourceMap

--- python/test/t_GeneralizedParetoFactory_std.py
+++ python/test/t_GeneralizedParetoFactory_std.py
@@ -6,14 +6,11 @@ from openturns.usecases import coles
 
 ot.TESTPREAMBLE()
 
-xi = [-0.75, 0.0, 0.75]
 size = 10000
-covariance = ot.CovarianceMatrix(2)
 factory = ot.GeneralizedParetoFactory()
-for i in range(3):
-    distribution = ot.GeneralizedPareto(2.5, xi[i], 0.5)
+for xi in [-0.75, 0.0, 0.75]:
+    distribution = ot.GeneralizedPareto(2.5, xi, 0.5)
     sample = distribution.getSample(size)
-    # Distribution estimatedDistribution(factory.build(sample, covariance))
     estimatedDistribution = factory.build(sample)
     print("Distribution          =", distribution)
     print("Estimated distribution=", estimatedDistribution)
@@ -23,19 +20,24 @@ for i in range(3):
     estimatedGeneralizedPareto = factory.buildAsGeneralizedPareto(sample)
     print("GeneralizedPareto          =", distribution)
     print("Estimated generalizedPareto=", estimatedGeneralizedPareto)
-    try:
+    assert estimatedGeneralizedPareto.__class__.__name__ == "GeneralizedPareto", "wrong name"
+
+    # method of moments
+    if xi <= 0.0:
         estimatedDistribution = factory.buildMethodOfMoments(sample)
         print("GeneralizedPareto from moments=", estimatedDistribution)
-        ott.assert_almost_equal(
-            estimatedDistribution.getParameter(),
-            distribution.getParameter(),
-            1e-1,
-            1e-1,
-        )
-    except TypeError:
-        # the method is invalid for some range of xsi
-        pass
+        ott.assert_almost_equal(estimatedDistribution.getParameter(), distribution.getParameter(), 1e-1, 1e-1)
 
+    # exponential regression
+    estimatedDistribution = factory.buildMethodOfExponentialRegression(sample)
+    print("GeneralizedPareto from exponential regression=", estimatedDistribution)
+    ott.assert_almost_equal(estimatedDistribution.getParameter(), distribution.getParameter(), 1e-1, 1e-1)
+
+    # pwm
+    if xi >= -0.5:
+        estimatedDistribution = factory.buildMethodOfProbabilityWeightedMoments(sample)
+        print("GeneralizedPareto from pwm=", estimatedDistribution)
+        ott.assert_almost_equal(estimatedDistribution.getParameter(), distribution.getParameter(), 1e-1, 1e-1)
 
 estimatedDistribution = factory.build()
 ott.assert_almost_equal(estimatedDistribution.getParameter(), [1.0, 0.0, 0.0])
@@ -43,10 +45,12 @@ print("Default distribution=", estimatedDistribution)
 estimatedDistribution = factory.build(distribution.getParameter())
 print("Distribution from parameters=", estimatedDistribution)
 estimatedGeneralizedPareto = factory.buildAsGeneralizedPareto()
+assert estimatedGeneralizedPareto.__class__.__name__ == "GeneralizedPareto", "wrong name"
 print("Default generalizedPareto=", estimatedGeneralizedPareto)
 estimatedGeneralizedPareto = factory.buildAsGeneralizedPareto(
     distribution.getParameter()
 )
+assert estimatedGeneralizedPareto.__class__.__name__ == "GeneralizedPareto", "wrong name"
 print("GeneralizedPareto from parameters=", estimatedGeneralizedPareto)
 ott.assert_almost_equal(
     estimatedGeneralizedPareto.getParameter(), distribution.getParameter()
