9387de5680f43c10ff7c98e3f23033299243f129
Michael Baudin <31351465+mbaudin47@users.noreply.github.com>
2023-10-12 11:40:10 +0200

    Update plot_graphs_fill_area.py
    Simplified the fill using a single Polygon, which prevents from drawing a large number of edges.

--- python/doc/examples/graphs/plot_graphs_fill_area.py
+++ python/doc/examples/graphs/plot_graphs_fill_area.py
@@ -6,7 +6,9 @@ How to fill an area
 # %%
 
 # %%
-# In this example, we show how to fill specified area with a given color.
+# In this example, we show how to fill a specified 2D area with a given color.
+# The bounds of the area are defined by the X coordinates and the
+# Y lower and upper bounds.
 
 # %%
 import openturns as ot
@@ -24,8 +26,6 @@ graph = dist.drawPDF()
 view = viewer.View(graph)
 
 # %%
-
-
 def linearSample(xmin, xmax, npoints):
     """Returns a sample created from a regular grid
     from xmin to xmax with npoints points."""
@@ -38,8 +38,6 @@ def linearSample(xmin, xmax, npoints):
 # %%
 a = 1.0
 b = 2.0
-
-# %%
 nplot = 100  # Number of points in the plot
 x = linearSample(a, b, nplot)
 y = dist.computePDF(x)
@@ -49,34 +47,62 @@ y = dist.computePDF(x)
 # The following function uses the `PolygonArray` class to create a area filled with a given color.
 
 # %%
-def drawInTheBounds(vLow, vUp, n_test):
+def fillWithColorInBetween(x, yLower, yUpper, color):
     """
-    Draw the area within the bounds.
+    Fill an area with a color between two bounds
+
+    Parameters
+    ----------
+    x : ot.Point(n_points)
+        The X coordinates.
+    yLower : ot.Point(n_points)
+        The Y coordinate of the lower bound.
+    yUpper : ot.Point(n_points)
+        The Y coordinate of the upper bound.
+
+    Return
+    ------
+    polygonArray : ot.PolygonArray()
+        The colored area.
     """
-    palette = ot.Drawable.BuildDefaultPalette(2)
-    myPaletteColor = palette[1]
-    polyData = [[vLow[i], vLow[i + 1], vUp[i + 1], vUp[i]] for i in range(n_test - 1)]
+    n_points = x.getDimension()
+    if yLower.getDimension() != n_points:
+        raise ValueError(f"The number of Y lower bounds is equal to {yLower.getDimension()} "
+                            f"but the number of X coordinates is {n_points}")
+    if yUpper.getDimension() != n_points:
+        raise ValueError(f"The number of Y upper bounds is equal to {yUpper.getDimension()} "
+                            f"but the number of X coordinates is {n_points}")
+    n_points = x.getDimension()
+    polyData = []
+    # Add the lower bound, forward
+    for i in range(n_points):
+        polyData.append([x[i], yLower[i]])
+    # Add the upper bound, backward
+    for i in range(n_points - 1, -1, -1):
+        polyData.append([x[i], yUpper[i]])
+
+    polyData = ot.Sample(polyData)
     polygonList = [
-        ot.Polygon(polyData[i], myPaletteColor, myPaletteColor)
-        for i in range(n_test - 1)
+        ot.Polygon(polyData, color, color)
     ]
-    boundsPoly = ot.PolygonArray(polygonList)
-    return boundsPoly
+    polygonArray = ot.PolygonArray(polygonList)
+    return polygonArray
 
 
 # %%
 # Compute the bounds to fill: the lower vertical bound is zero and the upper vertical bound is the PDF.
-
-# %%
-vLow = [[x[i, 0], 0.0] for i in range(nplot)]
-vUp = [[x[i, 0], y[i, 0]] for i in range(nplot)]
+xPoint = x.asPoint()
+yLower = ot.Point(nplot)
+yUpper = y.asPoint()
 
 # %%
 area = dist.computeCDF(b) - dist.computeCDF(a)
 
 # %%
-boundsPoly = drawInTheBounds(vLow, vUp, nplot)
+palette = ot.Drawable.BuildDefaultPalette(2)
 graph = dist.drawPDF()
+color = palette[0]
+boundsPoly = fillWithColorInBetween(xPoint, yLower, yUpper, color)
 graph.add(boundsPoly)
 graph.setTitle("Area = %.3f" % (area))
 graph.setLegends([""])
