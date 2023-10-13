fcb7a6ff3a9f01c8a042f4f33ac51b310b07830d
Julien Schueller <schueller@phimeca.com>
2023-10-13 17:24:34 +0200

    Ceres: Drop deprecated use_postordering option
    Fixes build with v2.2.0

--- lib/src/Base/Optim/Ceres.cxx
+++ lib/src/Base/Optim/Ceres.cxx
@@ -340,8 +340,6 @@ void Ceres::run()
       throw InvalidArgumentException(HERE) << "Invalid value for sparse_linear_algebra_library_type";
     if (ResourceMap::HasKey("Ceres-use_explicit_schur_complement"))
       options.use_explicit_schur_complement = ResourceMap::GetAsBool("Ceres-use_explicit_schur_complement");
-    if (ResourceMap::HasKey("Ceres-use_postordering"))
-      options.use_postordering = ResourceMap::GetAsBool("Ceres-use_postordering");
     if (ResourceMap::HasKey("Ceres-dynamic_sparsity"))
       options.dynamic_sparsity = ResourceMap::GetAsBool("Ceres-dynamic_sparsity");
     if (ResourceMap::HasKey("Ceres-min_linear_solver_iterations"))
