a07f0a75161b33f49499bba56760711e4fb0dbe7
Julien Schueller <schueller@phimeca.com>
2023-10-30 08:57:59 +0100

    LSDistributionFactory: Cleanup unused gradient

--- python/test/t_LeastSquaresDistributionFactory_std.py
+++ python/test/t_LeastSquaresDistributionFactory_std.py
@@ -1,12 +1,24 @@
 #! /usr/bin/env python
 
 import openturns as ot
+import openturns.testing as ott
 
-ot.PlatformInfo.SetNumericalPrecision(3)
+size = 1000
+
 distribution = ot.Pareto(3.3, 7.5, 0.0)
 print("distribution=", distribution)
-sample = distribution.getSample(1000)
+sample = distribution.getSample(size)
 factory = ot.LeastSquaresDistributionFactory(ot.Pareto())
 factory.setKnownParameter([0.0], [2])
-inf_distribution = factory.build(sample)
-print("estimated distribution=", inf_distribution)
+inf_dist = factory.build(sample)
+print("estimated distribution=", inf_dist)
+ott.assert_almost_equal(inf_dist.getParameter(), distribution.getParameter(), 1e-2, 1e-2)
+
+# Normal, optimal far away
+distribution = ot.Normal(17.0, 1.0)
+print("distribution=", distribution)
+sample = distribution.getSample(size)
+factory = ot.QuantileMatchingFactory(ot.Normal(), [0.01, 0.99])
+inf_dist = factory.build(sample)
+print("estimated distribution=", inf_dist)
+ott.assert_almost_equal(inf_dist.getParameter(), distribution.getParameter(), 1e-1, 1e-1)
