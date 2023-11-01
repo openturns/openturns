2fef7d7c899011f0c8b7c79ad8f6e75b843973f2
Michael BAUDIN <michael.baudin@gmail.com>
2023-11-01 12:43:47 +0100

    Polynomial chaos expansion: Improve the examples.
    Fixed wording and typo in OrthogonalProductPolynomialFactory.
    Trigger pretty-print when possible.

--- python/doc/examples/meta_modeling/polynomial_chaos_metamodel/plot_functional_chaos_database.py
+++ python/doc/examples/meta_modeling/polynomial_chaos_metamodel/plot_functional_chaos_database.py
@@ -1,12 +1,18 @@
 """
-Polynomial chaos over database
-==============================
+Create a full or sparse polynomial chaos expansion
+==================================================
 """
 # %%
-# In this example we are going to create a global approximation of a model response using functional chaos over a design of experiment.
-#
-# You will need to specify the distribution of the input parameters.
-# If not known, statistical inference can be used to select a possible candidate, and fitting tests can validate such an hypothesis.
+# In this example we create a global approximation of a model using
+# polynomial chaos expansion based on a design of experiment.
+# The goal of this example is to show how we can create a full or
+# sparse polynomial chaos expansion depending on our needs
+# and depending on the number of observations we have.
+# In general, we should have more observations than parameters to estimate.
+# This is why a sparse polynomial chaos may be interesting:
+# by carefully selecting the coefficients we estimate,
+# we may reduce overfitting and increase the predictions of the
+# metamodel.
 
 # %%
 import openturns as ot
@@ -14,49 +20,156 @@ import openturns as ot
 ot.Log.Show(ot.Log.NONE)
 
 # %%
-# Create a function R^n --> R^p
-# For example R^4 --> R
-myModel = ot.SymbolicFunction(["x1", "x2", "x3", "x4"], ["1+x1*x2 + 2*x3^2+x4^4"])
+# Define the model
+# ~~~~~~~~~~~~~~~~
 
-# Create a distribution of dimension n
-# for example n=3 with independent components
+# %%
+# Create the function.
+myModel = ot.SymbolicFunction(
+    ["x1", "x2", "x3", "x4"], ["1 + x1 * x2 + 2 * x3^2 + x4^4"]
+)
+
+# %%
+# Create a multivariate distribution.
 distribution = ot.JointDistribution(
     [ot.Normal(), ot.Uniform(), ot.Gamma(2.75, 1.0), ot.Beta(2.5, 1.0, -1.0, 2.0)]
 )
 
 # %%
-# Prepare the input/output samples
-sampleSize = 250
-X = distribution.getSample(sampleSize)
-Y = myModel(X)
-dimension = X.getDimension()
+# In order to create the PCE, we can specify the distribution of the
+# input parameters.
+# If not known, statistical inference can be used to select a possible
+# candidate, and fitting tests can validate such an hypothesis.
+# Please read :doc:`Fit a distribution from an input sample </auto_meta_modeling/polynomial_chaos_metamodel/plot_chaos_build_distribution>`
+# for an example of this method.
 
 # %%
-# build the orthogonal basis
+# Create a training sample
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+# %%
+# Create a pair of input and output samples.
+sampleSize = 250
+inputSample = distribution.getSample(sampleSize)
+outputSample = myModel(inputSample)
+
+# %%
+# Build the orthogonal basis
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# %%
+# In the next cell, we create the univariate orthogonal polynomial basis
+# for each marginal.
+inputDimension = inputSample.getDimension()
 coll = [
     ot.StandardDistributionPolynomialFactory(distribution.getMarginal(i))
-    for i in range(dimension)
+    for i in range(inputDimension)
 ]
-enumerateFunction = ot.LinearEnumerateFunction(dimension)
+enumerateFunction = ot.LinearEnumerateFunction(inputDimension)
 productBasis = ot.OrthogonalProductPolynomialFactory(coll, enumerateFunction)
 
 # %%
-# create the algorithm
-degree = 6
-adaptiveStrategy = ot.FixedStrategy(
-    productBasis, enumerateFunction.getStrataCumulatedCardinal(degree)
+# We can achieve the same result using :class:`~OrthogonalProductPolynomialFactory`.
+marginalDistributionCollection = [
+    distribution.getMarginal(i) for i in range(inputDimension)
+]
+multivariateBasis = ot.OrthogonalProductPolynomialFactory(
+    marginalDistributionCollection
 )
-projectionStrategy = ot.LeastSquaresStrategy()
-algo = ot.FunctionalChaosAlgorithm(
-    X, Y, distribution, adaptiveStrategy, projectionStrategy
-)
-algo.run()
+multivariateBasis
 
 # %%
-# get the metamodel function
+# Create a full PCE
+# ~~~~~~~~~~~~~~~~~
+
+# %%
+# Create the algorithm.
+# We compute the basis size from the total degree.
+# The next lines use the :class:`~openturns.LeastSquaresStrategy` class
+# with default parameters (the default is the
+# :class:`~openturns.PenalizedLeastSquaresAlgorithmFactory` class).
+# This creates a full polynomial chaos expansion, i.e.
+# we keep all the candidate coefficients produced by the enumeration
+# rule.
+# In order to create a sparse polynomial chaos expansion, we
+# must use the :class:`~openturns.LeastSquaresMetaModelSelectionFactory`
+# class instead.
+#
+totalDegree = 3
+candidateBasisSize = enumerateFunction.getBasisSizeFromTotalDegree(totalDegree)
+print("Candidate basis size = ", candidateBasisSize)
+adaptiveStrategy = ot.FixedStrategy(productBasis, candidateBasisSize)
+projectionStrategy = ot.LeastSquaresStrategy()
+algo = ot.FunctionalChaosAlgorithm(
+    inputSample, outputSample, distribution, adaptiveStrategy, projectionStrategy
+)
+algo.run()
 result = algo.getResult()
+result
+
+# %%
+# Get the number of coefficients in the PCE.
+selectedBasisSizeFull = result.getIndices().getSize()
+print("Selected basis size = ", selectedBasisSizeFull)
+
+# %%
+# We see that the number of coefficients in the selected basis is
+# equal to the number of coefficients in the candidate basis.
+# This is, indeed, a *full* PCE.
+
+# %%
+# Use the PCE
+# ~~~~~~~~~~~
+
+# %%
+# Get the metamodel function.
 metamodel = result.getMetaModel()
 
 # %%
-# Print residuals
+# In order to evaluate the metamodel on a single point, we just
+# use it as any other :class:`openturns.Function`.
+xPoint = distribution.getMean()
+yPoint = metamodel(xPoint)
+print("Value at ", xPoint, " is ", yPoint)
+
+# %%
+# Print residuals.
 result.getResiduals()
+
+# %%
+# Based on these results, we may want to validate our metamodel.
+# More details on this topic are presented in
+# :doc:`Validate a polynomial chaos </auto_meta_modeling/polynomial_chaos_metamodel/plot_chaos_draw_validation>`.
+
+# %%
+# Create a sparse PCE
+# ~~~~~~~~~~~~~~~~~~~
+
+# %%
+# In order to create a sparse polynomial chaos expansion, we
+# use the :class:`~openturns.LeastSquaresMetaModelSelectionFactory`
+# class instead.
+#
+totalDegree = 6
+candidateBasisSize = enumerateFunction.getBasisSizeFromTotalDegree(totalDegree)
+print("Candidate basis size = ", candidateBasisSize)
+adaptiveStrategy = ot.FixedStrategy(productBasis, candidateBasisSize)
+selectionAlgorithm = ot.LeastSquaresMetaModelSelectionFactory()
+projectionStrategy = ot.LeastSquaresStrategy(selectionAlgorithm)
+algo = ot.FunctionalChaosAlgorithm(
+    inputSample, outputSample, distribution, adaptiveStrategy, projectionStrategy
+)
+algo.run()
+result = algo.getResult()
+result
+
+# %%
+# Get the number of coefficients in the PCE.
+selectedBasisSizeSparse = result.getIndices().getSize()
+print("Selected basis size = ", selectedBasisSizeSparse)
+
+# %%
+# We see that the number of selected coefficients is lower than
+# the number of candidate coefficients.
+# This may reduce overfitting and can produce a PCE with more
+# accurate predictions.
