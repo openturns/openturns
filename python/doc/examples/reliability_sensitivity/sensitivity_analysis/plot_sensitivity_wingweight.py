56f9e3f901d2409838fb91c61d45e0953171c805
Michael BAUDIN <michael.baudin@gmail.com>
2023-11-01 12:43:47 +0100

    Polynomial chaos expansion: Improve the examples.
    Fixed wording and typo in OrthogonalProductPolynomialFactory.
    Trigger pretty-print when possible.

--- python/doc/examples/reliability_sensitivity/sensitivity_analysis/plot_sensitivity_wingweight.py
+++ python/doc/examples/reliability_sensitivity/sensitivity_analysis/plot_sensitivity_wingweight.py
@@ -357,7 +357,9 @@ print(result.getRelativeErrors())
 # The relative errors are low : this indicates that the PCE model has good accuracy.
 # Then, we exploit the surrogate model to compute the Sobol' indices.
 sensitivityAnalysis = ot.FunctionalChaosSobolIndices(result)
-print(sensitivityAnalysis)
+sensitivityAnalysis
+
+# %%
 firstOrder = [sensitivityAnalysis.getSobolIndex(i) for i in range(m.dim)]
 totalOrder = [sensitivityAnalysis.getSobolTotalIndex(i) for i in range(m.dim)]
 graph = ot.SobolIndicesAlgorithm.DrawSobolIndices(inputNames, firstOrder, totalOrder)
