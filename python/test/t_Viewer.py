af7830859fa9916ffc4e6eef466290653a653be8
Julien Schueller <schueller@phimeca.com>
2023-12-07 12:02:44 +0100

    Function: Contour legend outside by default

--- python/test/t_Viewer.py
+++ python/test/t_Viewer.py
@@ -11,8 +11,6 @@ view.show()
 
 # Contour
 graph = ot.Normal([1, 2], [3, 5], ot.CorrelationMatrix(2)).drawPDF()
-graph.setLegendPosition("upper left")
-graph.setLegendCorner([1.0, 1.0])
 view = View(graph)
 # view.save('curve2.png')
 view.show()
