4b3dcbdf6306c13913cc53dfde2c705c591a7ef9
Julien Schueller <schueller@phimeca.com>
2024-01-05 13:27:29 +0100

    SmoothedUniform: Override computeProbability

--- lib/src/Uncertainty/Distribution/SmoothedUniform.cxx
+++ lib/src/Uncertainty/Distribution/SmoothedUniform.cxx
@@ -261,6 +261,13 @@ Scalar SmoothedUniform::computeScalarQuantile(const Scalar prob,
   return x;
 }
 
+Scalar SmoothedUniform::computeProbability(const Interval & interval) const
+{
+  if (interval.getDimension() != 1)
+    throw InvalidArgumentException(HERE) << "computeProbability expected an interval of dimension=" << dimension_ << ", got dimension=" << interval.getDimension();
+  return computeProbabilityGeneral1D(interval.getLowerBound()[0], interval.getUpperBound()[0]);
+}
+
 /* Compute the mean of the distribution */
 void SmoothedUniform::computeMean() const
 {
