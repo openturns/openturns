d466a7367e8e3121e1678f1db46379bcefd29118
josephmure <joseph.mure@edf.fr>
2024-01-09 14:00:36 +0100

    DistributionImplementation::computeQuantile fix
    Checks for dimension 1 first.
    Extremities of the range are used only for prob (resp. 1 - prob) < cdfEpsilon_ rather than quantileEpsilon_
    Fixes #2513

--- lib/src/Uncertainty/Model/DistributionImplementation.cxx
+++ lib/src/Uncertainty/Model/DistributionImplementation.cxx
@@ -2434,13 +2434,12 @@ Point DistributionImplementation::computeQuantile(const Scalar prob,
     const Bool tail,
     Scalar & marginalProb) const
 {
-  const Scalar q = tail ? 1.0 - prob : prob;
-  marginalProb = q;
-  // Special case for bording values
-  if (prob < quantileEpsilon_) return (tail ? range_.getUpperBound() : range_.getLowerBound());
-  if (prob >= 1.0 - quantileEpsilon_) return (tail ? range_.getLowerBound() : range_.getUpperBound());
   // Special case for dimension 1
   if (dimension_ == 1) return Point(1, computeScalarQuantile(prob, tail));
+  // Special case for border values
+  if (prob < cdfEpsilon_) return (tail ? range_.getUpperBound() : range_.getLowerBound());
+  if (prob >= 1.0 - cdfEpsilon_) return (tail ? range_.getLowerBound() : range_.getUpperBound());
+  const Scalar q = tail ? 1.0 - prob : prob;
   // Special case for independent copula
   if (hasIndependentCopula())
   {
