af3ec8b21e51fcdd317cd951dc2a01188f831989
Julien Schueller <schueller@phimeca.com>
2024-02-13 17:24:32 +0100

    validation: Try ismev/extRemes

--- validation/src/ValidGeneralizedParetoFactory.R
+++ validation/src/ValidGeneralizedParetoFactory.R
@@ -2,28 +2,47 @@
 # R -e 'install.packages(c("extRemes", "evmix"))'
 # Rscript validation/src/ValidGeneralizedParetoFactory.R
 
-data <- as.matrix(read.csv("python/src/usecases/rain.csv"))
-# print(data)
+xdat <- as.matrix(read.csv("python/src/usecases/rain.csv"))
+n <- length(xdat)
+# print(xdat)
 
 # mean residual life
 library(extRemes)
 png("mlr.png")
-mrlplot(data)
+mrlplot(xdat)
 
 library(eva)
 png("mlexi.png")
 # mle
 cat("\n--- MLE ---\n")
-fit <- gpdFit(data, threshold=30)
+fit <- gpdFit(xdat, threshold=30)
 print(fit)
 
 cat("\n--- covariates ---\n")
-n <- length(data)
 covs <- as.data.frame(seq(1, n, 1))
 covs$const <- 1
-names(covs) <- c("Trend1")
-fit_cov <- gpdFit(data, threshold = 30, scalevars = covs, scaleform = ~ Trend1, scalelink = exp, method= "mps")
+names(covs) <- c("Trend1", "const")
+fit_cov <- gpdFit(xdat, threshold = 30, scalevars = covs, scaleform = ~ Trend1, scalelink = exp, method= "mps")
 print(fit_cov)
+
+
+library(extRemes)
+data <- as.data.frame(xdat)
+data$trend <- seq(1, n, 1)
+names(data) <- c("x", "trend")
+summary(data)
+cat("\n--- extRemes ---\n")
+scalef <- x ~ trend
+fit_cov2 <- fevd(xdat, data = data, threshold = 30, type = "GP", scale.fun = scalef, use.phi = TRUE, method = "MLE")
+print(fit_cov2)
+
+cat("\n--- ismev ---\n")
+library(ismev)
+summary(xdat)
+covs2 <- as.data.frame(seq(1, n, 1))
+names(covs2) <- c("trend")
+summary(covs2)
+fit_cov3 <- gpd.fit(xdat, threshold = 30, ydat = covs2, sigl = c(1), shl = NULL, siglink = exp)
 
 # return level
 cat("\n--- return level ---\n")
@@ -38,7 +57,7 @@ print(fit2)
 # parameter stability plots
 library(evmix)
 png("shape.png")
-tshapeplot(data, tlim = c(0, 50))
+tshapeplot(xdat, tlim = c(0, 50))
 png("scale.png")
-tscaleplot(data, tlim = c(0, 50)) # , try.thresh = c(8, 10, 12)
-tscaleplot(data, tlim = c(10, 40))
+tscaleplot(xdat, tlim = c(0, 50)) # , try.thresh = c(8, 10, 12)
+tscaleplot(xdat, tlim = c(10, 40))
