d62d4bccbaeb9b21dfa8358efd27beb6eb63cb0b
Julien Schueller <schueller@phimeca.com>
2024-02-29 20:27:31 +0100

    SQP: Check for timeout

--- lib/src/Base/Optim/SQP.cxx
+++ lib/src/Base/Optim/SQP.cxx
@@ -156,7 +156,7 @@ void SQP::run()
   /* Get a local copy of the level value */
   const Scalar levelValue = getProblem().getLevelValue();
 
-  Bool exitLoop = false;
+  Bool stop = false;
   UnsignedInteger iterationNumber = 0;
   const UnsignedInteger initialEvaluationNumber = levelFunction.getEvaluationCallsNumber();
   Scalar absoluteError = -1.0;
@@ -176,7 +176,9 @@ void SQP::run()
   result_ = OptimizationResult(getProblem());
   result_.store(currentPoint_, Point(1, currentLevelValue_), absoluteError, relativeError, residualError, constraintError);
 
-  while ((!exitLoop) && (iterationNumber <= getMaximumIterationNumber()) && (evaluationNumber <= getMaximumCallsNumber()))
+  std::chrono::steady_clock::time_point t0 = std::chrono::steady_clock::now();
+
+  while ((!stop) && (iterationNumber <= getMaximumIterationNumber()) && (evaluationNumber <= getMaximumCallsNumber()))
   {
     /* Go to next iteration */
     ++ iterationNumber;
@@ -244,7 +246,7 @@ void SQP::run()
 
     residualError = (currentPoint_ + currentLambda_ * currentGradient_).norm();
 
-    exitLoop = ((absoluteError < getMaximumAbsoluteError()) && (relativeError < getMaximumRelativeError())) || ((residualError < getMaximumResidualError()) && (constraintError < getMaximumConstraintError()));
+    stop = ((absoluteError < getMaximumAbsoluteError()) && (relativeError < getMaximumRelativeError())) || ((residualError < getMaximumResidualError()) && (constraintError < getMaximumConstraintError()));
 
     // update result
     result_.setCallsNumber(evaluationNumber);
@@ -258,24 +260,22 @@ void SQP::run()
     {
       progressCallback_.first((100.0 * evaluationNumber) / getMaximumCallsNumber(), progressCallback_.second);
     }
-    if (stopCallback_.first)
+    if (stopCallback_.first && stopCallback_.first(stopCallback_.second))
     {
-      Bool stop = stopCallback_.first(stopCallback_.second);
-      if (stop)
-      {
-        exitLoop = true;
-        LOGWARN(OSS() << "SQP was stopped by user");
-      }
+      stop = true;
+      LOGWARN(OSS() << "SQP was stopped by user");
+    }
+
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    const Scalar timeDuration = std::chrono::duration<Scalar>(t1 - t0).count();
+    if ((getMaximumTimeDuration() > 0.0) && (timeDuration > getMaximumTimeDuration()))
+    {
+      LOGINFO("Optim timeout");
+      stop = true;
+      result_.setStatus(OptimizationResult::TIMEOUT);
+      result_.setStatusMessage(OSS() << "SQP optimization timeout after " << timeDuration << "s");
     }
   }
-
-  /* Check if we converged */
-
-  if (!exitLoop)
-  {
-    LOGWARN(OSS() << "Warning! The AbdoRackwitz algorithm failed to converge after " << iterationNumber << " iterations, " << evaluationNumber << " evaluations." );
-  }
-
 } // run()
 
 /* Tau accessor */
