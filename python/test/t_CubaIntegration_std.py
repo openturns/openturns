a04470819399c833d248686436226a3cb6a34e40
Pierre Gruet <pgt@debian.org>
2024-03-31 23:21:14 +0200

    Adding the Python unit test for CubaIntegration

---
+++ python/test/t_CubaIntegration_std.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+import openturns as ot
+import math
+import openturns.testing as ott
+
+ot.TESTPREAMBLE()
+
+# Testing four algorithms on a function from R^3 to R.
+f = ot.SymbolicFunction(["x", "y", "z"], ["sin(x) * cos(y) * exp(z)"])
+lbIntegration = ot.Point((0., 0., 0.))
+ubIntegration = ot.Point((1., 1., 1.))
+routines = ["cuhre", "divonne", "suave", "vegas"]
+valueRef = -math.sin(1.) * (math.cos(1.) - 1.) * (math.e - 1.)
+
+for r in routines:
+    algoC = ot.CubaIntegration(r)
+    value = algoC.integrate(f, ot.Interval(lbIntegration, ubIntegration))
+    ott.assert_almost_equal(value[0], valueRef, 1.E-3, 1.E-3)
+
+# Testing three algorithms on a function from R^3 to R^2.
+f = ot.SymbolicFunction(["x", "y", "z"], ["exp(-(x * x + y * y + z *z))", "(x / 2 * x / 2 + y / 3 * y / 3 + z / 4 * z / 4 - 1.125)"])
+lbIntegration = ot.Point((0., 0., 1.))
+ubIntegration = ot.Point((2., 3., 4.))
+routines = ["cuhre", "divonne", "vegas"]
+valueRef = ot.Point((0.108972129575688278, -0.375))
+
+for r in routines:
+    algoC = ot.CubaIntegration(r)
+    if r == "vegas":
+        # Vegas seemingly needs more favorable parameters there
+        algoC.setEpsAbs(5.E-3);
+        algoC.setEpsRel(1.E-3);
+        algoC.setMaxeval(10000000);
+    value = algoC.integrate(f, ot.Interval(lbIntegration, ubIntegration))
+    ott.assert_almost_equal(value, valueRef, 1.E-3, 1.E-3)
