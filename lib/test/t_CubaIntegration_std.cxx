cfc86e420178b29328e366750429bd288b327f94
Pierre Gruet <pgt@debian.org>
2024-04-01 23:01:11 +0200

    Improving coding style around CubaIntegration
    - Using '0.0' notation for float constants instead of '0.' in Python code;
    - Above change and long line reshaping in the C++ part.

--- lib/test/t_CubaIntegration_std.cxx
+++ lib/test/t_CubaIntegration_std.cxx
@@ -37,8 +37,8 @@ int main(int, char *[])
     // Testing four algorithms on a function from R^3 to R.
     {
       const SymbolicFunction f({"x", "y", "z"}, {"sin(x) * cos(y) * exp(z)"});
-      const Point lbIntegration({0., 0., 0.});
-      const Point ubIntegration({1., 1., 1.});
+      const Point lbIntegration({0.0, 0.0, 0.0});
+      const Point ubIntegration({1.0, 1.0, 1.0});
       const String routines[] = {"cuhre", "divonne", "suave", "vegas"};
       const Scalar valueRef = -sin(1.) * (cos(1.) - 1.) * (M_E - 1.);
       for (UnsignedInteger i = 0 ; i < 4 ; i++)
@@ -51,9 +51,12 @@ int main(int, char *[])
 
     // Testing three algorithms on a function from R^3 to R^2.
     {
-      const SymbolicFunction f({"x", "y", "z"}, {"exp(-(x * x + y * y + z *z))", "(x / 2 * x / 2 + y / 3 * y / 3 + z / 4 * z / 4 - 1.125)"});
-      const Point lbIntegration({0., 0., 1.});
-      const Point ubIntegration({2., 3., 4.});
+      const SymbolicFunction f({"x", "y", "z"},
+                               {"exp(-(x * x + y * y + z *z))",
+                                "(x / 2 * x / 2 + y / 3 * y / 3 + z / 4 * z / 4 - 1.125)"}
+                              );
+      const Point lbIntegration({0.0, 0.0, 1.0});
+      const Point ubIntegration({2.0, 3.0, 4.0});
       const String routines[] = {"cuhre", "divonne", "vegas"};
       const Point valueRef({0.108972129575688278, -0.375});
       for (UnsignedInteger i = 0 ; i < 3 ; i++)
