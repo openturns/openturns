9668e11ea31036759e16c898f820b34d335dbc23
Ali.Hebbal <ali.a.hebbal@airbus.com>
2024-04-02 14:53:43 +0200

    Implementation of the Least Squares Solver for a system of non-linear equations
     * C++ classes and tests
     * Python bindings + tests
     * API doc
    
    The work is a continuation of the PR proposed by A.H accounting for some
    remarks

---
+++ lib/src/Base/Solver/LeastSquaresEquationsSolver.cxx
@@ -0,0 +1,96 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief Implementation class of an unbounded solver for systems of non-linear equations based on least square optimization
+ *
+ *  Copyright 2005-2025 Airbus-EDF-IMACS-ONERA-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "openturns/LeastSquaresEquationsSolver.hxx"
+#include "openturns/LeastSquaresProblem.hxx"
+#include "openturns/Log.hxx"
+#include "openturns/OptimizationAlgorithm.hxx"
+#include "openturns/Log.hxx"
+
+#include <cmath>
+#include "openturns/PersistentObjectFactory.hxx"
+
+
+BEGIN_NAMESPACE_OPENTURNS
+
+/**
+ * @class LeastSquaresEquationsSolver
+ *
+ * This class is an interface for the nonlinear LeastSquaresEquationsSolver
+ */
+
+CLASSNAMEINIT(LeastSquaresEquationsSolver)
+
+static const Factory<LeastSquaresEquationsSolver> Factory_LeastSquaresEquationsSolver;
+
+/* Parameter constructor */
+LeastSquaresEquationsSolver::LeastSquaresEquationsSolver(const Scalar absoluteError,
+                                                         const Scalar relativeError,
+                                                         const Scalar residualError,
+                                                         const UnsignedInteger maximumCallsNumber)
+  : SolverImplementation(absoluteError, relativeError, residualError, maximumCallsNumber)
+{
+  // Nothing to do
+}
+
+/* Virtual constructor */
+LeastSquaresEquationsSolver * LeastSquaresEquationsSolver::clone() const
+{
+  return new LeastSquaresEquationsSolver(*this);
+}
+
+/* String converter */
+String LeastSquaresEquationsSolver::__repr__() const
+{
+  OSS oss;
+  oss << "class=" << LeastSquaresEquationsSolver::GetClassName()
+      << " derived from " << SolverImplementation::__repr__();
+  return oss;
+}
+
+/* Solve attempt to find one root to the system of non-linear equations function(x) = 0 given a starting point x with a least square optimization method.
+   The Least Square Optimization method is based on the DLib implementation in OpenTurns.
+*/
+Point LeastSquaresEquationsSolver::solve(const Function & function,
+                                         const Point & startingPoint) const
+{ 
+  UnsignedInteger callsNumber = 0;
+  const UnsignedInteger maximumCallsNumber = getMaximumCallsNumber();
+  const Scalar absoluteError = getAbsoluteError();
+  const Scalar relativeError = getRelativeError();
+  const Scalar residualError = getResidualError();
+  LeastSquaresProblem lsqProblem(function);
+  OptimizationAlgorithm lsqAlgorithm = OptimizationAlgorithm().Build(lsqProblem);
+  lsqAlgorithm.setStartingPoint(startingPoint);
+  lsqAlgorithm.setMaximumCallsNumber(maximumCallsNumber);
+  lsqAlgorithm.setMaximumAbsoluteError(absoluteError);
+  lsqAlgorithm.setMaximumRelativeError(relativeError);
+  lsqAlgorithm.setMaximumResidualError(residualError); 
+  lsqAlgorithm.run();
+  callsNumber = lsqAlgorithm.getResult().getCallsNumber();
+  callsNumber_ = callsNumber;
+  const Point min_value_obtained = lsqAlgorithm.getResult().getOptimalValue();
+  if (  residualError < min_value_obtained[0]) throw InternalException(HERE) << "Error: solver did not find a solution that satisfies the threshold, here obtained residual=" << min_value_obtained[0];
+  const Point result = lsqAlgorithm.getResult().getOptimalPoint();
+  return result;
+}
+
+END_NAMESPACE_OPENTURNS
---
+++ lib/src/Base/Solver/openturns/LeastSquaresEquationsSolver.hxx
@@ -0,0 +1,65 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief Implementation class of an unbounded solver for systems of non-linear equations based on least square optimization
+ *
+ *  Copyright 2005-2025 Airbus-EDF-IMACS-ONERA-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef OPENTURNS_LEASTSQUARESEQUATIONSSOLVER_HXX
+#define OPENTURNS_LEASTSQUARESEQUATIONSSOLVER_HXX
+
+#include "openturns/OTprivate.hxx"
+#include "openturns/SolverImplementation.hxx"
+
+
+BEGIN_NAMESPACE_OPENTURNS
+
+/**
+ * @class LeastSquaresEquationsSolver
+ *
+ * This class is an interface for the 1D nonlinear solverImplementations
+ */
+class OT_API LeastSquaresEquationsSolver :
+  public SolverImplementation
+{
+  CLASSNAME
+public:
+
+  /** Parameter constructor */
+  explicit LeastSquaresEquationsSolver(const Scalar absoluteError = ResourceMap::GetAsScalar("Solver-DefaultAbsoluteError"),
+                                       const Scalar relativeError = ResourceMap::GetAsScalar("Solver-DefaultRelativeError"),
+                                       const Scalar residualError = ResourceMap::GetAsScalar("Solver-DefaultResidualError"),
+                                       const UnsignedInteger maximumFunctionEvaluation = ResourceMap::GetAsUnsignedInteger("Solver-DefaultMaximumFunctionEvaluation"));
+
+
+  /** Virtual constructor */
+  LeastSquaresEquationsSolver * clone() const override;
+
+  /** String converter */
+  String __repr__() const override;
+
+  /** Solve attempt to find one root to a system of equations function(x) = 0 given a starting point x_0 */
+  using SolverImplementation::solve;
+  Point solve(const Function & function,
+              const Point & startingPoint) const override;
+
+private:
+
+}; /* Class LeastSquaresEquationsSolver */
+
+END_NAMESPACE_OPENTURNS
+
+#endif /* OPENTURNS_LsqSolver_HXX */
