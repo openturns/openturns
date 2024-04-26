e755865b6e6a8d01bf418ed09f9d0b62c673866f
Michael BAUDIN <michael.baudin@gmail.com>
2024-04-26 14:42:29 +0200

    Update MetaModelValidation_doc.i.in
    Improve example

--- python/doc/pyplots/MetaModelValidation.py
+++ python/doc/pyplots/MetaModelValidation.py
@@ -1,12 +1,26 @@
 import openturns as ot
 from openturns.viewer import View
 
+# Define the model
 dist = ot.Uniform(-3.0, 3.0)
 model = ot.SymbolicFunction(["x"], ["sin(x)"])
-metaModel = ot.SymbolicFunction(["x"], ["x - x^3 / 6.0 + x^5 / 120.0"])
-x = dist.getSample(100)
-y = model(x)
-metamodelPredictions = metaModel(x)
-val = ot.MetaModelValidation(y, metamodelPredictions)
+
+# Build the metamodel
+x_train = dist.getSample(25)
+y_train = model(x_train)
+total_degree = 3
+polynomialCollection = ["x^%d" % (degree) for degree in range(1, total_degree + 1)]
+basis = ot.SymbolicFunction(["x"], polynomialCollection)
+designMatrix = basis(x_train)
+myLeastSquares = ot.LinearLeastSquares(designMatrix, y_train)
+myLeastSquares.run()
+leastSquaresModel = myLeastSquares.getMetaModel()
+metaModel = ot.ComposedFunction(leastSquaresModel, basis)
+
+# Validate the metamodel
+x_test = dist.getSample(100)
+y_test = model(x_test)
+metamodelPredictions = metaModel(x_test)
+val = ot.MetaModelValidation(y_test, metamodelPredictions)
 graph = val.drawValidation()
 View(graph)
