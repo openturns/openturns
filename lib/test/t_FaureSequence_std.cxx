08235e3714b9b9e6739dc403508c8424eafec3bc
Michael BAUDIN <michael.baudin@gmail.com>
2024-05-12 22:02:32 +0200

    Improve unit tests of Faure sequence

--- lib/test/t_FaureSequence_std.cxx
+++ lib/test/t_FaureSequence_std.cxx
@@ -31,31 +31,74 @@ int main(int, char *[])
 
   try
   {
-    // Create a Faure sequence
+    // Create 8 points from a Faure sequence in dimension 1
+    Sample expected1D(8, 1);
+    expected1D[0] = Point({1.0 / 2.0});
+    expected1D[1] = Point({1.0 / 4.0});
+    expected1D[2] = Point({3.0 / 4.0});
+    expected1D[3] = Point({1.0 / 8.0});
+    expected1D[4] = Point({5.0 / 8.0});
+    expected1D[5] = Point({3.0 / 8.0});
+    expected1D[6] = Point({7.0 / 8.0});
+    expected1D[7] = Point({1.0 / 16.0});
+    FaureSequence sequence1D(1);
+    fullprint << sequence1D << std::endl;
+    Sample faureSample1D(sequence1D.generate(8));
+    assert_almost_equal(faureSample1D, expected1D);
+
+    // Create 8 points from a Faure sequence in dimension 2
+    Sample expected2D(8, 2);
+    expected2D[0] = Point({0.5, 0.5});
+    expected2D[1] = Point({0.25, 0.75});
+    expected2D[2] = Point({0.75, 0.25});
+    expected2D[3] = Point({0.125, 0.625});
+    expected2D[4] = Point({0.625, 0.125});
+    expected2D[5] = Point({0.375, 0.375});
+    expected2D[6] = Point({0.875, 0.875});
+    expected2D[7] = Point({0.0625, 0.9375});
+    FaureSequence sequence2D(2);
+    fullprint << sequence2D << std::endl;
+    Sample faureSample2D(sequence2D.generate(8));
+    assert_almost_equal(faureSample2D, expected2D);
+
+    // Create 8 points from a Faure sequence in dimension 3
+    Sample expected3D(8, 3);
+    expected3D[0] = Point({1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0});
+    expected3D[1] = Point({2.0 / 3.0, 2.0 / 3.0, 2.0 / 3.0});
+    expected3D[2] = Point({1.0 / 9.0, 4.0 / 9.0, 7.0 / 9.0});
+    expected3D[3] = Point({4.0 / 9.0, 7.0 / 9.0, 1.0 / 9.0});
+    expected3D[4] = Point({7.0 / 9.0, 1.0 / 9.0, 4.0 / 9.0});
+    expected3D[5] = Point({2.0 / 9.0, 8.0 / 9.0, 5.0 / 9.0});
+    expected3D[6] = Point({5.0 / 9.0, 2.0 / 9.0, 8.0 / 9.0});
+    expected3D[7] = Point({8.0 / 9.0, 5.0 / 9.0, 2.0 / 9.0});
+    FaureSequence sequence3D(3);
+    fullprint << sequence3D << std::endl;
+    Sample faureSample3D(sequence3D.generate(8));
+    assert_almost_equal(faureSample3D, expected3D);
+
+    // Create a Faure sequence in dimension 15
     FaureSequence sequence(15);
     fullprint << sequence << std::endl;
-
-    // Create a numerical sample of the sequence
-    Sample faureSample(sequence.generate(10));
-    fullprint << faureSample << std::endl;
+    sequence.generate(10);
 
     // Create another Faure' sequence of dimension 2 to estimate Pi in [0; 1)^2
     UnsignedInteger dimension = 2;
     sequence = FaureSequence(dimension);
     UnsignedInteger pointInsideCircle = 0;
-    UnsignedInteger sampleSize = 1000;
+    UnsignedInteger sampleSize = std::pow(3, 7);  // This is significant!
     for(UnsignedInteger i = 0; i < sampleSize; ++i)
     {
       Point faurePoint(sequence.generate());
-      fullprint << faurePoint << std::endl;
       if(faurePoint.norm() < 1.0)
         ++ pointInsideCircle;
     }
     Scalar probabilityEstimate = 1.0 * pointInsideCircle / sampleSize;
     Scalar probability = M_PI / 4.0;
-    Scalar relativeError = std::abs(probability - probabilityEstimate) / probability;
     fullprint << "sample size=" << sampleSize << std::endl;
-    fullprint << "relative error to Pi=" << relativeError << std::endl;
+    fullprint << "computed probability =" << probabilityEstimate << std::endl;
+    fullprint << "expected probability =" << probability << std::endl;
+    Scalar rtol = 10.0 / sampleSize;
+    assert_almost_equal(probabilityEstimate, probability, rtol);
   }
 
   catch (TestFailed & ex)
--- python/test/t_FaureSequence_std.py
+++ python/test/t_FaureSequence_std.py
@@ -1,31 +1,121 @@
 #! /usr/bin/env python
 
 import openturns as ot
+import openturns.testing as ott
 import math as m
 
 ot.TESTPREAMBLE()
 
 
-# Create a Faure
+# First 8 points of Faure sequence in dimension 1
+expected = ot.Sample(
+    [
+        [1.0 / 2.0],
+        [1.0 / 4.0],
+        [3.0 / 4.0],
+        [1.0 / 8.0],
+        [5.0 / 8.0],
+        [3.0 / 8.0],
+        [7.0 / 8.0],
+        [1.0 / 16.0],
+    ]
+)
+sequence = ot.FaureSequence(1)
+print(sequence)
+faureSample = sequence.generate(8)
+ott.assert_almost_equal(faureSample, expected)
+
+# First 8 points of Faure sequence in dimension 2
+expected = ot.Sample(
+    [
+        [0.5, 0.5],
+        [0.25, 0.75],
+        [0.75, 0.25],
+        [0.125, 0.625],
+        [0.625, 0.125],
+        [0.375, 0.375],
+        [0.875, 0.875],
+        [0.0625, 0.9375],
+    ]
+)
+sequence = ot.FaureSequence(2)
+print(sequence)
+faureSample = sequence.generate(8)
+ott.assert_almost_equal(faureSample, expected)
+
+# First 8 points of Faure sequence in dimension 3
+expected = ot.Sample(
+    [
+        [1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0],
+        [2.0 / 3.0, 2.0 / 3.0, 2.0 / 3.0],
+        [1.0 / 9.0, 4.0 / 9.0, 7.0 / 9.0],
+        [4.0 / 9.0, 7.0 / 9.0, 1.0 / 9.0],
+        [7.0 / 9.0, 1.0 / 9.0, 4.0 / 9.0],
+        [2.0 / 9.0, 8.0 / 9.0, 5.0 / 9.0],
+        [5.0 / 9.0, 2.0 / 9.0, 8.0 / 9.0],
+        [8.0 / 9.0, 5.0 / 9.0, 2.0 / 9.0],
+    ]
+)
+sequence = ot.FaureSequence(3)
+print(sequence)
+faureSample = sequence.generate(8)
+ott.assert_almost_equal(faureSample, expected)
+
+# Create a Faure sequence in dimension 15
 sequence = ot.FaureSequence(15)
 print(sequence)
-
-# Create a numerical sample of the sequence
 faureSample = sequence.generate(10)
-print(repr(faureSample))
 
 # Create another Faure' sequence of dimension 2 to estimate Pi in [0 1)^2
 dimension = 2
 sequence = ot.FaureSequence(dimension)
 pointInsideCircle = 0
-sampleSize = 1000
+sampleSize = 3 ** 7  # This is significant!
 for i in range(sampleSize):
     faurePoint = sequence.generate()
-    print(faurePoint.__repr__())
     if faurePoint.norm() < 1.0:
-        pointInsideCircle = pointInsideCircle + 1
+        pointInsideCircle += 1
 probabilityEstimate = (1.0 * pointInsideCircle) / sampleSize
 probability = m.pi / 4.0
-relativeError = abs(probability - probabilityEstimate) / probability
 print("sample size=", sampleSize)
-print("relative error to Pi=%e" % relativeError)
+print("computed probability =", probabilityEstimate)
+print("expected probability =", probability)
+rtol = 10.0 / sampleSize
+ott.assert_almost_equal(probability, probabilityEstimate, rtol)
+
+# Test against GSobol' test function
+# https://github.com/openturns/openturns/issues/2653
+a = [0.0, 9.0, 99.0]
+
+
+def GSobolModel(X):
+    X = ot.Point(X)
+    d = X.getDimension()
+    Y = 1.0
+    for i in range(d):
+        Y *= (abs(4.0 * X[i] - 2.0) + a[i]) / (1.0 + a[i])
+    return ot.Point([Y])
+
+
+dimension = 3
+gSobolFunction = ot.PythonFunction(dimension, 1, GSobolModel)
+gSobolFunction.setOutputDescription(["Y"])
+
+# Define the distribution
+distributionList = [ot.Uniform(0.0, 1.0) for i in range(dimension)]
+distribution = ot.ComposedDistribution(distributionList)
+
+# Compute Mean
+gSobolMean = 1.0
+basis = 3  # ie the smallest prime number greater or equal to the dimension
+sampleSize = basis**7  # This is significant!
+sequence = ot.FaureSequence()
+experiment_QMC = ot.LowDiscrepancyExperiment(sequence, distribution, sampleSize, False)
+inputSample = experiment_QMC.generate()
+outputSample = gSobolFunction(inputSample)
+computedMean = outputSample.computeMean()[0]
+print("sample size=", sampleSize)
+print("computed mean =", computedMean)
+print("expected mean =", gSobolMean)
+rtol = 10.0 / sampleSize
+ott.assert_almost_equal(computedMean, gSobolMean, rtol)
