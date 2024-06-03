3bc622f857f2c09f6a6db63314833b1e56807d4e
MerlinKeller <43726507+MerlinKeller@users.noreply.github.com>
2024-06-03 10:47:09 +0200

    Mcmc unitary tests

--- lib/test/t_RandomVectorMetropolisHastings_std.cxx
+++ lib/test/t_RandomVectorMetropolisHastings_std.cxx
@@ -1,6 +1,6 @@
 //                                               -*- C++ -*-
 /**
- *  @brief The test file of class IndependentMetropolisHastings
+ *  @brief The test file of class RandomVectorMetropolisHastings
  *
  *  Copyright 2005-2024 Airbus-EDF-IMACS-ONERA-Phimeca
  *
@@ -34,7 +34,7 @@ int main(int, char *[])
 
   try
   {
-    // Test IndependentMetropolisHastings on Beta-Binomial conjugate model
+    // Test RandomVectorMetropolisHastings on Beta-Binomial conjugate model
 
     // Define Beta-binomial model
     Scalar a = 1.0;
@@ -48,21 +48,19 @@ int main(int, char *[])
     Binomial model(n, p);
 
     // Simulate data and compute analytical posterior
-    // cf. Wikipedia table of conjugate distributions
-    // https://en.wikipedia.org/wiki/Conjugate_prior#Table_of_conjugate_distributions
     Sample X(model.getSample(1));
     Beta posterior(a + X(0,0), b + n - X(0,0), lower, upper);
 
     // Define IMH sampler
-    IndependentMetropolisHastings imh_sampler(prior, {p}, Uniform(-1.0, 1.0), Indices(1));
+    IndependentMetropolisHastings rvmh_sampler(prior, {p}, Uniform(-1.0, 1.0), Indices(1));
     Description inVar({"x"});
     Description outVar({std::to_string(n), "x"});
     SymbolicFunction slf(inVar, outVar);
-    imh_sampler.setLikelihood(model, X, slf);
+    rvmh_sampler.setLikelihood(model, X, slf);
 
     // Generate posterior distribution sample
     UnsignedInteger N = 10000;
-    Sample Xsample(imh_sampler.getSample(N));
+    Sample Xsample(rvmh_sampler.getSample(N));
 
     // Compare empirical to theoretical moments
     assert_almost_equal(Xsample.computeMean(), posterior.getMean(), 0.0, 10.0 / std::sqrt(N));
@@ -70,39 +68,19 @@ int main(int, char *[])
 
     // End Beta-Binomial conjugate model test
 
-    RandomGenerator::SetSeed(1);
-    const Point lower_bound(1);
-    const Point upper_bound({2.0 * M_PI});
-    JointDistribution::DistributionCollection distributionCollection(2);
-    distributionCollection[0] = Exponential(1.0);
-    distributionCollection[1] = Normal(5.3, 0.4);
-    const Point weights({0.9, 0.1});
-    const Mixture instrumentalDistribution(distributionCollection, weights);
+    // Elementary test
+    Normal normal;
+    const Normal & ref_normal(normal);
+    RandomVector randomVector(ref_normal);
+    RandomVectorMetropolisHastings sampler(randomVector, {0.0});
+    Sample x(sampler.getSample(N));
+    Point mean(x.computeMean());
+    Point stddev(x.computeStandardDeviation());
+    assert_almost_equal(mean, {0.0}, 0.0, 0.03);
+    assert_almost_equal(stddev, {1.0}, 0.03, 0.0);
 
-    // MetropolisHastings classes expect to receive the logarithm to the target density.
-    const SymbolicFunction log_density ("x", "log(2 + sin(x)^2) - (2 + cos(3*x)^3 + sin(2*x)^3) * x");
-
-    const Point initialState({3.0});  // not important in this case
-    const Interval support(lower_bound, upper_bound);
-    IndependentMetropolisHastings independentMH(log_density, support, initialState, instrumentalDistribution, {0});
-
-    // Get a sample
-    const Scalar sampleSize = 1000000;
-    const Sample sample(independentMH.getSample(sampleSize));
-
-
-    // Compute posterior mean
-
-    const Scalar mean_ref = 1.22498;
-    const Scalar posterior_mean = sample.computeMean()[0];
-
-    assert_almost_equal(posterior_mean, mean_ref, 1e-5, 0.0);
-
-    const Scalar std_ref = 1.61558;
-    const Scalar posterior_std = sample.computeStandardDeviation()[0];
-
-    assert_almost_equal(posterior_std, std_ref, 1e-5, 0.0);
-
+    // with link function
+    
   }
   catch (TestFailed & ex)
   {
