eed447fff9e43aa8bbebdf9ad1ea6bc7de7286af
adutfoy <anne.dutfoy@m4x.org>
2024-06-24 19:51:46 +0200

    Update python/doc/pyplots/DrawParallelCoordinates.py
    Co-authored-by: Michael Baudin <31351465+mbaudin47@users.noreply.github.com>

--- python/doc/pyplots/DrawParallelCoordinates.py
+++ python/doc/pyplots/DrawParallelCoordinates.py
@@ -22,5 +22,4 @@ cobweb = ot.VisualTest.DrawParallelCoordinates(
 
 cobweb.setLegendCorner([1.0, 0.0])
 cobweb.setLegendPosition('lower right')
-#View(cobweb, figure_kw={"figsize": (10, 6)}, legend_kw={"loc": "lower right"})
 View(cobweb, figure_kw={"figsize": (9, 5)})
