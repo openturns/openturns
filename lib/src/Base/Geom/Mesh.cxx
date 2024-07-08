67a4392767b7301e351c0ebce79f6ff5216b663d
Julien Schueller <schueller@phimeca.com>
2024-07-08 18:16:11 +0200

    Mesh: Initialize attributes

--- lib/src/Base/Geom/Mesh.cxx
+++ lib/src/Base/Geom/Mesh.cxx
@@ -56,9 +56,7 @@ static const Factory<Mesh> Factory_Mesh;
 Mesh::Mesh(const UnsignedInteger dimension)
   : PersistentObject()
   , dimension_(dimension)
-  , hasBeenChecked_(false)
   , vertices_(1, dimension) // At least one point
-  , simplices_()
 {
   // Nothing to do
   if (vertices_.getDescription().isBlank()) vertices_.setDescription(Description::BuildDefault(dimension, "t"));
@@ -68,9 +66,7 @@ Mesh::Mesh(const UnsignedInteger dimension)
 Mesh::Mesh(const Sample & vertices)
   : PersistentObject()
   , dimension_(vertices.getDimension())
-  , hasBeenChecked_(false)
   , vertices_(0, vertices.getDimension())
-  , simplices_()
 {
   // Use the vertices accessor to initialize the kd-tree
   setVertices(vertices);
@@ -82,7 +78,6 @@ Mesh::Mesh(const Sample & vertices,
            const Bool checkMeshValidity)
   : PersistentObject()
   , dimension_(vertices.getDimension())
-  , hasBeenChecked_(false)
   , vertices_(0, vertices.getDimension())
   , simplices_(simplices)
 {
