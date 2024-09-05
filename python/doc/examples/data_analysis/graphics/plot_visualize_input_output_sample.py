c76eb9d088b9427244b1a57d3dff867abf353b1a
Mathieu Balesdent <mathieubalesdent@gmail.com>
2024-09-05 15:35:28 +0200

    Modification of index

--- python/doc/examples/data_analysis/graphics/plot_visualize_input_output_sample.py
+++ python/doc/examples/data_analysis/graphics/plot_visualize_input_output_sample.py
@@ -4,9 +4,11 @@ Visualize pairs between two samples
 """
 
 # %%
-# In this example we are going to a visualize the relationships between the marginal samples of two given samples. This is usual when we analyze the relationship of output sample of a model depending on input sample.
+# In this example we are going to a visualize the relationships between the marginal samples of two given samples. 
+# This is usual when we analyze the relationship of output sample of a model depending on input sample.
 # This can be achieved by plotting the outputs versus the inputs.
-# When there are several inputs (which is the most often encountered case) and several outputs (which is less often), the :meth:`~openturns.VisualTest.DrawPairs` method provides a tool to plot the pairs of input and output marginal samples and see the correlations graphically.
+# When there are several inputs (which is the most often encountered case) and several outputs (which is less often).
+# The :meth:`~openturns.VisualTest.DrawPairs` method provides a tool to plot the pairs of input and output marginal samples and see the correlations graphically.
 # %%
 import openturns as ot
 import openturns.viewer as viewer
