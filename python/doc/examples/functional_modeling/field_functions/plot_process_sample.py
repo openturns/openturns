ac9da0c6f72ec02164fe9b54e64489a8fd73f53c
Loic Brevault <loic.brevault@onera.fr>
2024-09-05 13:48:51 +0200

    Correction of whitespaces

--- python/doc/examples/functional_modeling/field_functions/plot_process_sample.py
+++ python/doc/examples/functional_modeling/field_functions/plot_process_sample.py
@@ -7,7 +7,7 @@ Create a process sample from a sample
 # In this example, a :class:`~openturns.ProcessSample` is created from a sample.
 # The purpose is to illustrate how to create a :class:`~openturns.ProcessSample` from an already available sample of a field.
 # In addition, the computation of statistics of the process sample is presented.
-# The :class:`~openturns.ProcessSample` is defined over a :class:`~openturns.Mesh`.  
+# The :class:`~openturns.ProcessSample` is defined over a :class:`~openturns.Mesh`.
 #
 # For this example, the realizations of a stochastic process are obtained using the Chaboche model.
 # A detailed explanation of this mechanical law is presented :ref:`here <use-case-chaboche>`.
@@ -26,7 +26,6 @@ Create a process sample from a sample
 import openturns as ot
 import openturns.viewer as viewer
 from openturns.usecases import chaboche_model
-import numpy as np
 
 ot.Log.Show(ot.Log.NONE)
 
@@ -53,12 +52,13 @@ vertices = mesh.getVertices()
 # --------------------------------
 
 # %%
-# One sample of the field is obtained by evaluating the Chaboche model on the mesh vertices for one specific realization of the input vector :math:`\vect{X}_0=[700\times 10^6,2500\times 10^6,8.]^T`.
+# One sample of the field is obtained by evaluating the Chaboche model on the mesh vertices
+# for one specific realization of the input vector :math:`\vect{X}_0=[700\times 10^6,2500\times 10^6,8.]^T`.
 
 # %%
-R = 700e6  
-C = 2500e6  
-gamma = 8.0 
+R = 700e6
+C = 2500e6
+gamma = 8.0
 X0 = [R, C, gamma]
 X_indices = [1, 2, 3]
 f = ot.ParametricFunction(cm.model, X_indices, X0)
@@ -69,7 +69,7 @@ Y = [f(vertices[i])[0] for i in range(N)]
 
 # %%
 graph = ot.Graph("One realization of the stochastic process", "Strain", "Stress (Pa)", True, "")
-curve = ot.Curve(vertices.asPoint(),Y)
+curve = ot.Curve(vertices.asPoint(), Y)
 graph.add(curve)
 viewer.View(graph)
 
@@ -78,10 +78,10 @@ viewer.View(graph)
 
 # %%
 R_distribution = ot.LogNormal()
-R_distribution.setParameter(ot.LogNormalMuSigma()([750e6,11e6, 0.0]))
-C_distribution = ot.Normal(2750e6,250e6)
-gamma_distribution = ot.Normal(10.,2.)
-X_distribution = ot.JointDistribution([R_distribution,C_distribution,gamma_distribution])
+R_distribution.setParameter(ot.LogNormalMuSigma()([750e6, 11e6, 0.0]))
+C_distribution = ot.Normal(2750e6, 250e6)
+gamma_distribution = ot.Normal(10., 2.)
+X_distribution = ot.JointDistribution([R_distribution, C_distribution, gamma_distribution])
 
 # %%
 # Generate different samples of the field
@@ -100,7 +100,7 @@ X_samples = X_distribution.getSample(n_samples)
 # %%
 Y_list = []
 for i in range(n_samples):
-    f = ot.ParametricFunction(cm.model, X_indices, X_samples[i,:])
+    f = ot.ParametricFunction(cm.model, X_indices, X_samples[i, :])
     Y = [[f(vertices[i])[0]] for i in range(N)]
     Y_list.append(Y)
 
@@ -110,7 +110,7 @@ for i in range(n_samples):
 # %%
 graph = ot.Graph("Realizations of the stochastic process", "Strain", "Stress (Pa)", True, "")
 for i in range(n_samples):
-    curve = ot.Curve(vertices,Y_list[i])
+    curve = ot.Curve(vertices, Y_list[i])
     curve.setColor("blue")
     graph.add(curve)
 viewer.View(graph)
@@ -121,10 +121,12 @@ viewer.View(graph)
 
 # %%
 # It is possible to create a :class:`~openturns.ProcessSample` from the obtained field samples.
-# For that, each obtained sample is added to the :class:`~openturns.ProcessSample` using the :class:`~openturns.Field` structure. When the :class:`~openturns.ProcessSample` is created, by default a process sample with a value of 0. for all the vertices is stored so it is important to remove it.
+# For that, each obtained sample is added to the :class:`~openturns.ProcessSample` using the :class:`~openturns.Field` structure. 
+# When the :class:`~openturns.ProcessSample` is created, by default a process sample with a value of 0. 
+# for all the vertices is stored so it is important to remove it.
 
 # %%
-process_sample = ot.ProcessSample(mesh,n_samples,1)
+process_sample = ot.ProcessSample(mesh, n_samples, 1)
 process_sample.clear()
 for i in range(n_samples):
     process_sample.add(ot.Field(mesh, Y_list[i]))
@@ -133,8 +135,8 @@ for i in range(n_samples):
 # It is then possible to compute different statistics on the :class:`~openturns.ProcessSample` such as the mean, median, variance, ...
 
 # %%
-process_sample_mean     = process_sample.computeMean()
-process_sample_median   = process_sample.computeMedian()
+process_sample_mean = process_sample.computeMean()
+process_sample_median = process_sample.computeMedian()
 process_sample_variance = process_sample.computeVariance()
 
 # %%
@@ -144,15 +146,15 @@ process_sample_variance = process_sample.computeVariance()
 graph = ot.Graph("Sample process mean and realizations", "Strain", "Stress (Pa)", True, "")
 for i in range(n_samples):
     if i==0:
-        curve = ot.Curve(vertices,Y_list[i],"process samples")
+        curve = ot.Curve(vertices, Y_list[i], "process samples")
         curve.setColor("blue")
         graph.add(curve)
     else:
-        curve = ot.Curve(vertices,Y_list[i])
+        curve = ot.Curve(vertices, Y_list[i])
         curve.setColor("blue")
         graph.add(curve) 
 
-curve = ot.Curve(vertices,process_sample_mean,"process sample mean")
+curve = ot.Curve(vertices, process_sample_mean, "process sample mean")
 curve.setColor("red")
 graph.add(curve)
 graph.setLegendPosition("topleft")
