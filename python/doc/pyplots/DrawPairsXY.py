8b8057c4f5c5c8a4752471cc320a32f414daf295
Mathieu Balesdent <mathieubalesdent@gmail.com>
2024-09-18 14:18:49 +0200

    Whitespace typo fix

--- python/doc/pyplots/DrawPairsXY.py
+++ python/doc/pyplots/DrawPairsXY.py
@@ -5,7 +5,7 @@ ot.RandomGenerator.SetSeed(0)
 
 sampleX = ot.Normal(2).getSample(100)
 
-f = ot.SymbolicFunction(['x1','x2'],['x1 + x2','2 * x1 - x2'])
+f = ot.SymbolicFunction(['x1', 'x2'], ['x1 + x2', '2 * x1 - x2'])
 
 sampleY = f(sampleX)
 
