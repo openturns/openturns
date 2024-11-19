c8b2aaefb3f02f0483dff243d554fbbd5293fcdc
josephmure <joseph.mure@edf.fr>
2024-11-19 09:05:18 +0100

    Shorten DrawInsideOutside title

--- python/doc/pyplots/DrawInsideOutside.py
+++ python/doc/pyplots/DrawInsideOutside.py
@@ -4,11 +4,9 @@ from openturns.viewer import View
 ot.RandomGenerator.SetSeed(0)
 
 domain = ot.Interval([-2.0, -1.0, 1.0], [2.0, 1.0, 3.0])
-domain.setName("Cuboid")
 uniform = ot.Uniform(-3.0, 4.0)
 dist = ot.JointDistribution([uniform] * 3)
 sample = dist.getSample(200)
-sample.setName("Uniform")
 grid = ot.VisualTest.DrawInsideOutside(domain, sample)
 
 View(grid, figure_kw={"figsize": (4.5, 4.5)})
