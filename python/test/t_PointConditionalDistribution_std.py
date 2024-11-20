571941de3c476b08c0427dd4210f4b241c3203a8
Julien Schueller <schueller@phimeca.com>
2024-11-20 15:16:02 +0100

    PointConditionalD: Check simplified cases

--- python/test/t_PointConditionalDistribution_std.py
+++ python/test/t_PointConditionalDistribution_std.py
@@ -70,7 +70,15 @@ normal = ot.Normal([0.0] * 3, R)
 distribution = otexp.PointConditionalDistribution(normal, [1], [2.0])
 simplified = distribution.getSimplifiedVersion()
 print(simplified)
-ott.assert_almost_equal(simplified.getMarginal(0), ot.Normal(1.8, 0.43589))
+simplified_ref = ot.Normal([1.8] * 2, [0.43589] * 2, ot.CorrelationMatrix([[1.0, 0.473684], [0.473684, 1.0]]))
+ott.assert_almost_equal(simplified, simplified_ref)
+
+# ensure services are ok
+mean_ref = simplified_ref.getMean()
+ott.assert_almost_equal(distribution.getMean(), mean_ref)
+ott.assert_almost_equal(distribution.getStandardDeviation(), simplified_ref.getStandardDeviation())
+ott.assert_almost_equal(distribution.computePDF(mean_ref), simplified_ref.computePDF(mean_ref))
+ott.assert_almost_equal(distribution.computeCDF(mean_ref), simplified_ref.computeCDF(mean_ref))
 
 # special case for Student
 student = ot.Student(2.5, [0.4] * 3, [1.2] * 3, R)
@@ -78,6 +86,7 @@ distribution = otexp.PointConditionalDistribution(student, [1], [2.0])
 simplified = distribution.getSimplifiedVersion()
 print(simplified)
 ott.assert_almost_equal(simplified.getMarginal(0), ot.Student(3.5, 1.84, 1.07564))
+ott.assert_almost_equal(distribution.computePDF(distribution.getMean()), 0.156194)
 
 # special case for JointDistribution
 R = ot.CorrelationMatrix(3)
@@ -88,6 +97,7 @@ distribution = otexp.PointConditionalDistribution(joint, [1], [0.8])
 simplified = distribution.getSimplifiedVersion()
 print(simplified)
 assert simplified.getName() == "JointDistribution", "wrong type"
+ott.assert_almost_equal(distribution.computePDF(distribution.getMean()), 0.266138)
 
 # special case for Mixture
 mixture = ot.Mixture([ot.Normal(2), ot.Normal(2)], [0.3, 0.7])
@@ -95,6 +105,7 @@ distribution = otexp.PointConditionalDistribution(mixture, [1], [2.0])
 simplified = distribution.getSimplifiedVersion()
 print(simplified)
 assert simplified.getName() == "Mixture", "wrong type"
+ott.assert_almost_equal(distribution.computePDF(distribution.getMean()), 0.398942)
 
 # special case for KernelMixture
 kernel = ot.Uniform()
@@ -105,6 +116,7 @@ distribution = otexp.PointConditionalDistribution(kernelMixture, [1], [1.0])
 simplified = distribution.getSimplifiedVersion()
 print(simplified)
 assert simplified.getName() == "Mixture", "wrong type"
+ott.assert_almost_equal(distribution.computePDF([0]), 0.25)
 
 # ratio of uniform method initialization is harder for Dirichlet
 R = ot.CorrelationMatrix(3, [1.0, 0.5, 0.1, 0.5, 1.0, 0.2, 0.1, 0.2, 1.0])
@@ -128,12 +140,14 @@ distribution = otexp.PointConditionalDistribution(blockIndep, [1, 3, 0, 2], [0.5
 simplified = distribution.getSimplifiedVersion()
 print(simplified)
 assert simplified.getName() == "BlockIndependentDistribution", "wrong type"
+ott.assert_almost_equal(distribution.computePDF(distribution.getMean()), 0.0248766)
 # test a conditioning which remove all but the last block
 # As the last block can be simplified, it is its actual simplification which is used
 distribution = otexp.PointConditionalDistribution(blockIndep, [0, 1, 2, 3, 4, 5, 6], [0.5] * 7)
 simplified = distribution.getSimplifiedVersion()
 print(simplified)
 assert simplified.getName() == "Mixture", "wrong type"
+ott.assert_almost_equal(distribution.computePDF(distribution.getMean()), 0.398942)
 
 # special case for BlockIndependentCopula
 R = ot.CorrelationMatrix(3)
@@ -144,3 +158,9 @@ distribution = otexp.PointConditionalDistribution(blockIndep, [1, 3], [0.4, 0.5]
 simplified = distribution.getSimplifiedVersion()
 print(simplified)
 assert simplified.getName() == "BlockIndependentDistribution", "wrong type"
+ott.assert_almost_equal(distribution.computePDF(distribution.getMean()), 9.01202)
+
+# negative bound sampling case
+ot.ResourceMap.SetAsBool("PointConditionalDistribution-UseSimplifiedVersion", False)
+distribution = otexp.PointConditionalDistribution(copula - 1.0, [1], [-0.8])
+ott.assert_almost_equal(distribution.getSample(1000).computeMean(), [-0.75] * 2, 1e-2, 1e-2)
