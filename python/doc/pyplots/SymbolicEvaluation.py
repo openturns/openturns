35049bd752223f76e84a07db192fdc7d5efd39c1
Anne Dutfoy <anne.dutfoy@m4x.org>
2024-11-21 22:49:37 +0100

    Removed that file: transferred to SymbolicFunction

--- python/doc/pyplots/SymbolicEvaluation.py
+++ python/doc/pyplots/SymbolicEvaluation.py
@@ -1,7 +1,7 @@
 import openturns as ot
 from openturns.viewer import View
 
-f = ot.SymbolicFunction(['x'], ['sin(pi_*sin(pi_*x))'])
+f = ot.SymbolicEvaluation(['x'], ['sin(pi_*sin(pi_*x))'])
 graph = ot.Graph(r'Function $x  \mapsto \sin(\pi \sin(\pi x))$', r'$x$', r'$y$', True)
 graph.add(f.draw(-0.5, 3., 1024))
 
