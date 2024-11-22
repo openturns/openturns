923ae15540a2b61905313fc6224616519afe3ee8
Julien Schueller <schueller@phimeca.com>
2024-11-22 14:36:02 +0100

    DistFunc: Restore numerical bound
    Closes #2723

--- python/test/t_DistFunc_normal.py
+++ python/test/t_DistFunc_normal.py
@@ -13,27 +13,19 @@ grid = [0.0] * nX
 for iX in range(nX):
     x = xMin + (xMax - xMin) * iX / (nX - 1)
     grid[iX] = x
-    print(
-        "pNormal( %.12g" % x,
-        ")=%.6g" % ot.DistFunc.pNormal(x),
-        ", complementary=%.6g" % ot.DistFunc.pNormal(x, True),
-    )
+    print(f"pNormal({x:.12g})={ot.DistFunc.pNormal(x):.6g}, complementary={ot.DistFunc.pNormal(x, True):.6g}")
 print("pNormal(", grid, "=", ot.DistFunc.pNormal(grid))
 # qNormal
-qMin = 0.1
-qMax = 0.9
-nQ = 10
+qMin = 0.0
+qMax = 1.0
+nQ = 11
 grid = [0.0] * nQ
 for iQ in range(nQ):
     q = qMin + (qMax - qMin) * iQ / (nQ - 1)
     grid[iQ] = q
-    print(
-        "qNormal( %.12g" % q,
-        ")=%.6g" % ot.DistFunc.qNormal(q),
-        ", complementary=%.6g" % ot.DistFunc.qNormal(q, True),
-    )
+    print(f"qNormal({q:.12g})={ot.DistFunc.qNormal(q):.6g}, complementary={ot.DistFunc.qNormal(q, True):.6g}")
 print("qNormal(", grid, ")=", ot.DistFunc.qNormal(grid))
 # rNormal
 nR = 10
 for iR in range(nR):
-    print("rNormal()=%.6g" % ot.DistFunc.rNormal())
+    print(f"rNormal()={ot.DistFunc.rNormal():.6g}")
