ece5263711dbb0f7c6fce808871cbdca9852e502
Anne Dutfoy <anne.dutfoy@m4x.org>
2024-11-24 19:03:33 +0100

    Fixed blanks

--- python/doc/pyplots/SymbolicFunction.py
+++ python/doc/pyplots/SymbolicFunction.py
@@ -5,4 +5,4 @@ f = ot.SymbolicFunction(['x'], ['sin(pi_*sin(pi_*x))'])
 graph = ot.Graph(r'Function $x  \mapsto \sin(\pi \sin(\pi x))$', r'$x$', r'$y$', True)
 graph.add(f.draw(-0.5, 3., 1024))
 
-view = View(graph, figure_kw={"figsize": (10, 4)}, add_legend = False)
+view = View(graph, figure_kw={"figsize": (10, 4)}, add_legend=False)
