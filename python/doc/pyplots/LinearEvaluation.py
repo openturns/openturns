5a93c12dea0ec1b1160a8e5b06a91599d3149717
Anne Dutfoy <anne.dutfoy@m4x.org>
2024-12-05 11:31:37 +0100

    Removed dependence to matplotlib in LinearEvaluation.py

--- python/doc/pyplots/LinearEvaluation.py
+++ python/doc/pyplots/LinearEvaluation.py
@@ -1,5 +1,4 @@
 import openturns as ot
-from matplotlib import pyplot as plt
 from openturns.viewer import View
 
 A = ot.Matrix(2, 1)
@@ -13,7 +12,4 @@ graph = func.draw([0.0, 0.0], [4.0, 4.0], [64] * 2)
 graph.setTitle(r'Linear function $(x,y) \mapsto (x-2) + 2(y-3) + 1.5$')
 graph.setXTitle(r'$x$')
 graph.setYTitle(r'$y$')
-
-fig = plt.figure(figsize=(10, 4))
-func_axis = fig.add_subplot(111)
-view = View(graph, figure=fig, axes=[func_axis], add_legend=False)
+View(graph, figure_kw={"figsize": (10, 4)}, add_legend=True)
