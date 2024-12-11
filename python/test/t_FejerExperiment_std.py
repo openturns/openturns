e52c024b34eabe511dacd403f06d7efa8893d5f9
Julien Schueller <schueller@phimeca.com>
2024-12-11 11:33:13 +0100

    FejerExperiment: Add nesting level support

--- python/test/t_FejerExperiment_std.py
+++ python/test/t_FejerExperiment_std.py
@@ -13,11 +13,38 @@ all_rules = [
 ]
 
 for rule in all_rules:
-    experiment = otexp.FejerExperiment([3], rule)
-    experiment.setBounds(ot.Interval(4.0, 5.0))
-    nodes, weights = experiment.generateWithWeights()
-    print(rule, nodes, weights)
-    assert len(nodes) == len(weights)
-    assert not experiment.hasUniformWeights()
-    ott.assert_almost_equal(nodes.computeMean(), [4.5])
-    ott.assert_almost_equal(sum(weights), 2.0)
+    for N in [1, 2, 3, 4, 5]:
+        experiment = otexp.FejerExperiment([N], rule)
+        experiment.setBounds(ot.Interval(4.0, 5.0))
+        nodes, weights = experiment.generateWithWeights()
+        print(f"---------- rule={rule} N={N} ----------")
+        print(rule, nodes, weights)
+        assert len(nodes) == N
+        assert len(weights) == N
+        assert experiment.hasUniformWeights() == (N < 2)
+        ott.assert_almost_equal(nodes.computeMean(), [4.5])
+        ott.assert_almost_equal(sum(weights), 2.0)
+
+# check nesting
+mapN = {otexp.FejerExperiment.FEJERTYPE2: lambda n: 2 ** n - 1}
+mapN[otexp.FejerExperiment.CLENSHAWCURTIS] = lambda n: 2 ** n + 1
+for rule in [otexp.FejerExperiment.FEJERTYPE2, otexp.FejerExperiment.CLENSHAWCURTIS]:
+    prev_nodes = None
+    for N in [1, 2, 3, 4, 5]:
+        experiment = otexp.FejerExperiment([1], rule)
+        experiment.setUseNestedLevels(True)
+        experiment.setLevels([N])
+        nodes, weights = experiment.generateWithWeights()
+        print(f"---------- rule={rule} N={N} nested ----------")
+        print(rule, nodes, weights)
+        Nref = mapN[rule](N)
+        assert len(nodes) == Nref
+        assert len(weights) == len(nodes)
+        # check previous nodes are nested in current nodes
+        if prev_nodes is not None:
+            tree = ot.KDTree(nodes)
+            for x in prev_nodes:
+                k = tree.query(x)
+                d = ot.Point(x - nodes[k]).norm()
+                assert d < 1e-15, f"x={x} xk={nodes[k]} d={d}"
+        prev_nodes = nodes
