ece3e587e08cb8aa08259ef62c6c8dd567911167
regislebrun <regis.lebrun@m4x.org>
2025-03-14 10:30:57 +0100

    Improved PointConditionalDistribution
    Now, one can force the use of the generic computeConditional[PDF, CDF, Quantile] if the conditioning is done on the n-1 first components and the last component is the conditioned one.
    The specific case of bivariate symmetrical copulas is also implemented the following way: if it is the first component which is conditiod by the second one, thet are swaped in order to use the generic methods.

--- lib/src/Base/Common/ResourceMap.cxx
+++ lib/src/Base/Common/ResourceMap.cxx
@@ -1156,9 +1156,10 @@ void ResourceMap::loadDefaultConfiguration()
   addAsBool("PointConditionalDistribution-InitializeSampling", true);
   addAsBool("PointConditionalDistribution-InitializeTransformation", true);
   addAsBool("PointConditionalDistribution-UseSimplifiedVersion", true);
-  addAsString("PointConditionalDistribution-CubaAlgorithm", "cuhre");
+  addAsBool("PointConditionalDistribution-UseGenericConditionalMethods", true);
+  addAsString("PointConditionalDistribution-CubaAlgorithm", "cuhre", {"cuhre", "divonne", "suave", "vegas"});
   addAsString("PointConditionalDistribution-OptimizationAlgorithm", "Cobyla");
-  addAsString("PointConditionalDistribution-RangeAdaptationMethod", "Rank");
+  addAsString("PointConditionalDistribution-RangeAdaptationMethod", "NormalCopula", {"None", "Normal", "NormalCopula"});
   addAsUnsignedInteger("PointConditionalDistribution-RatioUniformCandidateNumber", 10000);
   addAsUnsignedInteger("PointConditionalDistribution-SmallDimension", 6);
 
