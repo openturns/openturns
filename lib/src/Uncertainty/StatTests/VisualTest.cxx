56dbeb892de0a3c970bcd94853d5bfdc7598c6ee
Mathieu Balesdent <mathieubalesdent@gmail.com>
2025-03-20 15:05:33 +0100

    modification of axes title

--- lib/src/Uncertainty/StatTests/VisualTest.cxx
+++ lib/src/Uncertainty/StatTests/VisualTest.cxx
@@ -94,7 +94,7 @@ Graph VisualTest::DrawQQplot(const Sample & sample,
   }
   Cloud cloudQQplot(data);
   cloudQQplot.setPointStyle(VisualTestGetPointStyle(size));
-  Graph graphQQplot("Sample versus model QQ-plot", sample.getDescription()[0], dist.__str__(), true, "topleft");
+  Graph graphQQplot("Sample versus model QQ-plot", "Empirical Quantile", "Distribution Quantile", true, "topleft");
   // First, the bisector
   Sample diagonal(2, 2);
   diagonal(0, 0) = data(0, 0);
