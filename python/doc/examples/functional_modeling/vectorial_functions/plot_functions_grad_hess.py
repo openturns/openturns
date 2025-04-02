ca19d8dee5ec93fbf5c8d2d1516a06fe9eac5852
Anne Dutfoy <anne.dutfoy@m4x.org>
2025-04-02 20:08:48 +0200

    Typos from Julien's remarks

--- python/doc/examples/functional_modeling/vectorial_functions/plot_functions_grad_hess.py
+++ python/doc/examples/functional_modeling/vectorial_functions/plot_functions_grad_hess.py
@@ -19,6 +19,7 @@ Get the gradient and hessian as functions
 #
 import openturns as ot
 
+# %%
 # Case 1: The :class:`~openturns.SymbolicFunction`
 # ================================================
 #
@@ -28,26 +29,26 @@ import openturns as ot
 #
 # Let :math:`f : \Rset^2 \rightarrow \Rset^2` be defined by:
 #
-# ..math::
+# .. math::
 #    f(\vect{x}) = (f_1(x_1, x_2), f_2(x_1, x_2)) = (x_1^2 + 2x_2^2, x_1 + 3x_2^4)
 #
 # We want to define the gradient function :math:`\Rset^2 \rightarrow \Rset^4` defined by:
 #
-# ..math::
+# .. math::
 #    :label: gradientFct
 #
-#    \vect{x} \rightaoow & = (\dfrac{\partial f_1 (\vect{x})}{\partial x_1},
+#    \vect{x} \rightarrow & = (\dfrac{\partial f_1 (\vect{x})}{\partial x_1},
 #                     \dfrac{\partial f_1 (\vect{x})}{\partial x_2},
 #                     \dfrac{\partial f_2 (\vect{x})}{\partial x_1},
 #                     \dfrac{\partial f_2 (\vect{x})}{\partial x_2})\\
 #                & = (2x_1, 4x_2, 1, 12x_2^3)
 #
 # To get the gradient as a function, we first get the analytical expressions of the derivatives:
-f = ot.SymbolicFunction(['x1', 'x2'], ['x1^2 + 2*x2^2', 'x1 + 3*x2^4'])
-formula_f1_deriv_x1 = f.getGradient().getImplementation().getFormula(0,0)
-formula_f1_deriv_x2 = f.getGradient().getImplementation().getFormula(1,0)
-formula_f2_deriv_x1 = f.getGradient().getImplementation().getFormula(0,1)
-formula_f2_deriv_x2 = f.getGradient().getImplementation().getFormula(1,1)
+f = ot.SymbolicFunction(['x1', 'x2'], ['x1^2 + 2 * x2^2', 'x1 + 3 * x2^4'])
+formula_f1_deriv_x1 = f.getGradient().getImplementation().getFormula(0, 0)
+formula_f1_deriv_x2 = f.getGradient().getImplementation().getFormula(1, 0)
+formula_f2_deriv_x1 = f.getGradient().getImplementation().getFormula(0, 1)
+formula_f2_deriv_x2 = f.getGradient().getImplementation().getFormula(1, 1)
 
 # %%
 # Then we create a new :class:`~openturns.SymbolicFunction` from these analytical expressions:
@@ -58,10 +59,10 @@ print(gradient_AsFunction)
 # %%
 # We want to define the hessian function :math:`\Rset^2 \rightarrow \Rset^6` defined by:
 #
-# ..math::
+# .. math::
 #    :label: hessianFct
 #
-#    \vect{x} \rightaoow & = (\dfrac{\partial^2 f_1 (\vect{x})}{\partial x_1^2},
+#    \vect{x} \rightarrow & = (\dfrac{\partial^2 f_1 (\vect{x})}{\partial x_1^2},
 #                    \dfrac{\partial^2 f_1 (\vect{x})}{ \partial x_1\partial x_2},
 #                    \dfrac{\partial^2 f_1 (\vect{x})}{\partial x_2^2},
 #                    \dfrac{\partial f_2 (\vect{x})}{\partial x_1^2},
@@ -70,12 +71,12 @@ print(gradient_AsFunction)
 #                & = (2, 0 , 4, 0, 0, 36x_2^2)
 #
 # To get the hessian as a function, we first get the analytical expressions of the second derivatives:
-formula_f1_hessian_x1x1 = f.getHessian().getImplementation().getFormula(0,0,0)
-formula_f1_hessian_x1x2 = f.getHessian().getImplementation().getFormula(1,0,0)
-formula_f1_hessian_x2x2 = f.getHessian().getImplementation().getFormula(1,1,0)
-formula_f2_hessian_x1x1 = f.getHessian().getImplementation().getFormula(0,0,1)
-formula_f2_hessian_x1x2 = f.getHessian().getImplementation().getFormula(1,0,1)
-formula_f2_hessian_x2x2 = f.getHessian().getImplementation().getFormula(1,1,1)
+formula_f1_hessian_x1x1 = f.getHessian().getImplementation().getFormula(0, 0, 0)
+formula_f1_hessian_x1x2 = f.getHessian().getImplementation().getFormula(1, 0, 0)
+formula_f1_hessian_x2x2 = f.getHessian().getImplementation().getFormula(1, 1, 0)
+formula_f2_hessian_x1x1 = f.getHessian().getImplementation().getFormula(0, 0, 1)
+formula_f2_hessian_x1x2 = f.getHessian().getImplementation().getFormula(1, 0, 1)
+formula_f2_hessian_x2x2 = f.getHessian().getImplementation().getFormula(1, 1, 1)
 
 # %%
 # Then we create a new :class:`~openturns.SymbolicFunction` from these analytical expressions:
@@ -94,11 +95,12 @@ print(hessian_AsFunction)
 # To get the gradient function as a function defined in :eq:`gradientFct`, we have to use a
 # :class:`~openturns.PythonFunction`. We re-use the prevous function for for educational purposes.
 def gradient_AsFunction_Python(inPoint):
-    f1_deriv_x1 = f.gradient(inPoint)[0,0]
-    f1_deriv_x2 = f.gradient(inPoint)[1,0]
-    f2_deriv_x1 = f.gradient(inPoint)[0,1]
-    f2_deriv_x2 = f.gradient(inPoint)[1,1]
+    f1_deriv_x1 = f.gradient(inPoint)[0, 0]
+    f1_deriv_x2 = f.gradient(inPoint)[1, 0]
+    f2_deriv_x1 = f.gradient(inPoint)[0, 1]
+    f2_deriv_x2 = f.gradient(inPoint)[1, 1]
     return [f1_deriv_x1, f1_deriv_x2, f2_deriv_x1, f2_deriv_x2]
+
 
 gradient_AsFunction_OT = ot.PythonFunction(2, 4, gradient_AsFunction_Python)
 print(gradient_AsFunction_OT([1.0, 2.0]))
@@ -106,14 +108,15 @@ print(gradient_AsFunction_OT([1.0, 2.0]))
 # %%
 # To get the hessian function as a functiond efined in :eq:`hessianFct`, we do the same:
 def hessian_AsFunction_Python(inPoint):
-    f1_hessian_x1x1 = f.hessian(inPoint)[0,0,0]
-    f1_hessian_x1x2 = f.hessian(inPoint)[1,0,0]
-    f1_hessian_x2x2 = f.hessian(inPoint)[1,1,0]
-    f2_hessian_x1x1 = f.hessian(inPoint)[0,0,1]
-    f2_hessian_x1x2 = f.hessian(inPoint)[1,0,1]
-    f2_hessian_x2x2 = f.hessian(inPoint)[1,1,1]
+    f1_hessian_x1x1 = f.hessian(inPoint)[0, 0, 0]
+    f1_hessian_x1x2 = f.hessian(inPoint)[1, 0, 0]
+    f1_hessian_x2x2 = f.hessian(inPoint)[1, 1, 0]
+    f2_hessian_x1x1 = f.hessian(inPoint)[0, 0, 1]
+    f2_hessian_x1x2 = f.hessian(inPoint)[1, 0, 1]
+    f2_hessian_x2x2 = f.hessian(inPoint)[1, 1, 1]
     return [f1_hessian_x1x1, f1_hessian_x1x2, f1_hessian_x2x2, f2_hessian_x1x1, f2_hessian_x1x2,
             f2_hessian_x2x2]
+
 
 hessian_AsFunction_OT = ot.PythonFunction(2, 6, hessian_AsFunction_Python)
 print(hessian_AsFunction_OT([1.0, 2.0]))
