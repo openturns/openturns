0da73473c1858211834f46d0691aca8f0a38c4ee
Anne Dutfoy <anne.dutfoy@m4x.org>
2025-07-18 08:21:40 +0200

    Fixed #2993

--- python/doc/examples/functional_modeling/vectorial_functions/plot_functions_grad_hess.py
+++ python/doc/examples/functional_modeling/vectorial_functions/plot_functions_grad_hess.py
@@ -1,6 +1,6 @@
 """
 =========================================
-Get the gradient and hessian as functions
+Get the gradient and Hessian as functions
 =========================================
 """
 
@@ -8,14 +8,14 @@ Get the gradient and hessian as functions
 # Context
 # =======
 #
-# Let :math:`f : \mathbb{R}^\inputDim \mapsto \mathbb{R}^q` a vectorial function defined by
+# Let :math:`f : \mathbb{R}^\inputDim \mapsto \mathbb{R}^q` a function defined by
 # its component functions :math:`f_i: \mathbb{R}^d \mapsto \mathbb{R}`. We want to get its gradient
-# and hessian as functions. The way to get it depends on the type of the function :math:`f`.
+# and Hessian as functions. The way to get it depends on the type of the function :math:`f`.
 #
 # We can do that:
 #
 # - Case 1: This case covers the :class:`~openturns.SymbolicFunction`,
-# - Case 2: This case covers all the general case.
+# - Case 2: This case covers the general case.
 #
 import openturns as ot
 
@@ -37,10 +37,10 @@ import openturns as ot
 # .. math::
 #    :label: gradientFct
 #
-#    \vect{x} \rightarrow & = (\dfrac{\partial f_1 (\vect{x})}{\partial x_1},
+#    \vect{x} = (x_1, x_2) \rightarrow & \left(\dfrac{\partial f_1 (\vect{x})}{\partial x_1},
 #                     \dfrac{\partial f_1 (\vect{x})}{\partial x_2},
 #                     \dfrac{\partial f_2 (\vect{x})}{\partial x_1},
-#                     \dfrac{\partial f_2 (\vect{x})}{\partial x_2})\\
+#                     \dfrac{\partial f_2 (\vect{x})}{\partial x_2}\right)\\
 #                & = (2x_1, 4x_2, 1, 12x_2^3)
 #
 # To get the gradient as a function, we first get the analytical expressions of the derivatives:
@@ -69,12 +69,12 @@ print(gradient_AsFunction)
 # .. math::
 #    :label: hessianFct
 #
-#    \vect{x} \rightarrow & = (\dfrac{\partial^2 f_1 (\vect{x})}{\partial x_1^2},
+#    \vect{x} = (x_1, x_2) \rightarrow & \left(\dfrac{\partial^2 f_1 (\vect{x})}{\partial x_1^2},
 #                    \dfrac{\partial^2 f_1 (\vect{x})}{ \partial x_1\partial x_2},
 #                    \dfrac{\partial^2 f_1 (\vect{x})}{\partial x_2^2},
 #                    \dfrac{\partial f_2 (\vect{x})}{\partial x_1^2},
-#                    \dfrac{\partial f_2 (\vect{x})}{ \partial x_1\partial x_2}),
-#                    \dfrac{\partial^2 f_2 (\vect{x})}{\partial x_2^2}\\
+#                    \dfrac{\partial f_2 (\vect{x})}{ \partial x_1\partial x_2},
+#                    \dfrac{\partial^2 f_2 (\vect{x})}{\partial x_2^2} \right)\\
 #                & = (2, 0 , 4, 0, 0, 36x_2^2)
 #
 # To get the hessian as a function, we first get the analytical expressions of the second derivatives:
@@ -104,8 +104,8 @@ print(hessian_AsFunction)
 # Case 2: The general case
 # ========================
 #
-# Here, we consider a function :math:`f` such that we do not have access to the analytical
-# expression of its gradient and hessian functions.
+# Here, we consider a function :math:`f` for which the analytical expressions
+# of its gradient and Hessian are not available.
 #
 # To get the gradient function as a function defined in :eq:`gradientFct`, we have to use a
 # :class:`~openturns.PythonFunction`. We re-use the previous function for for educational purposes.
@@ -123,7 +123,7 @@ gradient_AsFunction_OT = ot.PythonFunction(2, 4, gradient_AsFunction_Python)
 print(gradient_AsFunction_OT([1.0, 2.0]))
 
 # %%
-# To get the hessian function as a functiond efined in :eq:`hessianFct`, we do the same:
+# To get the hessian function as a function defined in :eq:`hessianFct`, we do the same:
 
 
 def hessian_AsFunction_Python(inPoint):
