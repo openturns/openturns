bdc8d88b4915b413761db8f33458e6a6684f86f6
josephmure <31989332+josephmure@users.noreply.github.com>
2025-11-27 15:36:49 +0100

    Refactor search result handling in mysearchtools.js
    Refactor _finishSearch to count only visible items and update related calls.
    
    Closes #2254

--- python/doc/_static/js/mysearchtools.js
+++ python/doc/_static/js/mysearchtools.js
@@ -135,21 +135,22 @@ const _displayItem = (item, highlightTerms, searchTerms) => {
       });
   Search.output.appendChild(listItem);
 };
-const _finishSearch = (resultCount) => {
+const _finishSearch = () => {
   Search.stopPulse();
   Search.title.innerText = _("Search Results");
-  if (!resultCount)
+  // Count only visible items (fixes #2254)
+  const visibleCount = Search.output.querySelectorAll('li.show, li:not(.hide)').length;
+  if (!visibleCount)
     Search.status.innerText = Documentation.gettext(
       "Your search did not match any documents. Please make sure that all words are spelled correctly and that you've selected enough categories."
     );
   else
     Search.status.innerText = _(
-      `Search finished, found ${resultCount} page(s) matching the search query.`
+      `Search finished, found ${visibleCount} page(s) matching the search query.`
     );
 };
 const _displayNextItem = (
   results,
-  resultCount,
   highlightTerms,
   searchTerms
 ) => {
@@ -158,12 +159,12 @@ const _displayNextItem = (
   if (results.length) {
     _displayItem(results.pop(), highlightTerms, searchTerms);
     setTimeout(
-      () => _displayNextItem(results, resultCount, highlightTerms, searchTerms),
+      () => _displayNextItem(results, highlightTerms, searchTerms),
       5
     );
   }
   // search finished, update title and status message
-  else _finishSearch(resultCount);
+  else _finishSearch();
 };
 
 /**
@@ -375,7 +376,7 @@ const Search = {
     // console.info("search results:", Search.lastresults);
 
     // print the results
-    _displayNextItem(results, results.length, highlightTerms, searchTerms);
+    _displayNextItem(results, highlightTerms, searchTerms);
   },
 
   /**
