f3a96584160f6c7c7c20451e85a69dc39652e47d
Julien Schueller <schueller@phimeca.com>
2026-01-14 11:28:04 +0100

    Mesh: Fix segfault in getIntrinsicDimension

--- lib/src/Base/Geom/Mesh.cxx
+++ lib/src/Base/Geom/Mesh.cxx
@@ -103,6 +103,8 @@ UnsignedInteger Mesh::getDimension() const
 /* Intrinsic dimension accessor */
 UnsignedInteger Mesh::getIntrinsicDimension() const
 {
+  if (!getSimplicesNumber())
+    return 0;
   UnsignedInteger verticesPerSimplex = 1;
   UnsignedInteger lastIndex = simplices_(0, 0);
   while ((verticesPerSimplex <= dimension_) && (simplices_(0, verticesPerSimplex) != lastIndex))
