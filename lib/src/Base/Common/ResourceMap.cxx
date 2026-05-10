0e89b7c2a4607ca2f2fd80acbd8aa1c44f4db232
Michael BAUDIN <michael.baudin@gmail.com>
2026-05-10 23:03:29 +0200

    ResourceMap.cxx: Fixed HyperbolicEnumerateFunction-QNormTolerance default value

--- lib/src/Base/Common/ResourceMap.cxx
+++ lib/src/Base/Common/ResourceMap.cxx
@@ -1640,7 +1640,7 @@ void ResourceMap::loadDefaultConfiguration()
 
     // HyperbolicEnumerateFunction parameters //
   addAsScalar("HyperbolicEnumerateFunction-DefaultQ", 0.4);
-  addAsScalar("HyperbolicEnumerateFunction-QNormTolerance", 0.4);
+  addAsScalar("HyperbolicEnumerateFunction-QNormTolerance", 1.0e-13);
 
   // MarginalTransformationEvaluation parameters //
   addAsBool("MarginalTransformationEvaluation-Simplify", true);
