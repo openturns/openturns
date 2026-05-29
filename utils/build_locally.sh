252348e036c47bf2b1a769666069ae8787664f2d
Julien Schueller <schueller@phimeca.com>
2026-05-29 15:06:25 +0200

    CI: Native aarch64 job
    Closes #3208

--- utils/build_locally.sh
+++ utils/build_locally.sh
@@ -22,8 +22,14 @@ case $choice in
     docker run --rm -e MAKEFLAGS -e OPENTURNS_NUM_THREADS=2 -e OMP_NUM_THREADS=1 -v `pwd`:/io openturns/archlinux-python /io/.ci_support/run_docker_coverage.sh `id -u`:`id -g`
     ;;
   "4" | "aarch64")
-    docker pull openturns/debian-aarch64
-    docker run --rm -e MAKEFLAGS -e OPENTURNS_NUM_THREADS=2 -v `pwd`:/io openturns/debian-aarch64 /io/.ci_support/run_docker_aarch64.sh
+    if test `uname -m` == "aarch64"
+    then
+      docker pull debian:13
+      docker run --rm -e MAKEFLAGS -e OPENTURNS_NUM_THREADS=2 -v `pwd`:/io debian:13 /io/.ci_support/run_docker_debian_native.sh
+    else
+      docker pull openturns/debian-aarch64
+      docker run --rm -e MAKEFLAGS -e OPENTURNS_NUM_THREADS=2 -v `pwd`:/io openturns/debian-aarch64 /io/.ci_support/run_docker_aarch64.sh
+    fi
     ;;
   "5" | "mindeps")
     docker pull debian:11
