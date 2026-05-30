ddaf777ef35dcbcbbd83c86be191ae20bc1255db
Julien Schueller <schueller@phimeca.com>
2026-05-30 11:21:49 +0200

    CI/mindeps: Add timeout

--- .ci_support/run_docker_mindeps.sh
+++ .ci_support/run_docker_mindeps.sh
@@ -8,5 +8,5 @@ cd /tmp
 cmake -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -D_GLIBCXX_ASSERTIONS" -DCMAKE_INSTALL_PREFIX=$PWD/install -DSWIG_COMPILE_FLAGS="-O1" -S /io -B build
 cd build
 make install
-OPENTURNS_NUM_THREADS=1 ctest -R pyinstallcheck --output-on-failure --schedule-random ${MAKEFLAGS} -E FunctionalChaosSobolIndices_std
+OPENTURNS_NUM_THREADS=1 ctest -R pyinstallcheck --output-on-failure --timeout 200 --schedule-random ${MAKEFLAGS} -E FunctionalChaosSobolIndices_std
 
