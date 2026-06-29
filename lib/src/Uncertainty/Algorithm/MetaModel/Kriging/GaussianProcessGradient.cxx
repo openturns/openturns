f55396edaedeb7868058d655dbe928c05f90c083
Julien Schueller <schueller@phimeca.com>
2026-06-29 09:13:25 +0200

    GPR: Fix exception message

--- lib/src/Uncertainty/Algorithm/MetaModel/Kriging/GaussianProcessGradient.cxx
+++ lib/src/Uncertainty/Algorithm/MetaModel/Kriging/GaussianProcessGradient.cxx
@@ -70,7 +70,7 @@ GaussianProcessGradient::GaussianProcessGradient(const Basis & basis,
   }
   if (covarianceModel.getInputDimension() != inputSample.getDimension())
     throw InvalidArgumentException(HERE) << "In GaussianProcessGradient::GaussianProcessGradient, error: the input dimension=" << covarianceModel.getInputDimension() << " of the covariance model should match the dimension=" << inputSample.getDimension() << " of the input sample";
-  if (gamma.getSize() != inputSample.getSize()) throw InvalidArgumentException(HERE) << "In GaussianProcessGradient::GaussianProcessGradient, error: the number of covariance coefficients=" << gamma.getSize() << " is different from the output sample dimension=" << covarianceModel.getOutputDimension();
+  if (gamma.getSize() != inputSample.getSize()) throw InvalidArgumentException(HERE) << "In GaussianProcessGradient::GaussianProcessGradient, error: the number of covariance coefficients=" << gamma.getSize() << " is different from the input sample size=" << inputSample.getSize();
   setParameter(Point(getInputDimension()));
 }
 
--- lib/src/Uncertainty/Algorithm/MetaModel/Kriging/KrigingEvaluation.cxx
+++ lib/src/Uncertainty/Algorithm/MetaModel/Kriging/KrigingEvaluation.cxx
@@ -68,7 +68,7 @@ KrigingEvaluation::KrigingEvaluation (const Basis & basis,
   }
 
   if (covarianceModel.getInputDimension() != inputSample.getDimension()) throw InvalidArgumentException(HERE) << "In KrigingEvaluation::KrigingEvaluation, error: the input dimension=" << covarianceModel.getInputDimension() << " of the covariance model should match the dimension=" << inputSample.getDimension() << " of the input sample";
-  if (gamma.getSize() != inputSample.getSize()) throw InvalidArgumentException(HERE) << "In KrigingEvaluation::KrigingEvaluation, error: the number of covariance coefficients=" << gamma.getSize() << " is different from the output sample dimension=" << covarianceModel.getOutputDimension();
+  if (gamma.getSize() != inputSample.getSize()) throw InvalidArgumentException(HERE) << "In KrigingEvaluation::KrigingEvaluation, error: the number of covariance coefficients=" << gamma.getSize() << " is different from the input sample size=" << inputSample.getSize();
   setInputDescription(Description::BuildDefault(getInputDimension(), "x"));
   setOutputDescription(Description::BuildDefault(getOutputDimension(), "y"));
   setParameter(Point(getInputDimension()));
