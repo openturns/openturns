92b242867e81b39504f5145b42ee0e8ee365b5c5
regislebrun <regis.lebrun@m4x.org>
2026-07-22 10:32:24 +0200

    ProbabilitySimulationAlgorithm: fix test calling nonexistent setEvent method

--- python/test/t_ProbabilitySimulationAlgorithm_std.py
+++ python/test/t_ProbabilitySimulationAlgorithm_std.py
@@ -134,7 +134,7 @@ for i, event in enumerate(all_events):
     print("MonteCarlo result=", myAlgo.getResult())
     print("probability distribution=", ot.ProbabilitySimulationResult(myAlgo.getResult()).getProbabilityDistribution())
 
-# Test setter of event
+# Test recomputation with a different event
 ot.RandomGenerator.SetSeed(0)
 experiment = ot.MonteCarloExperiment()
 X = ot.RandomVector(ot.Normal())
@@ -150,10 +150,13 @@ ott.assert_almost_equal(
 )
 Y2 = ot.CompositeRandomVector(ot.SymbolicFunction(["X"], ["2*X"]), X)
 event2 = ot.ThresholdEvent(Y2, ot.Less(), -2.0)
-algo.setEvent(event2)
+experiment2 = ot.MonteCarloExperiment()
+algo2 = ot.ProbabilitySimulationAlgorithm(event2, experiment2)
+algo2.setMaximumOuterSampling(100000)
+algo2.setMaximumCoefficientOfVariation(0.01)
 ot.RandomGenerator.SetSeed(0)
-algo.run()
-result2 = ot.ProbabilitySimulationResult(algo.getResult())
+algo2.run()
+result2 = ot.ProbabilitySimulationResult(algo2.getResult())
 ott.assert_almost_equal(
     result2.getProbabilityEstimate(), ot.Normal().computeCDF(-1), 1.0e-1, 0.0
 )
