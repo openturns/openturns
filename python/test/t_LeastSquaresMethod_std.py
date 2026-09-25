57e222bf011fae3cf8a42ff823695e35033c5bc8
regislebrun <regis.lebrun@m4x.org>
2026-09-25 10:04:33 +0200

    LARS+OMP expansion: use a Point rhs in the solveNormalGram test

--- python/test/t_LeastSquaresMethod_std.py
+++ python/test/t_LeastSquaresMethod_std.py
@@ -110,7 +110,7 @@ for j in range(largeBasisSize):
 proxyUnder = ot.DesignProxy(Xsmall, phisLarge)
 fullUnder = range(largeBasisSize)
 designUnder = ot.Matrix(proxyUnder.computeDesign(fullUnder))
-ySmall = ot.Sample([[1.0], [2.0], [4.0]])
+ySmall = ot.Point([1.0, 2.0, 4.0])
 gramRhs = designUnder.transpose() * ySmall
 methodUnder = ot.SVDMethod(proxyUnder, fullUnder)
 xGram = methodUnder.solveNormalGram(gramRhs)
